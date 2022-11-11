fed597eab824fd19787981b359725597bb323178
Nicole Konegen <konegen@radikant.com>
2022-11-11 12:05:26 +0100

    feat: implement sass plugin

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,4 +1,5 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'infineon-design-system-stencil',
@@ -18,4 +19,7 @@ export const config: Config = {
       serviceWorker: null, // disable service workers
     },
   ],
+  plugins: [
+    sass()
+  ]
 };
