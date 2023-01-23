fd5f3c51fbc7a534b0c384fb35900dd895d44b7c
Verena Lechner <lechneve@ISCN5CG1201S7M.infineon.com>
2023-01-23 14:54:49 +0100

    show docs as first tab

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,17 +4,24 @@ import {defineCustomElements} from '../loader';
 
 defineCustomElements();
 
- 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
+      date: /Date$/
     },
   },
+  previewTabs: {
+    // canvas: {
+    //   hidden: true,
+    // },
+    'storybook/docs/panel': { index: -1 },
+  },
+  viewMode: 'docs',
+
   docs: {
-    source: {
+     source: {
         state: 'open',
     },
   }
