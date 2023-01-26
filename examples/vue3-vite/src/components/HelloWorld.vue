cd69ae0333471d9baf963544d356c214f1e7c367
Verena Lechner <lechneve@ISCN5CG1201S7M.infineon.com>
2023-01-26 13:04:53 +0100

    vue3-vite and vanilla-cdn added as examples

---
+++ examples/vue3-vite/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <ifx-button variant="outline" href="" target="_blank" color="primary" size="m" disabled="false" icon="false">
+    Button
+  </ifx-button>
+
+
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
