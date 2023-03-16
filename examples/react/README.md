557f5aef76505fd93dc713ffacbce272da796750
Verena Lechner <lechneve@ISCN5CG1201S7M.infineon.com>
2023-03-16 17:44:58 +0100

    added react and angular

---
+++ examples/react/README.md
@@ -0,0 +1,14 @@
+# Stencil web components in React 
+
+Install the React example app to see, how Stencil web components can be used within it.
+
+
+## Installation
+
+```
+npm install
+```
+
+```
+npm run start
+```
---
+++ examples/react/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello world
+        </p>
+
+        <ifx-button variant="solid" icon="" position="left" href="" target="_blank" color="primary" size="m" disabled="false">
+          Button
+        </ifx-button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
