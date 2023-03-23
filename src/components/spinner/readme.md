163cdc333a98faf6014e322ed59779c1cd7dd1a8
Verena Lechner <lechneve@ISCN5CG1201S7M.infineon.com>
2023-03-23 11:49:10 +0100

    basic spinner component

---
+++ src/components/spinner/readme.md
@@ -0,0 +1,17 @@
+# ifx-spinner
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property | Attribute | Description | Type     | Default     |
+| -------- | --------- | ----------- | -------- | ----------- |
+| `size`   | `size`    |             | `string` | `undefined` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
---
+++ src/components/spinner/spinner.tsx
@@ -0,0 +1,24 @@
+import { Component, h, Prop } from '@stencil/core';
+
+@Component({
+  tag: 'ifx-spinner',
+  styleUrl: 'spinner.scss',
+  shadow: true,
+})
+export class Spinner {
+  @Prop() size: string;
+
+  render() {
+    return (
+      <div class="spinner" style={{ width: this.getSize(), height: this.getSize() }}>
+        <div class="border"></div>
+      </div>
+    );
+  }
+
+  getSize() {
+    return `${this.size}` === "s"
+      ? "25px"
+      : "40px";
+  }
+}
