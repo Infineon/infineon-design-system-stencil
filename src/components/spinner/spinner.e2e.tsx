7161dba13def502df21f2ade092303f6f5c36ca4
Verena Lechner <lechneve@ISCN5CG1201S7M.infineon.com>
2023-03-23 17:01:48 +0100

    added wait to spinner test

--- src/components/spinner/spinner.e2e.tsx
+++ src/components/spinner/spinner.e2e.tsx
@@ -6,14 +6,15 @@ describe('spinner', () => {
     await page.setContent('<spinner></spinner>');
 
     const element = await page.find('spinner');
+    await page.waitForChanges();
     expect(element).toHaveClass('hydrated');
   });
 
   it('displays the correct size', async () => {
     const page = await newE2EPage();
     await page.setContent('<spinner size="s"></spinner>');
-
     const element = await page.find('spinner >>> .spinner');
+    await page.waitForChanges();
     expect(element).toHaveClass('s');
   });
 });
