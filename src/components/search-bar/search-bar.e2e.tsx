ce757be4e511166c6506e9d8cf5693cbf95cd564
Verena Lechner <lechneve@ISCN5CG1201S7M.infineon.com>
2023-03-28 20:05:09 +0200

    test updated

--- src/components/search-bar/search-bar.e2e.tsx
+++ src/components/search-bar/search-bar.e2e.tsx
@@ -4,9 +4,9 @@ describe('ifx-search-bar', () => {
   it('emits search event on input', async () => {
     const page = await newE2EPage();
     await page.setContent('<ifx-search-bar></ifx-search-bar>');
-
-    const input = await page.waitForSelector('.search-bar input');
+    const input = await page.find('ifx-search-bar >>> .search-bar__input-wrapper input');
     await input.type('query');
+
     const event = await page.waitForEvent('search');
     expect(event).not.toBeNull();
   });
