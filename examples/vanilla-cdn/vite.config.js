54ada1162a93257bbd1e40659bcc5555c2d7c33c
Verena Lechner <lechneve@ISCN5CG1201S7M.infineon.com>
2023-03-29 11:16:58 +0200

    simplified the code for the vanilla example hot reload

--- examples/vanilla-cdn/vite.config.js
+++ examples/vanilla-cdn/vite.config.js
@@ -1,16 +1,6 @@
 import { defineConfig } from 'vite'
-import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
-  plugins: [
-    createHtmlPlugin({
-      injectOptions: {
-        data: {
-          folderTimestamp: Date.now()
-        }
-      }
-    })
-  ],
   server: {
     watch: {
       // Watch the 'dist' folder for changes
