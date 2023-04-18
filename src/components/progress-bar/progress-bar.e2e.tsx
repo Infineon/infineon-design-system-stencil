25c39893bec382f4f7e235f332e60e139fa159cb
Verena Lechner <lechneve@ISCN5CG1201S7M.infineon.com>
2023-04-18 11:11:15 +0200

    test updated

--- src/components/progress-bar/progress-bar.e2e.tsx
+++ src/components/progress-bar/progress-bar.e2e.tsx
@@ -16,6 +16,18 @@ describe('ifx-progress-bar', () => {
     const element = await page.find('ifx-progress-bar >>> .progress-bar');
     await page.waitForChanges();
     expect(element).toHaveClass('s');
+  });
 
+  it('emits percentageChange event when percentage changes', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<ifx-progress-bar percentage="25"></ifx-progress-bar>');
+
+    const progressBar = await page.find('ifx-progress-bar');
+    const percentageChange = await progressBar.spyOnEvent('percentageChange');
+
+    progressBar.setProperty('percentage', 50);
+    await page.waitForChanges();
+
+    expect(percentageChange).toHaveReceivedEventDetail(50);
   });
 });
