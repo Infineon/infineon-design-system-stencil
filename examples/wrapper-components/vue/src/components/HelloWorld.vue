78cf1950dbf914b368f482fa47b7264d85cc54ce
Verena Lechner <lechneve@ISCN5CG1201S7M.infineon.com>
2023-05-02 17:12:03 +0200

    example app using vue wrapper updated

--- examples/wrapper-components/vue/src/components/HelloWorld.vue
+++ examples/wrapper-components/vue/src/components/HelloWorld.vue
@@ -3,9 +3,15 @@
 <template>
   <div class="greetings">
     <h1 class="green">Stencil Framework integration - Vue3 </h1>
-    <ifx-button variant="solid" icon="" position="left" href="" target="_blank" color="primary" size="m" disabled="false">
-      Button
-    </ifx-button>
+    <h2>V-For</h2>
+
+    <th v-for="btn in buttons" scope="col">
+      <ifx-button variant="solid" icon="" position="left" href="" target="_blank" color="primary" size="m"
+        disabled="false">
+        {{ btn }}
+      </ifx-button>
+    </th>
+
   </div>
 </template>
 
@@ -20,3 +26,11 @@ h3 {
   font-size: 1.2rem;
 }
 </style>
+
+<script setup>
+import {
+  ref,
+} from 'vue';
+
+const buttons = ref(["Button 1", "Button 2", "Button 3"]);
+</script>
