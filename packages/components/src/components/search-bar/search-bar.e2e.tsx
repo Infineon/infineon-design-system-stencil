907e07e147003b29e585b2086958bd6c241d03b6
Verena Lechner <lechneve@ISCN5CG1201S7M.infineon.com>
2023-05-15 09:22:06 +0200

    search-bar e2e test fix

--- packages/components/src/components/search-bar/search-bar.e2e.tsx
+++ packages/components/src/components/search-bar/search-bar.e2e.tsx
@@ -1,17 +1,15 @@
 import { newE2EPage } from '@stencil/core/testing';
 
 describe('ifx-search-bar', () => {
-  it('emits search event on input', async () => {
+  it('emits ifx-change event on input', async () => {
     const page = await newE2EPage();
     await page.setContent('<ifx-search-bar></ifx-search-bar>');
 
-
-    const shadowRoot = await page.evaluateHandle(() => document.querySelector('ifx-search-bar').shadowRoot);
-
-    const input = await shadowRoot.$('ifx-search-input >>> .search-input__wrapper input[type="text"]');
+    const searchBar = await page.find('ifx-search-bar');
+    const input = await searchBar.find('ifx-search-input >>> .search-input__wrapper input[type="text"]');
     await input.type('query');
 
-    const event = await page.waitForEvent('search');
+    const event = await page.waitForEvent('ifxChange');
     expect(event).not.toBeNull();
   });
 });
