dce1906d237a610d61a02fe735040dee65ca460c
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2023-05-24 14:33:21 +0200

    removed unused code

--- examples/wrapper-components/react/src/App.tsx
+++ examples/wrapper-components/react/src/App.tsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
-import { IfxProgressBar, IfxButton } from '@infineon/infineon-design-system-react';
+import { IfxProgressBar, IfxSearchBar, IfxButton } from '@infineon/infineon-design-system-react';
 
 
 function App() {
   const [progressValue, setProgressValue] = useState(25);  // Add this line
+  const ifxButtonRef = useRef(null);
 
   // Define your methods here
   const updateProgressOnClick = () => {
     setProgressValue((currentValue) => {
       return currentValue < 100 ? currentValue += 10 : currentValue = 10;
     });
+    console.log("ifxButtonRef. ", ifxButtonRef.current)
+   };
+
+  const handleSearch = (event: CustomEvent) => {
+    console.log("handling search: ", event.detail?.detail)
   };
   
 
@@ -19,11 +25,15 @@ function App() {
     <>
       <div className="greetings">
     <h1 className="green">Stencil Framework integration - React </h1>
+    <h2>Ifx-Search-Bar</h2>
+    <IfxSearchBar onIfxChange={handleSearch} style={{ width: '100%' }}  show-close-button="true"></IfxSearchBar>
+    <br />
+
       <h2>Ifx-Progress-Bar</h2>
       <IfxProgressBar value={progressValue} size="m" show-label="true"></IfxProgressBar>
       <br />
  
-    <IfxButton variant="outline" icon="" position="left" href="" target="_blank" color="primary" size="m"
+    <IfxButton ref={ifxButtonRef} variant="outline" icon="" position="left" href="" target="_blank" color="primary" size="m"
       disabled={false} onClick={updateProgressOnClick}>
       Increase by 10
     </IfxButton>
