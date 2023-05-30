5e6004c88a19453d311a912d7e71ae50f32cbf74
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2023-05-30 11:03:19 +0200

    ran npm build to update wrapper libraries

--- packages/components-vue/lib/components.ts
+++ packages/components-vue/lib/components.ts
@@ -3,7 +3,7 @@
 /* auto-generated vue proxies */
 import { defineContainer } from './vue-component-lib/utils';
 
-import type { JSX } from '../../components';
+import type { JSX } from '@infineon/infineon-design-system-stencil';
 
 
 
