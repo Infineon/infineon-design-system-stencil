ecb5e11834b34a82c6ceb73fc497285eb7d6383e
tishoyanchev <tihomir.yanchev-EE@infineon.com>
2023-06-15 10:23:21 +0200

    in the middle of fixing

--- examples/wrapper-components/react-javascript/src/App.js
+++ examples/wrapper-components/react-javascript/src/App.js
@@ -1,42 +1,12 @@
 import './App.css';
-import { useState } from 'react'
-import { IfxSearchBar, IfxProgressBar, IfxButton } from '@infineon/infineon-design-system-react';
+import { IfxButton } from '@infineon/infineon-design-system-react';
 
 
 function App() {
-  const [progressValue, setProgressValue] = useState(25);  // Add this line
-
-  // Define your methods here
-  const updateProgressOnClick = () => {
-    setProgressValue((currentValue) => {
-      console.log("updating progress-bar value ", progressValue)
-      return currentValue < 100 ? currentValue += 10 : currentValue = 10;
-    });
-  };
-  const handleSearch = (event) => {
-    console.log("handling search: ", event.detail?.detail)
-  };
-
-
-  const handleMouseOver = () => {
-    console.log('Mouse over event detected');
-  };
-
+  
   return (
     <div>
-      <h1 className="header">Stencil Framework integration - React + JS</h1>
-      <h2>Ifx-Search-Bar</h2>
-      <IfxSearchBar onIfxChange={handleSearch} style={{ width: '100%' }} show-close-button="true"></IfxSearchBar>
-      <br />
-
-      <h2>Ifx-Progress-Bar</h2>
-      <IfxProgressBar value={progressValue} size="m" show-label="true"></IfxProgressBar>
-      <br />
-
-      <IfxButton variant="outline" icon="" position="left" href="" target="_blank" color="primary" size="m"
-        disabled={false} onClick={updateProgressOnClick} onMouseOver={handleMouseOver}>
-        Increase by 10
-      </IfxButton>
+      <IfxButton variant='outline-text' href='http://google.com' target='_blank' color="warning" position='left' icon=''>Button</IfxButton>
     </div>
   );
 }
