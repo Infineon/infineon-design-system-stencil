345546fcff6c47190ef1262f19eadd3bc5f43bac
tishoyanchev <tihomir.yanchev-EE@infineon.com>
2023-06-22 15:41:31 +0200

    fixed angular issue

--- examples/stencil-components/angular/src/app/app.component.ts
+++ examples/stencil-components/angular/src/app/app.component.ts
@@ -1,4 +1,10 @@
+import { Component } from '@angular/core';
 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
 export class AppComponent {
   progressValue = 10;
   checked = false;
