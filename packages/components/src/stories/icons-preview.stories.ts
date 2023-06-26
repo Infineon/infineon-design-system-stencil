86952be994991d4d1bd8246e3431d49cc778831b
tishoyanchev <tihomir.yanchev-EE@infineon.com>
2023-06-26 14:49:56 +0200

    added icons-preview to storybook

---
+++ packages/components/src/stories/icons-preview.stories.ts
@@ -0,0 +1,17 @@
+import CustomMDXDocumentation from './Custom-MDX-Documentation.mdx';
+
+export default {
+  title: 'Components/IconsPreview',
+  parameters: {
+    docs: {
+      page: CustomMDXDocumentation,
+    },
+  },
+}
+
+const DefaultTemplate = () =>
+  `<ifx-icons-preview></ifx-icons-preview>`;
+
+
+export const Default = DefaultTemplate.bind({});
+
