f4274e58a46349c40b681a3c5e83ebfbed213c0e
tishoyanchev <tihomir.yanchev-EE@infineon.com>
2023-07-04 11:52:05 +0200

    added example apps

--- examples/wrapper-components/vue-typescript/src/App.vue
+++ examples/wrapper-components/vue-typescript/src/App.vue
@@ -1,23 +1,12 @@
 
 
 <template>
-  <h1 class="header">Stencil Framework integration - Vue + TS </h1>
-  <Alert />
-  <Button />
-  <Checkbox />
-  <ProgressBar />
-  <Search />
-  <TextInput />
+  <SearchInput />
 </template>
 
 
 <script lang="ts" setup>
-import Alert from './components/Alert.vue'
-import Button from './components/Button.vue'
-import Checkbox from './components/Checkbox.vue'
-import ProgressBar from './components/ProgressBar.vue'
-import Search from './components/Search.vue'
-import TextInput from './components/TextInput.vue'
+import SearchInput from './components/SearchInput.vue'
 
 
 </script>
