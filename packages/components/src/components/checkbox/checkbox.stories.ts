a4308f4fb53f1b830c059c15fbe4851eea5e3907
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2023-07-04 10:20:45 +0200

    cleaned up the usage template

--- packages/components/src/components/checkbox/checkbox.stories.ts
+++ packages/components/src/components/checkbox/checkbox.stories.ts
@@ -2,7 +2,7 @@ import { action } from '@storybook/addon-actions';
 
 export default {
   title: "Components/Checkbox",
-  tags: ['autodocs'],
+  // tags: ['autodocs'],
   args: {
     error: false,
     disabled: false,
