963c4205c966ab3206ab3b1f7a1e93c0cd88cd8a
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2023-07-04 09:00:22 +0200

    testing gh actions deploy

--- examples/wrapper-components/vue-javascript/src/components/Search.vue
+++ examples/wrapper-components/vue-javascript/src/components/Search.vue
@@ -13,9 +13,6 @@
     <h3>Using v-model</h3>
     <ifx-search-bar v-model="searchBarQuery" style="width: 100%" show-close-button="true"></ifx-search-bar>
     <p>Search bar 1: {{ searchBar1 }}</p>
-    <h3>One-way data binding</h3>
-    <ifx-search-bar @ifxChange="handleSearch2" style="width: 100%" show-close-button="true"></ifx-search-bar>
-    <p>Search bar 2: {{ searchBar2 }}</p>
 
     <br />
   </div>
@@ -24,7 +21,6 @@
 <script  setup>
 import { computed, ref } from 'vue'
 
-const searchBar2 = ref('');
 const searchBar1 = ref('');
 const searchInput = ref('');
 
@@ -42,11 +38,6 @@ const searchBarQuery = computed({
     handleSearch1(newEvent)
   }
 });
-
-function handleSearch2(event) {
-  console.log("handling search 2", event)
-  searchBar2.value = event.detail?.detail; //search input emits an event and the search bar, so it needs to be accessed this way
-}
 
 
 function handleSearch1(event) {
