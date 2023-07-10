1e071613f720e3eccb9ab3431b7844719344e61b
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2023-07-10 12:56:17 +0200

    link added to vanilla and angular example apps

--- examples/wrapper-components/vue-javascript/src/components/Link.vue
+++ examples/wrapper-components/vue-javascript/src/components/Link.vue
@@ -1,8 +1,8 @@
 
 
 <template>
-  <div class="app">
-      <ifx-link href="http://google.com" color="secondary" target="_blank" bold="" underline="">link</ifx-link>
+  <div>
+    <ifx-link href="http://google.com" color="secondary" target="_blank" bold="" underline="">link</ifx-link>
   </div>
 </template>
 
@@ -12,13 +12,5 @@
 
 </script>
 
-<style scoped>
-.app {
-  text-align: center;
-  flex-direction: column;
-  display: block;
-  align-items: center;
-}
-</style>
 
  
