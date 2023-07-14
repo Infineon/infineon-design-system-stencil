0efa436fee2621d4fc51a35f6f0ce6c44a339f12
tishoyanchev <tihomir.yanchev-EE@infineon.com>
2023-07-14 09:41:47 +0200

    updated navbar

--- examples/wrapper-components/react-typescript/src/App.tsx
+++ examples/wrapper-components/react-typescript/src/App.tsx
@@ -1,45 +1,13 @@
  import './App.css'
-import Button from './components/Button/button';
-import TextField from './components/TextFIeld/TextField';
-import ProgressBar from './components/ProgressBar/ProgressBar';
-import SearchBar from './components/SearchBar/SearchBar';
-import Accordion from './components/Accordion/Accordion';
-import RadioButton from './components/RadioButton/RadioButton';
 
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
-  
-
   return (
-       <div>
-    <h1 className="header">Stencil Framework integration - React + TS </h1>
-    <h2>Search Bar</h2>
-      <SearchBar />
-      <br />
-
-      <h2>Button</h2>
-      <Button />
-      <br />
-
-      <h2>Progress Bar</h2>
-      <ProgressBar />
-      <br />
-
-      <h2>Text Field</h2>
-      <TextField />
-      <br />
-
-      <h2>Accordion</h2>
-      <Accordion />
-      <br />
-
-      <h2>Radio Button</h2>
-      <RadioButton />
-      <br />
+      <div>
+         <Navbar />
       </div>
-    
    )
-
 }
 
 export default App
