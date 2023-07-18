a7ab7a92914e506bf364799407a8752c1b6a1efb
neumayreva <Eva.Neumayr@infineon.com>
2023-07-18 11:24:31 +0200

    added new components to react

--- examples/wrapper-components/react-javascript/src/components/FilterInput/FilterInput.js
+++ examples/wrapper-components/react-javascript/src/components/FilterInput/FilterInput.js
@@ -4,7 +4,7 @@ import { IfxFilterInput } from '@infineon/infineon-design-system-react';
 function FilterInput() {
     return (
         <div>
-           <IfxFilterInput label='Dropdown' size='m' disabled='true' icon='calendar16' search='false' filter='true' color='danger'></IfxFilterInput> 
+           <IfxFilterInput label='Dropdown Filter' size='m' disabled='false' icon='calendar16' search='false' filter='true' color='danger'></IfxFilterInput> 
         </div>
     );
 }
