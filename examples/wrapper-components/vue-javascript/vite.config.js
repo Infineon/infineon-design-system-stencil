e4e857985ab99c2dd5906d35243bcc29b592c55e
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2023-07-18 11:51:18 +0200

    deploying preview urls for the example apps using wrapper components

--- examples/wrapper-components/vue-javascript/vite.config.js
+++ examples/wrapper-components/vue-javascript/vite.config.js
@@ -1,7 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,7 +18,11 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    assetsDir: './assets',
+    base: './',
+  },
+});
