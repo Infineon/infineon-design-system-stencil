45dfa2b0ee29a2128db8493d5a7dd97bf7c6e2f6
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2023-07-19 16:51:39 +0200

    vue example fixed

--- examples/wrapper-components/vue-javascript/src/main.js
+++ examples/wrapper-components/vue-javascript/src/main.js
@@ -3,5 +3,4 @@ import App from './App.vue'
 
 import './assets/main.css'
 import { ComponentLibrary } from '@infineon/infineon-design-system-vue';
-console.log("component library", ComponentLibrary)
 createApp(App).use(ComponentLibrary).mount('#app');
