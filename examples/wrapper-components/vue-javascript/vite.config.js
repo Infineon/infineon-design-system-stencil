02029cdc584fe3e9174d10861cdb2ebfc4167d61
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2023-07-19 17:16:11 +0200

    vue example fixed

--- examples/wrapper-components/vue-javascript/vite.config.js
+++ examples/wrapper-components/vue-javascript/vite.config.js
@@ -6,6 +6,9 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "./",
+  exclude: [
+    '@ionic/core/loader' //fix weird Vite error "outdated optimize dep"
+  ],
   plugins: [
     vue({
       template: {
