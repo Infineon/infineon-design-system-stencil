072e96a6f31215154dbce2838f8b2d1549647909
tishoyanchev <tihomir.yanchev-EE@infineon.com>
2023-08-24 16:40:35 +0200

    current page working

--- packages/components/src/components/pagination/pagination.tsx
+++ packages/components/src/components/pagination/pagination.tsx
@@ -17,102 +17,141 @@ export class Pagination {
   @Prop() total: number = 1;
   @Prop() surroundingPageCount: number = 1;
   @State() visiblePageIndices: Array<number | string> = [];
-
+  @State() isLeftBtnDisabled: boolean = false;
+  @State() isRightBtnDisabled: boolean = false;
+  @State() currItem: number = 1;;
+  
+  private CLASS_DISABLED = "disabled"
+  private CLASS_ACTIVE = "active"
+  private CLASS_SIBLING_ACTIVE = "active-sibling"
+  private DATA_KEY = "pagination";
 
   @Watch('ifxSelect')
-  setItemsPerPage(e) { 
+  setItemsPerPage() { 
     //this.itemsPerPage = e.detail;
-  }
-
-  exampleFunction() { 
-    console.log('test')
   }
 
   componentDidLoad() { 
    this.calculateVisiblePageIndices()
+
+   var paginationElement = this.el.shadowRoot.querySelector(".pagination");
+   let leftArrow = paginationElement.querySelector('.prev')
+   this.navigateSinglePage(leftArrow, true) 
   }
 
   componentWillLoad() { 
-    if(this.currentPage <= 0) { 
-      this.internalPage = 1;
-    } else this.internalPage = this.currentPage;
-    
     const total = this.total <= this.itemsPerPage ? this.itemsPerPage : this.total;
     const itemsPerPage = this.itemsPerPage;
     const totalPageNumber = total / itemsPerPage;
+
+    if(this.currentPage <= 0) { 
+      this.internalPage = 1;
+    } else if(this.currentPage > totalPageNumber) {
+      this.internalPage = totalPageNumber;
+      this.currItem = this.internalPage;
+    } else this.internalPage = this.currentPage;
+
     this.numberOfPages = Array.from({ length: totalPageNumber }, (_, index) => index + 1);
+
+  
   }
 
+  initPagination(paginationContainer) {
+    var listItems = paginationContainer.querySelectorAll("li");
+   
+    paginationContainer.dataset[this.DATA_KEY] = Array.from(listItems).indexOf(paginationContainer.querySelector(".active")); 
+    
+    paginationContainer.querySelector(".prev").addEventListener("click", (e) => this.navigateSinglePage(e, false));
+    paginationContainer.querySelector(".next").addEventListener("click", (e) => this.navigateSinglePage(e, false));
+
+    
+    listItems.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        var parent = paginationContainer;
+        let listItems = parent.querySelectorAll("li");
+
+        parent.dataset[this.DATA_KEY] = Array.from(listItems).indexOf(e.currentTarget) 
+        this.changePage(parent, e, false)
+      });
+    });
+  }
+
+  navigateSinglePage(e, initialValue) {
+    let el = e;
+    if(typeof e.target === 'object') { 
+      el = e.target
+    } 
+  
+    if (!el.classList.contains(this.CLASS_DISABLED)) {
+      var parent = el.closest(".pagination");
+      var currActive = parseInt(parent.dataset[this.DATA_KEY], 10);
+      currActive += 1 * (el.classList.contains("prev") ? -1 : 1);
+
+      if(currActive === -1) { 
+        currActive = 0;
+      }
+
+      parent.dataset[this.DATA_KEY] = currActive;
+      this.changePage(parent, e, initialValue)
+    }
+  
+  }
+
+  changePage(pagination, e, initialValue) {
+    const paginationContainer = pagination;
+    var listItems = paginationContainer.querySelectorAll("li");
+    var currActive = parseInt(paginationContainer.dataset[this.DATA_KEY], 10);
+   
+    let currentPage = currActive;
+    let totalPages = this.numberOfPages.length;
+    let prevPage = currActive;
+    let nextPage = currActive+2;
+
+    this.ifxPageChange.emit({currentPage, totalPages, prevPage, nextPage})
+
+    listItems.forEach((item) => {
+      item.classList.remove(this.CLASS_ACTIVE);
+      item.classList.remove(this.CLASS_SIBLING_ACTIVE);
+    });
+    
+    if(typeof e.currentTarget === 'object') { 
+      currentPage = currActive;
+    }
+
+    if(initialValue && this.internalPage > 1) { 
+      currentPage = this.internalPage - 1;
+      paginationContainer.dataset[this.DATA_KEY] = currentPage;
+    }
+
+    listItems[currentPage].classList.add(this.CLASS_ACTIVE);
+
+    if (currentPage === 0) {
+      paginationContainer.querySelector(".prev").classList.add(this.CLASS_DISABLED);
+      paginationContainer.querySelector(".prev").disabled = true;
+      
+     
+    } else {
+      listItems[currentPage - 1].classList.add(this.CLASS_SIBLING_ACTIVE);
+      paginationContainer.querySelector(".prev").classList.remove(this.CLASS_DISABLED);
+      paginationContainer.querySelector(".prev").disabled = false;
+      
+    }
+
+    if (currentPage === (listItems.length - 1)) {
+      paginationContainer.querySelector(".next").classList.add(this.CLASS_DISABLED);
+      paginationContainer.querySelector(".next").disabled = true;
+     
+    } else {
+      paginationContainer.querySelector(".next").classList.remove(this.CLASS_DISABLED);
+      paginationContainer.querySelector(".next").disabled = false;
+    
+    }
+  }
 
   calculateVisiblePageIndices() { 
-    let self = this;
-    var CLASS_DISABLED = "disabled",
-    CLASS_ACTIVE = "active",
-    CLASS_SIBLING_ACTIVE = "active-sibling",
-    DATA_KEY = "pagination";
+    var paginationElement = this.el.shadowRoot.querySelector(".pagination");
+    this.initPagination(paginationElement)
 
-    var paginationElements = this.el.shadowRoot.querySelectorAll(".pagination");
-    paginationElements.forEach(initPagination);
-
-    function initPagination(element) {
-      
-      element.dataset[DATA_KEY] = Array.from(element.querySelectorAll("li")).indexOf(element.querySelector(".active")); 
-    
-      element.querySelector(".prev").addEventListener("click", navigateSinglePage);
-      element.querySelector(".next").addEventListener("click", navigateSinglePage);
-      var listItems = element.querySelectorAll("li");
-      listItems.forEach(function(item) {
-        item.addEventListener("click", function() {
-          var parent = this.closest(".pagination");
-          parent.dataset[DATA_KEY] = Array.from(parent.querySelectorAll("li")).indexOf(this);
-          changePage.apply(parent);
-        });
-      });
-    }
-
-    function navigateSinglePage() {
-      if (!this.classList.contains(CLASS_DISABLED)) {
-        var parent = this.closest(".pagination");
-        var currActive = parseInt(parent.dataset[DATA_KEY], 10);
-
-        currActive += 1 * (this.classList.contains("prev") ? -1 : 1);
-
-        parent.dataset[DATA_KEY] = currActive;
-        changePage.apply(parent);
-      }
-    }
-
-    function changePage() {
-      var listItems = this.querySelectorAll("li");
-      var currActive = parseInt(this.dataset[DATA_KEY], 10);
-
-      let currentPage = currActive+1;
-      let totalPages = self.numberOfPages.length;
-      let prevPage = currActive;
-      let nextPage = currActive+2;
-
-      self.ifxPageChange.emit({currentPage, totalPages, prevPage, nextPage})
-
-      listItems.forEach(function(item) {
-        item.classList.remove(CLASS_ACTIVE);
-        item.classList.remove(CLASS_SIBLING_ACTIVE);
-      });
-      
-      listItems[currActive].classList.add(CLASS_ACTIVE);
-
-      if (currActive === 0) {
-        this.querySelector(".prev").classList.add(CLASS_DISABLED);
-      } else {
-        listItems[currActive - 1].classList.add(CLASS_SIBLING_ACTIVE);
-        this.querySelector(".prev").classList.remove(CLASS_DISABLED);
-      }
-
-      if (currActive === (listItems.length - 1)) {
-        this.querySelector(".next").classList.add(CLASS_DISABLED);
-      } else {
-        this.querySelector(".next").classList.remove(CLASS_DISABLED);
-      }
-    }
   }
 
   render() {
@@ -131,9 +170,9 @@ export class Pagination {
         <div class='items__total-wrapper'>
           <div class='page__numbers-wrapper'>
           <div class="pagination">
-            <ifx-icon-button variant='outline' class="prev disabled" color='primary' icon='arrow-left-24'></ifx-icon-button>
+            <ifx-icon-button variant='outline' class="prev" color='primary' icon='arrow-left-24'></ifx-icon-button>
             <ol>
-              {this.numberOfPages.map((item, i) => 
+              {this.numberOfPages.map((item) => 
               <li class={`${this.internalPage === item? 'active' : ""}`}><a href={undefined}>{item}</a></li>)}
             </ol>
             <ifx-icon-button class="next" variant='outline' color='primary' icon='arrow-right-24'></ifx-icon-button>
