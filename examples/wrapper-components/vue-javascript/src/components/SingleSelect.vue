3524ce56f16fe9f6a619deca7172c525127f5545
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2023-08-28 09:38:57 +0200

    replaced document with this.root to not query the document

--- examples/wrapper-components/vue-javascript/src/components/SingleSelect.vue
+++ examples/wrapper-components/vue-javascript/src/components/SingleSelect.vue
@@ -12,6 +12,12 @@
       <ifx-button variant="outline" color="primary" @click="toggleError">Toggle Error</ifx-button>
     </div>
     <br />
+
+    <ifx-select type="single" value="undefined" ifx-size="medium (40px)" placeholder="true" search-enabled="true"
+      search-placeholder-value="Search..." :ifx-disabled="disabled2" :ifx-error="error" :ifx-error-message="errorMessage"
+      ifx-label="" ifx-placeholder-value="Placeholder" :ifx-options="JSON.stringify(options)"></ifx-select>
+
+    <br />
     <br />
   </div>
 </template>
@@ -25,6 +31,8 @@
 import { ref } from 'vue';
 
 let disabled = ref(false);
+let disabled2 = ref(false);
+
 let error = ref(false);
 let errorMessage = ref("Some error");
 
