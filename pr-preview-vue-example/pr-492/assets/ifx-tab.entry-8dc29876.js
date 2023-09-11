5c61bf21d88774007c4057f55513cf9192797ffb
tishoyanchev <tishoyanchev@users.noreply.github.com>
2023-09-11 13:41:53 +0000

    Deploy preview for PR 492 🛫

--- pr-preview-vue-example/pr-492/assets/ifx-tab.entry-8dc29876.js
+++
@@ -1,1 +0,0 @@
-import{r as s,c as t,h as a,g as c}from"./index-70469cba.js";const i=":host{display:block}",r=class{constructor(e){s(this,e),this.tabBecameActive=t(this,"tabBecameActive",7),this.tabBecameInactive=t(this,"tabBecameInactive",7),this.header=void 0}render(){return a("slot",null)}get el(){return c(this)}};r.style=i;export{r as ifx_tab};
