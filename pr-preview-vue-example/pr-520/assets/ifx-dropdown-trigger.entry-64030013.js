73d9f84317d1d3b8974c5ff61c7593f918cd2932
verena-ifx <verena-ifx@users.noreply.github.com>
2023-09-13 19:44:28 +0000

    Deploy preview for PR 520 🛫

---
+++ pr-preview-vue-example/pr-520/assets/ifx-dropdown-trigger.entry-64030013.js
@@ -0,0 +1,1 @@
+import{r as s,h as o}from"./index-287e0877.js";const t=":host{display:inline-block;cursor:pointer}",e=class{constructor(r){s(this,r),this.isOpen=!1}render(){return o("slot",null)}};e.style=t;export{e as ifx_dropdown_trigger};
--- pr-preview-vue-example/pr-520/assets/ifx-tab.entry-b7424108.js
+++
@@ -1,1 +0,0 @@
-import{r as s,c as e,h as a,g as i}from"./index-b1717833.js";const c=":host{display:block}",n=class{constructor(t){s(this,t),this.tabBecameActive=e(this,"tabBecameActive",7),this.tabBecameInactive=e(this,"tabBecameInactive",7),this.header=void 0,this.disabled=void 0}render(){return a("slot",null)}get el(){return i(this)}};n.style=c;export{n as ifx_tab};
