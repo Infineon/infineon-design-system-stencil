33537b8bb4e139f9fceccc6566dcd71589edebf2
verena-ifx <verena-ifx@users.noreply.github.com>
2023-09-14 19:36:47 +0000

    Deploy preview for PR 529 🛫

--- pr-preview-angular-example/pr-529/1116.js
+++ pr-preview-angular-example/pr-529/1116.js
@@ -1,1 +1,1 @@
-"use strict";(self.webpackChunkangular=self.webpackChunkangular||[]).push([[1116],{1116:(e,l,s)=>{s.r(l),s.d(l,{ifx_tab:()=>n});var t=s(2669);const n=class{constructor(a){(0,t.r)(this,a)}render(){return(0,t.h)("slot",null)}get el(){return(0,t.g)(this)}};n.style=":host{display:block}"}}]);
+"use strict";(self.webpackChunkangular=self.webpackChunkangular||[]).push([[1116],{1116:(r,l,s)=>{s.r(l),s.d(l,{ifx_tab:()=>n});var t=s(9525);const n=class{constructor(u){(0,t.r)(this,u)}render(){return(0,t.h)("slot",null)}get el(){return(0,t.g)(this)}};n.style=":host{display:block}"}}]);
