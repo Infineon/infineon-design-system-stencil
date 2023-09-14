f538a21c63e224c135a2dbd8fc7a4b8a5c55fb13
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2023-09-14 21:31:27 +0200

    example apps update

--- examples/wrapper-components/vue-javascript/src/components/IconButton.vue
+++ examples/wrapper-components/vue-javascript/src/components/IconButton.vue
@@ -2,6 +2,7 @@
 
 <template>
   <div>
+    <h2>Icon Button</h2>
     <ifx-icon-button shape="round" variant="primary" icon="calendar16" href="http://google.com" target="" size="s"
       disabled="true"></ifx-icon-button>
   </div>
