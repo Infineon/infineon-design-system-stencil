6135acbee8ad2ab3e8e985160439f303832631eb
tishoyanchev <tihomir.yanchev-EE@infineon.com>
2023-09-14 15:10:58 +0200

    updated storybook

--- packages/components/src/components/footer/footer.stories.ts
+++ packages/components/src/components/footer/footer.stories.ts
@@ -14,27 +14,27 @@ export default {
 
 const DefaultTemplate = () =>
   `<ifx-footer variant="medium">
-    <div slot="socials">
-      <ifx-link href="http://facebook.com/infineon">
-        <ifx-icon icon="c-info-16"></ifx-icon>
-      </ifx-link>
-      <ifx-link href="http://youtube.com/infineon">
-        <ifx-icon icon="c-info-16"></ifx-icon>
-      </ifx-link>
-      <ifx-link href="http://instagram.com/infineon">
-        <ifx-icon icon="c-info-16"></ifx-icon>
-      </ifx-link>
-      <ifx-link href="http://linkedin.com/infineon">
-        <ifx-icon icon="c-info-16"></ifx-icon>
-      </ifx-link>
-      <ifx-link href="http://twitter.com/infineon">
-        <ifx-icon icon="c-info-16"></ifx-icon>
-      </ifx-link>
-      <ifx-link href="http://xing.com/infineon">
-        <ifx-icon icon="c-info-16"></ifx-icon>
-      </ifx-link>
-    </div>
-  </ifx-footer>`;
+  <div slot="socials">
+    <ifx-link variant="title" href="http://facebook.com/infineon">
+      <ifx-icon icon="facebook"></ifx-icon>
+    </ifx-link>
+    <ifx-link variant="title" href="http://youtube.com/infineon">
+      <ifx-icon icon="youtube"></ifx-icon>
+    </ifx-link>
+    <ifx-link variant="title" href="http://instagram.com/infineon">
+      <ifx-icon icon="instagram"></ifx-icon>
+    </ifx-link>
+    <ifx-link variant="title" href="http://linkedin.com/infineon">
+      <ifx-icon icon="linkedin"></ifx-icon>
+    </ifx-link>
+    <ifx-link variant="title" href="http://twitter.com/infineon">
+      <ifx-icon icon="twitter"></ifx-icon>
+    </ifx-link>
+    <ifx-link variant="title" href="http://xing.com/infineon">
+      <ifx-icon icon="xing"></ifx-icon>
+    </ifx-link>
+  </div>
+</ifx-footer>`;
 
 
 export const Medium = DefaultTemplate.bind({});
@@ -46,56 +46,56 @@ const SmallTemplate = () =>
 export const Small = SmallTemplate.bind({});
 
 const LargeTemplate = () =>
-  `<ifx-footer variant="large">
-    <ifx-footer-column slot="col">
-      <h5 slot="title">Title</h5>
-      <ifx-link bold="false" slot="link">Footer Link</ifx-link>
-      <ifx-link bold="false" slot="link">Footer Link</ifx-link>
-      <ifx-link bold="false" slot="link">Footer Link</ifx-link>
-      <ifx-link bold="false" slot="link">Footer Link</ifx-link>
-    </ifx-footer-column>
-    <ifx-footer-column slot="col">
-      <h5 slot="title">Title</h5>
-      <ifx-link bold="false" slot="link">Footer Link</ifx-link>
-      <ifx-link bold="false" slot="link">Footer Link</ifx-link>
-      <ifx-link bold="false" slot="link">Footer Link</ifx-link>
-      <ifx-link bold="false" slot="link">Footer Link</ifx-link>
-    </ifx-footer-column>
-    <ifx-footer-column slot="col">
-      <h5 slot="title">Title</h5>
-      <ifx-link bold="false" slot="link">Footer Link</ifx-link>
-      <ifx-link bold="false" slot="link">Footer Link</ifx-link>
-      <ifx-link bold="false" slot="link">Footer Link</ifx-link>
-      <ifx-link bold="false" slot="link">Footer Link</ifx-link>
-    </ifx-footer-column>
-    <ifx-footer-column slot="col">
-      <h5 slot="title">Title</h5>
-      <ifx-link bold="false" slot="link">Footer Link</ifx-link>
-      <ifx-link bold="false" slot="link">Footer Link</ifx-link>
-      <ifx-link bold="false" slot="link">Footer Link</ifx-link>
-      <ifx-link bold="false" slot="link">Footer Link</ifx-link>
-    </ifx-footer-column>
-    <div slot="socials">
-      <ifx-link href="http://facebook.com/infineon">
-        <ifx-icon icon="c-info-16"></ifx-icon>
-      </ifx-link>
-      <ifx-link href="http://youtube.com/infineon">
-        <ifx-icon icon="calendar-16"></ifx-icon>
-      </ifx-link>
-      <ifx-link href="http://instagram.com/infineon">
-        <ifx-icon icon="fileexe16"></ifx-icon>
-      </ifx-link>
-      <ifx-link href="http://linkedin.com/infineon">
-        <ifx-icon icon="filegif16"></ifx-icon>
-      </ifx-link>
-      <ifx-link href="http://twitter.com/infineon">
-        <ifx-icon icon="filehtml16"></ifx-icon>
-      </ifx-link>
-      <ifx-link href="http://xing.com/infineon">
-        <ifx-icon icon="filejpg16"></ifx-icon>
-      </ifx-link>
-    </div>
-  </ifx-footer>`;
+  `<ifx-footer variant="medium">
+  <ifx-footer-column slot="col">
+    <h5 slot="title">Title</h5>
+    <ifx-link href="#" variant="underlined" slot="link">Footer Link</ifx-link>
+    <ifx-link href="#" variant="underlined" slot="link">Footer Link</ifx-link>
+    <ifx-link href="#" variant="underlined" slot="link">Footer Link</ifx-link>
+    <ifx-link href="#" variant="underlined" slot="link">Footer Link</ifx-link>
+  </ifx-footer-column>
+  <ifx-footer-column slot="col">
+    <h5 slot="title">Title</h5>
+    <ifx-link href="#" variant="underlined" slot="link">Footer Link</ifx-link>
+    <ifx-link href="#" variant="underlined" slot="link">Footer Link</ifx-link>
+    <ifx-link href="#" variant="underlined" slot="link">Footer Link</ifx-link>
+    <ifx-link href="#" variant="underlined" slot="link">Footer Link</ifx-link>
+  </ifx-footer-column>
+  <ifx-footer-column slot="col">
+    <h5 slot="title">Title</h5>
+    <ifx-link href="#" variant="underlined" slot="link">Footer Link</ifx-link>
+    <ifx-link href="#" variant="underlined" slot="link">Footer Link</ifx-link>
+    <ifx-link href="#" variant="underlined" slot="link">Footer Link</ifx-link>
+    <ifx-link href="#" variant="underlined" slot="link">Footer Link</ifx-link>
+  </ifx-footer-column>
+  <ifx-footer-column slot="col">
+    <h5 slot="title">Title</h5>
+    <ifx-link variant="underlined" slot="link">Footer Link</ifx-link>
+    <ifx-link variant="underlined" slot="link">Footer Link</ifx-link>
+    <ifx-link variant="underlined" slot="link">Footer Link</ifx-link>
+    <ifx-link variant="underlined" slot="link">Footer Link</ifx-link>
+  </ifx-footer-column>
+  <div slot="socials">
+    <ifx-link variant="title" href="http://facebook.com/infineon">
+      <ifx-icon icon="facebook"></ifx-icon>
+    </ifx-link>
+    <ifx-link variant="title" href="http://youtube.com/infineon">
+      <ifx-icon icon="youtube"></ifx-icon>
+    </ifx-link>
+    <ifx-link variant="title" href="http://instagram.com/infineon">
+      <ifx-icon icon="instagram"></ifx-icon>
+    </ifx-link>
+    <ifx-link variant="title" href="http://linkedin.com/infineon">
+      <ifx-icon icon="linkedin"></ifx-icon>
+    </ifx-link>
+    <ifx-link variant="title" href="http://twitter.com/infineon">
+      <ifx-icon icon="twitter"></ifx-icon>
+    </ifx-link>
+    <ifx-link variant="title" href="http://xing.com/infineon">
+      <ifx-icon icon="xing"></ifx-icon>
+    </ifx-link>
+  </div>
+</ifx-footer>`;
 
 
 export const Large = LargeTemplate.bind({});
