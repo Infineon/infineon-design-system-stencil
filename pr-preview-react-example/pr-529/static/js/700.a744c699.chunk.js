f2b7dc87b06b96f0d6bc42b6ee94420c03cc74cf
verena-ifx <verena-ifx@users.noreply.github.com>
2023-09-14 19:37:29 +0000

    Deploy preview for PR 529 🛫

---
+++ pr-preview-react-example/pr-529/static/js/700.a744c699.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_javascript=self.webpackChunkreact_javascript||[]).push([[700],{700:function(r,n,t){t.r(n),t.d(n,{ifx_dropdown_separator:function(){return c}});var a=t(5671),e=t(3144),o=t(2204),c=function(){function r(n){(0,a.Z)(this,r),(0,o.r)(this,n)}return(0,e.Z)(r,[{key:"render",value:function(){return(0,o.h)("div",{class:"dropdown-separator"})}}]),r}();c.style=".dropdown-separator{height:1px;background-color:#EEEDED;margin:8px 16px}"}}]);
+//# sourceMappingURL=700.a744c699.chunk.js.map
