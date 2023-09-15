5e3a4d6c4c7987d2d266a6accfd8e61f29d880c2
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2023-09-15 09:08:02 +0200

    added focus state looking the same as hover to avoid having the default browser focus

--- packages/components/src/components/card/card.tsx
+++ packages/components/src/components/card/card.tsx
@@ -16,48 +16,85 @@ export class Card {
   @Prop() target: string = "_self";
 
   @Listen('imgPosition')
-  setImgPosition(event) { 
+  setImgPosition(event) {
     this.alignment = event.detail
   }
 
-  handleComponentAdjustment() { 
+  handleComponentAdjustment() {
     const image = this.el.querySelector('ifx-card-image')
     const links = this.el.querySelector('ifx-card-links')
 
-    if(!image) { 
+    if (!image) {
       this.noImg = true;
     } else this.noImg = false;
 
-    if(!links) { 
+    if (!links) {
       this.noBtns = true;
     } else this.noBtns = false;
   }
 
-  handleHovering() { 
+  handleHovering() {
     const card = this.el.shadowRoot.querySelector('.card')
-    card.addEventListener('mouseover', (ev) => { 
+    card.addEventListener('mouseover', (ev) => {
       this.el.querySelector('ifx-card-headline').isHovered = true;
-      if(ev.target.nodeName === 'IFX-CARD-LINKS' || ev.target.nodeName === 'IFX-BUTTON') { 
+      if (ev.target.nodeName === 'IFX-CARD-LINKS' || ev.target.nodeName === 'IFX-BUTTON') {
         this.el.shadowRoot.querySelector('.card').style.borderColor = '#ebe9e9';
         this.el.querySelector('ifx-card-headline').isHovered = false;
       } else this.el.shadowRoot.querySelector('.card').style.borderColor = '#0A8276';
     })
 
-    card.addEventListener('mouseout', () => { 
+
+    card.addEventListener('mouseout', () => {
       this.el.querySelector('ifx-card-headline').isHovered = false;
       this.el.shadowRoot.querySelector('.card').style.borderColor = '#ebe9e9';
     })
   }
-  
+
   componentWillLoad() {
     this.handleComponentAdjustment()
   }
 
-  componentDidLoad() { 
-    this.handleHovering()
+  componentDidLoad() {
+    this.handleHovering();
+    this.addEventListenersToHandleCustomFocusAndActiveState();
+
   }
 
-  componentWillUpdate() { 
+
+  private addEventListenersToHandleCustomFocusAndActiveState() {
+    const element = this.el.shadowRoot.firstChild;
+    console.log("ev listeners")
+    if (!element) {
+      console.error('element not found');
+      return;
+    }
+    const upperBodyWrapper = element.querySelector('.upper__body-wrapper');
+    if (!upperBodyWrapper) {
+      console.error('upper body wrapper not found');
+      return;
+    }
+
+    element.tabIndex = 0;
+    upperBodyWrapper.tabIndex = 0;
+
+    element.addEventListener('focus', () => {
+      element.classList.add('focus');
+    });
+
+    element.addEventListener('blur', () => {
+      element.classList.remove('focus');
+    });
+    upperBodyWrapper.addEventListener('focus', () => {
+      upperBodyWrapper.classList.add('focus');
+    });
+
+    upperBodyWrapper.addEventListener('blur', () => {
+      upperBodyWrapper.classList.remove('focus');
+    });
+  }
+
+
+  componentWillUpdate() {
     this.handleComponentAdjustment()
   }
 
@@ -70,39 +107,39 @@ export class Card {
           ${this.direction} 
           ${this.alignment}`
         }>
-         
-        {this.direction === 'horizontal' &&
-           <div class="horizontal">
+
+          {this.direction === 'horizontal' &&
+            <div class="horizontal">
               <a class={`card-img ${this.noImg ? 'noImage' : ""}`} href={this.href}>
                 <slot name="img" />
               </a>
 
               <div class='lower__body-wrapper'>
                 <a class='upper-body' href={this.href}>
-                    <slot />
+                  <slot />
                 </a>
                 <div>
                   <slot name='buttons' />
                 </div>
               </div>
-         </div>}
+            </div>}
 
-         {this.direction === 'vertical' && 
-          <div class="vertical">
-            <a class='upper__body-wrapper' href={this.href} target={this.target}>
-              <div class={`card-img ${this.noImg ? 'noImage' : ""}`}>
-                <slot name="img" />
+          {this.direction === 'vertical' &&
+            <div class="vertical">
+              <a class='upper__body-wrapper' href={this.href} target={this.target}>
+                <div class={`card-img ${this.noImg ? 'noImage' : ""}`}>
+                  <slot name="img" />
+                </div>
+
+                <div class='upper-body'>
+                  <slot />
+                </div>
+              </a>
+
+              <div class='lower__body-wrapper'>
+                <slot name='buttons' />
               </div>
-
-              <div class='upper-body'>
-                <slot />
-              </div>
-            </a>
-
-            <div class='lower__body-wrapper'>
-              <slot name='buttons' />
-            </div>
-          </div>}
+            </div>}
         </div>
       </Host>
     );
