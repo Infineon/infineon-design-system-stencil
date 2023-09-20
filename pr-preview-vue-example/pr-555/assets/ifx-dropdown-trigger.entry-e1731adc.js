3383faf54d1be77815551da51d1e714db4ecc7f0
benedikt-ifx <benedikt-ifx@users.noreply.github.com>
2023-09-20 14:31:28 +0000

    Deploy preview for PR 555 🛫

---
+++ pr-preview-vue-example/pr-555/assets/ifx-dropdown-trigger.entry-e1731adc.js
@@ -0,0 +1,1 @@
+import{r as s,h as o}from"./index-d1c90e0e.js";const t=":host{display:inline-block;cursor:pointer}",e=class{constructor(r){s(this,r),this.isOpen=!1}render(){return o("slot",null)}};e.style=t;export{e as ifx_dropdown_trigger};
