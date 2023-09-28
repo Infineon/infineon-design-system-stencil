9b3a8b32b99d2822c395acb88dee82344613c052
radikantchris <144214044+radikantchris@users.noreply.github.com>
2023-09-28 13:57:09 +0000

    Deploying to gh-pages from @ Infineon/infineon-design-system-stencil@c5fdab65f134f7c98e24387231ceb0a56413a0b6 🚀

--- angular-example/1116.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkangular=self.webpackChunkangular||[]).push([[1116],{1116:(r,l,s)=>{s.r(l),s.d(l,{ifx_tab:()=>n});var t=s(5544);const n=class{constructor(u){(0,t.r)(this,u)}render(){return(0,t.h)("slot",null)}get el(){return(0,t.g)(this)}};n.style=":host{display:block}"}}]);
