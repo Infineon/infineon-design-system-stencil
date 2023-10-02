886587be6fcea91f2c8de565cd1a9fd5ad26d1de
tishoyanchev <tihomir.yanchev-EE@infineon.com>
2023-10-02 09:30:02 +0200

    updated border-bottom and aligned the items vertically

--- packages/components/src/components/navbar/navbar.tsx
+++ packages/components/src/components/navbar/navbar.tsx
@@ -193,7 +193,7 @@ export class Navbar {
 
   render() {
     return (
-      <div class="navbar__wrapper">
+      <div class={`navbar__wrapper ${this.fixed ? 'fixed' : ""}`}>
         <div class={`navbar__main-container ${this.fixed ? 'fixed' : ""}`}>
           <div class={`navbar__container ${this.searchBarIsOpen ? "searchOpened" : ""}`}>
             <div class={`navbar__container-left ${this.searchBarIsOpen ? 'searchOpened' : ""}`}>
