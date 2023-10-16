988fac0f4f6bfba2bcbf74304e1b1a38c9ce5dbc
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2023-10-16 14:23:02 +0200

    removed build-utils folder

--- build-utils/constants.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-'use strict';
-
-const util = require('./util');
-exports.ExtensionRootDir = util.ExtensionRootDir;
-exports.isWindows = /^win/.test(process.platform);
-exports.isCI = process.env.TF_BUILD !== undefined || process.env.GITHUB_ACTIONS === 'true';
