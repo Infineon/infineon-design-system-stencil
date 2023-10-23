ec879dd665beab4b3a1d88033d620a18aa41f272
verena-ifx <93577764+verena-ifx@users.noreply.github.com>
2023-10-23 10:43:50 +0000

    Deploying to gh-pages from @ Infineon/infineon-design-system-stencil@c13d94d7d45678ea467666394f5f555874146f41 🚀

--- angular-example/2939.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkangular=self.webpackChunkangular||[]).push([[2939],{2939:(t,o,r)=>{r.r(o),r.d(o,{ifx_dropdown_separator:()=>s});var a=r(5544);const s=class{constructor(d){(0,a.r)(this,d)}render(){return(0,a.h)("div",{class:"dropdown-separator"})}};s.style=".dropdown-separator{height:1px;background-color:#EEEDED;margin:8px 16px}"}}]);
