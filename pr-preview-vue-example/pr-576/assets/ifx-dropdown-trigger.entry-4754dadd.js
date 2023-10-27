f16db80bff26a3a5e6ad168c15806c1ddfb38e2a
tishoyanchev <tishoyanchev@users.noreply.github.com>
2023-10-27 08:00:43 +0000

    Deploy preview for PR 576 🛫

--- pr-preview-vue-example/pr-576/assets/ifx-dropdown-trigger.entry-4754dadd.js
+++
@@ -1,1 +0,0 @@
-import{r as s,h as o}from"./index-41d4f723.js";const t=":host{display:inline-block;cursor:pointer}",e=class{constructor(r){s(this,r),this.isOpen=!1}render(){return o("slot",null)}};e.style=t;export{e as ifx_dropdown_trigger};
