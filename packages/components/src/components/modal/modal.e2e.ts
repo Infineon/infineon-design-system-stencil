97f13a68b7906e1e25dd821a2b3d3114aa4c251f
Yanchev Tihomir (IT ERP DXT / EE) <Tihomir.Yanchev-EE@infineon.com>
2023-10-30 13:16:02 +0100

    updated modal e2e tests to the latest based on modal PR

--- packages/components/src/components/modal/modal.e2e.ts
+++ packages/components/src/components/modal/modal.e2e.ts
@@ -11,28 +11,28 @@ describe('ifx-modal', () => {
 
     // Open the modal
     await modal.callMethod('open');
-  
-    
+    let modalContainerOpen = await page.find('ifx-modal >>> .modal-container');
+    expect(modalContainerOpen).toHaveClass('open');
 
 
     // Close the modal
     await modal.callMethod('close');
-  
+
   });
 
-  it('emits events on open, close, OK button click, and Cancel button click', async () => {
+  it('emits events on open', async () => {
     const page = await newE2EPage();
     await page.setContent('<ifx-modal></ifx-modal>');
 
     const modal = await page.find('ifx-modal');
 
     // Open the modal
-    const openEvent = await modal.spyOnEvent('modalOpen');
+    const openEvent = await modal.spyOnEvent('ifxModalOpen');
     await modal.callMethod('open');
     expect(openEvent).toHaveReceivedEvent();
 
     // Close the modal
-    const closeEvent = await modal.spyOnEvent('modalClose');
+    const closeEvent = await modal.spyOnEvent('ifxModalClose');
     await modal.callMethod('close');
     expect(closeEvent).toHaveReceivedEvent();
 
