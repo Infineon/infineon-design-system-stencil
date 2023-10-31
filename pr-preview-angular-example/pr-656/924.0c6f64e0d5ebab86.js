8fa7a850fe87bc56c61783fcc2b1c90d7ddc1397
verena-ifx <verena-ifx@users.noreply.github.com>
2023-10-31 13:36:31 +0000

    Deploy preview for PR 656 🛫

--- pr-preview-angular-example/pr-656/924.0c6f64e0d5ebab86.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkangular=self.webpackChunkangular||[]).push([[924],{924:(e,n,s)=>{s.r(n),s.d(n,{ifx_dropdown_trigger:()=>o});var r=s(5063);const o=class{constructor(t){(0,r.r)(this,t),this.isOpen=!1}render(){return(0,r.h)("slot",null)}};o.style=":host{display:inline-block;cursor:pointer}"}}]);
