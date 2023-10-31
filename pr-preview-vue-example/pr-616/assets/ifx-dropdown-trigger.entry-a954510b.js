4b44bd306b80187c5482525c430c5f1938b45f6c
verena-ifx <verena-ifx@users.noreply.github.com>
2023-10-31 08:52:52 +0000

    Deploy preview for PR 616 🛫

---
+++ pr-preview-vue-example/pr-616/assets/ifx-dropdown-trigger.entry-a954510b.js
@@ -0,0 +1,1 @@
+import{r as s,h as o}from"./index-855b5a2b.js";const t=":host{display:inline-block;cursor:pointer}",e=class{constructor(r){s(this,r),this.isOpen=!1}render(){return o("slot",null)}};e.style=t;export{e as ifx_dropdown_trigger};
