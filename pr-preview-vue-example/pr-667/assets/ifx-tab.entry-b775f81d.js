98fcc27d138334c76d5174af165bcc42298bd1d8
tishoyanchev <tishoyanchev@users.noreply.github.com>
2023-11-02 14:32:33 +0000

    Deploy preview for PR 667 🛫

---
+++ pr-preview-vue-example/pr-667/assets/ifx-tab.entry-b775f81d.js
@@ -0,0 +1,1 @@
+import{r as s,h as e,g as r}from"./index-80eae1a3.js";const n=":host{display:block}",o=class{constructor(t){s(this,t)}render(){return e("slot",null)}get el(){return r(this)}};o.style=n;export{o as ifx_tab};
