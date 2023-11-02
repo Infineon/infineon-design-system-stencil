4f3da03de6a8ed0dcacd07d00ef1e4b44caacf21
verena-ifx <93577764+verena-ifx@users.noreply.github.com>
2023-11-02 20:51:00 +0000

    Deploying to gh-pages from @ Infineon/infineon-design-system-stencil@1ef006ef129b2e421f55264442342d537ee59dba 🚀

--- vue-example/assets/ifx-dropdown-separator.entry-bb9252c4.js
+++
@@ -1,1 +0,0 @@
-import{r as o,h as s}from"./index-f2d8f0f9.js";const a=".dropdown-separator{height:1px;background-color:#EEEDED;margin:8px 16px}",t=class{constructor(r){o(this,r)}render(){return s("div",{class:"dropdown-separator"})}};t.style=a;export{t as ifx_dropdown_separator};
