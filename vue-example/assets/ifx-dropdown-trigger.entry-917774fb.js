27c5dba34e6a13dbdd71db5c65710c897e3fb817
tishoyanchev <108734092+tishoyanchev@users.noreply.github.com>
2023-11-03 14:34:16 +0000

    Deploying to gh-pages from @ Infineon/infineon-design-system-stencil@4ab380a7efc1db99137a2f60d9fc1f7f707ab6e9 🚀

---
+++ vue-example/assets/ifx-dropdown-trigger.entry-917774fb.js
@@ -0,0 +1,1 @@
+import{r as s,h as o}from"./index-4c8977d1.js";const t=":host{display:inline-block;cursor:pointer}",e=class{constructor(r){s(this,r),this.isOpen=!1}render(){return o("slot",null)}};e.style=t;export{e as ifx_dropdown_trigger};
