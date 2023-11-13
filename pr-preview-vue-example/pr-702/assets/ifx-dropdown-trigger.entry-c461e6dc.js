8653eda890d98112400623cecaffba02703e3713
tishoyanchev <tishoyanchev@users.noreply.github.com>
2023-11-13 13:16:22 +0000

    Deploy preview for PR 702 🛫

---
+++ pr-preview-vue-example/pr-702/assets/ifx-dropdown-trigger.entry-c461e6dc.js
@@ -0,0 +1,1 @@
+import{r as s,h as o}from"./index-c8c4b7f0.js";const t=":host{display:inline-block;cursor:pointer}",e=class{constructor(r){s(this,r),this.isOpen=!1}render(){return o("slot",null)}};e.style=t;export{e as ifx_dropdown_trigger};
