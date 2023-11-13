ade7228bd486e5d12b649e662449091283dc02d0
tishoyanchev <108734092+tishoyanchev@users.noreply.github.com>
2023-11-13 19:46:22 +0000

    Deploying to gh-pages from @ Infineon/infineon-design-system-stencil@3854d9d74e802afa45ca0832470c9f619c0443c0 🚀

--- vue-example/assets/ifx-tab.entry-fc716097.js
+++
@@ -1,1 +0,0 @@
-import{r as s,h as e,g as r}from"./index-291380ea.js";const n=":host{display:block}",o=class{constructor(t){s(this,t)}render(){return e("slot",null)}get el(){return r(this)}};o.style=n;export{o as ifx_tab};
