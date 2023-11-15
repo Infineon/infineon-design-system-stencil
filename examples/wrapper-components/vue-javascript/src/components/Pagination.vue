2d37e761d1eeffacf0017ddec789f369c32d76b0
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2023-11-15 09:45:15 +0100

    pagination event handling demonstration

--- examples/wrapper-components/vue-javascript/src/components/Pagination.vue
+++ examples/wrapper-components/vue-javascript/src/components/Pagination.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
     <h2>Pagination</h2>
-    <ifx-pagination total="500" current-page="1"></ifx-pagination>
+    <ifx-pagination @ifxPageChange="handleChange" total="500" :current-page="current"></ifx-pagination>
   </div>
 </template>
 
 <script setup>
+import { computed, onMounted, ref } from 'vue'
 
+const currentPage = ref(1);
+
+const current = computed(() => {
+  return currentPage.value;
+});
+
+onMounted(() => {
+  updatePage();
+  setInterval(updatePage, 10000);
+})
+
+function handleChange() {
+  console.log("page value changed");
+}
+
+function updatePage() {
+  console.log("updating page value")
+  currentPage.value < 500 ? currentPage.value += 1 : currentPage.value = 1;
+
+}
 </script>
 
 
