cf635bb629dede71fadeb7df694cae714f5be4ae
Yanchev Tihomir (IT ERP DXT / EE) <Tihomir.Yanchev-EE@infineon.com>
2023-11-28 15:55:05 +0100

    added sidebar v-2 to example apps

--- examples/wrapper-components/react-typescript/src/components/Sidebar/Sidebar.tsx
+++ examples/wrapper-components/react-typescript/src/components/Sidebar/Sidebar.tsx
@@ -1,15 +1,120 @@
-
+import { useEffect, useRef } from 'react';
 import { IfxSidebar, IfxSidebarItem } from '@infineon/infineon-design-system-react';
 
 function Sidebar() {
+  const Sidebar = useRef(null);
+
+  useEffect(() => {
+    const sidebarRef = Sidebar.current;
+    const handleSidebarItem = (e) => {
+      e.detail.component.setAttribute('active', 'true')
+      if(e.detail.value === 'thisPage') { 
+        //do something
+      }
+    };
+     
+    sidebarRef.addEventListener('ifxSidebar', handleSidebarItem);
+  
+    return () => {
+      if (sidebarRef.current) {
+        sidebarRef.current.removeEventListener('ifxSidebar', handleSidebarItem);
+      }
+    };
+  }, []);
+
+
+
   return (
     <div >
-      <IfxSidebar>
-        <IfxSidebarItem href="http://google.com" target="_blank" icon="image-16">Item One</IfxSidebarItem>
-        <IfxSidebarItem href="http://google.com" target="_blank" icon="image-16">Item Two</IfxSidebarItem>
-        <IfxSidebarItem href="http://google.com" target="_blank" icon="image-16">Item Three</IfxSidebarItem>
-        <IfxSidebarItem href="http://google.com" target="_blank" icon="image-16">Item Four</IfxSidebarItem>
-      </IfxSidebar>
+        <IfxSidebar application-name="Application name"  ref={Sidebar}>
+        <IfxSidebarItem active="false">
+          Header Section
+          <IfxSidebarItem icon="image-16" active="false" >
+            Second layer
+            <IfxSidebarItem target="_blank" active={false} icon="image-16">
+            3rd layer Menu Item
+            </IfxSidebarItem>
+
+            <IfxSidebarItem target="_blank" active="false" icon="image-16" value="thisPage">
+            This Page
+            </IfxSidebarItem>
+
+            <IfxSidebarItem target="_blank" active="false" icon="image-16">
+            3rd layer Menu Item
+            </IfxSidebarItem>
+          </IfxSidebarItem>
+
+          <IfxSidebarItem icon="image-16" active="false">
+            Second layer
+            <IfxSidebarItem target="_blank" icon="image-16" active="false">
+            Menu Item
+            </IfxSidebarItem>
+          </IfxSidebarItem>
+
+          <IfxSidebarItem icon="image-16" active="false">
+            Second layer
+            <IfxSidebarItem target="_blank" icon="image-16" active="false">
+            Menu Item
+            </IfxSidebarItem>
+          </IfxSidebarItem>
+        </IfxSidebarItem>
+        
+        <IfxSidebarItem active="false">
+        Header Section
+        <IfxSidebarItem icon="image-16" active="false">
+          Menu Item
+          <IfxSidebarItem active="false" target="_blank" icon="image-16">
+          Sub Menu Item
+          </IfxSidebarItem>
+          <IfxSidebarItem target="_blank" icon="image-16" value="thisPage">
+          This one too
+          </IfxSidebarItem>
+          <IfxSidebarItem target="_blank" icon="image-16">
+          Menu Item
+          </IfxSidebarItem>
+        </IfxSidebarItem>
+        <IfxSidebarItem icon="image-16">
+          Menu Item
+          <IfxSidebarItem target="_blank" icon="image-16">
+          Menu Item
+          </IfxSidebarItem>
+        </IfxSidebarItem>
+        <IfxSidebarItem icon="image-16">
+          Menu Item
+          <IfxSidebarItem target="_blank" icon="image-16">
+          Menu Item
+          </IfxSidebarItem>
+        </IfxSidebarItem>
+        </IfxSidebarItem>
+
+        <IfxSidebarItem>
+        Header Section
+        <IfxSidebarItem icon="image-16">
+          Menu Item
+          <IfxSidebarItem href="http://google.com" target="_blank" icon="image-16">
+          Menu Item
+          </IfxSidebarItem>
+          <IfxSidebarItem href="http://google.com" target="_blank" icon="image-16">
+          Menu Item
+          </IfxSidebarItem>
+          <IfxSidebarItem href="http://google.com" target="_blank" icon="image-16">
+          Menu Item
+          </IfxSidebarItem>
+        </IfxSidebarItem>
+        <IfxSidebarItem icon="image-16">
+          Menu Item
+          <IfxSidebarItem href="http://google.com" target="_blank" icon="image-16">
+          Menu Item
+          </IfxSidebarItem>
+        </IfxSidebarItem>
+        <IfxSidebarItem icon="image-16">
+          Menu Item
+          <IfxSidebarItem href="http://google.com" target="_blank" icon="image-16">
+          Menu Item
+          </IfxSidebarItem>
+        </IfxSidebarItem>
+        </IfxSidebarItem>
+    </IfxSidebar>
     </div>
   )
 }
--- examples/wrapper-components/vue-typescript/src/components/Sidebar.vue
+++ examples/wrapper-components/vue-typescript/src/components/Sidebar.vue
@@ -1,16 +1,109 @@
 <template>
   <div class="app">
-    <ifx-sidebar application-name="This is a very long application">
-    <ifx-sidebar-item href="http://google.com" target="_blank" icon="image-16">Item One</ifx-sidebar-item>
-    <ifx-sidebar-item href="http://yahoo.com" target="_blank" icon="image-16">Item Two</ifx-sidebar-item>
-    <ifx-sidebar-item href="http://bing.com" target="_blank" icon="image-16">Item Three</ifx-sidebar-item>
-    <ifx-sidebar-item href="http://naver.com" target="_blank" icon="image-16">Item Four</ifx-sidebar-item>
-  </ifx-sidebar>
+    <ifx-sidebar application-name="Application name"  id="sidebar">
+      <ifx-sidebar-item active="false">
+        Header Section
+        <ifx-sidebar-item icon="image-16" active="false" >
+          Second layer
+          <ifx-sidebar-item target="_blank" active="true" icon="image-16">
+          3rd layer Menu Item
+          </ifx-sidebar-item>
+
+          <ifx-sidebar-item target="_blank" active="false" icon="image-16" value="thisPage">
+          This Page
+          </ifx-sidebar-item>
+
+          <ifx-sidebar-item target="_blank" active="false" icon="image-16">
+          3rd layer Menu Item
+          </ifx-sidebar-item>
+        </ifx-sidebar-item>
+
+        <ifx-sidebar-item icon="image-16" active="false">
+          Second layer
+          <ifx-sidebar-item target="_blank" icon="image-16" active="false">
+          Menu Item
+          </ifx-sidebar-item>
+        </ifx-sidebar-item>
+
+        <ifx-sidebar-item icon="image-16" active="false">
+          Second layer
+          <ifx-sidebar-item target="_blank" icon="image-16" active="false">
+          Menu Item
+          </ifx-sidebar-item>
+        </ifx-sidebar-item>
+      </ifx-sidebar-item>
+      
+      <!-- header section -->
+      <ifx-sidebar-item active="false">
+      Header Section
+      <ifx-sidebar-item icon="image-16" active="false">
+        Menu Item
+        <ifx-sidebar-item active="false" target="_blank" icon="image-16">
+        Sub Menu Item
+        </ifx-sidebar-item>
+        <ifx-sidebar-item target="_blank" icon="image-16" value="thisPage">
+        This one too
+        </ifx-sidebar-item>
+        <ifx-sidebar-item target="_blank" icon="image-16">
+        Menu Item
+        </ifx-sidebar-item>
+      </ifx-sidebar-item>
+      <ifx-sidebar-item icon="image-16">
+        Menu Item
+        <ifx-sidebar-item target="_blank" icon="image-16">
+        Menu Item
+        </ifx-sidebar-item>
+      </ifx-sidebar-item>
+      <ifx-sidebar-item icon="image-16">
+        Menu Item
+        <ifx-sidebar-item target="_blank" icon="image-16">
+        Menu Item
+        </ifx-sidebar-item>
+      </ifx-sidebar-item>
+      </ifx-sidebar-item>
+
+      <!-- header section -->
+      <ifx-sidebar-item>
+      Header Section
+      <ifx-sidebar-item icon="image-16">
+        Menu Item
+        <ifx-sidebar-item href="http://google.com" target="_blank" icon="image-16">
+        Menu Item
+        </ifx-sidebar-item>
+        <ifx-sidebar-item href="http://google.com" target="_blank" icon="image-16">
+        Menu Item
+        </ifx-sidebar-item>
+        <ifx-sidebar-item href="http://google.com" target="_blank" icon="image-16">
+        Menu Item
+        </ifx-sidebar-item>
+      </ifx-sidebar-item>
+      <ifx-sidebar-item icon="image-16">
+        Menu Item
+        <ifx-sidebar-item href="http://google.com" target="_blank" icon="image-16">
+        Menu Item
+        </ifx-sidebar-item>
+      </ifx-sidebar-item>
+      <ifx-sidebar-item icon="image-16">
+        Menu Item
+        <ifx-sidebar-item href="http://google.com" target="_blank" icon="image-16">
+        Menu Item
+        </ifx-sidebar-item>
+      </ifx-sidebar-item>
+      </ifx-sidebar-item>
+    </ifx-sidebar>
   </div>
 </template>
 
 <script setup>
-
+    window.addEventListener('load', () => { 
+      const sidebar = document.getElementById('sidebar')
+      sidebar.addEventListener('ifxSidebar', (e) => { 
+        if(e.detail.value === 'thisPage') { 
+          e.detail.component.setAttribute('active', 'true')
+          //do something
+        }
+      })
+    })
 
 
 </script>
