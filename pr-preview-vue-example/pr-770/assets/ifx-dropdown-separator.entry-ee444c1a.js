72f1a0b22934c2d586f934a29ed64bee788b2ee5
verena-ifx <verena-ifx@users.noreply.github.com>
2023-11-29 14:52:55 +0000

    Deploy preview for PR 770 🛫

---
+++ pr-preview-vue-example/pr-770/assets/ifx-dropdown-separator.entry-ee444c1a.js
@@ -0,0 +1,1 @@
+import{r as o,h as s}from"./index-d9279dbe.js";const a=".dropdown-separator{height:1px;background-color:#EEEDED;margin:8px 16px}",t=class{constructor(r){o(this,r)}render(){return s("div",{class:"dropdown-separator"})}};t.style=a;export{t as ifx_dropdown_separator};
