75c2d298a390787d6fcab7483c43893931ebec05
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2023-11-30 10:30:35 +0100

    vue example app updated

--- examples/wrapper-components/vue-javascript/src/components/SearchField.vue
+++ examples/wrapper-components/vue-javascript/src/components/SearchField.vue
@@ -1,26 +1,27 @@
 <template>
   <div>
-    <h2>Search-Field</h2>
+    <h2>Search-Field (v-model)</h2>
     <ifx-search-field v-model="searchInput" disabled="false" size='m' show-delete-icon="true"></ifx-search-field>
-    <p>Search field: {{ searchInput }}</p>
+    <p>Search field 1: {{ searchInput }}</p>
+    <h2>Search-Field (manual combination of value binding + event listener)</h2>
+    <ifx-search-field @ifxInput="handleSearchInput" :value="searchValue" disabled="false" size='m'
+      show-delete-icon="true"></ifx-search-field>
+    <p>Search field 2: {{ searchValue }}</p>
+
     <br />
     <br />
   </div>
 </template>
 
 <script  setup>
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 const searchInput = ref('');
-
-onMounted(() => {
-  updateSearchBarAndSearchInput();
-  setInterval(updateSearchBarAndSearchInput, 50000);
-})
+let searchValue = ref('');
 
 
-function updateSearchBarAndSearchInput() {
-  console.log("updating search field")
-  searchInput.value = searchInput.value + "+1";
+function handleSearchInput(event) {
+  console.log("updating search field 2: ", event.detail)
+  searchValue.value = event.detail;
 }
 
 </script>
