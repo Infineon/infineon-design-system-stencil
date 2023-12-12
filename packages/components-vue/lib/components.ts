01ba296a2556f586df3f2f60ca52b978898a6156
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2023-12-12 17:58:21 +0100

    wd cleaned

--- packages/components-vue/lib/components.ts
+++ packages/components-vue/lib/components.ts
@@ -5,9 +5,8 @@ import { defineContainer } from './vue-component-lib/utils';
 
 import type { JSX } from '@infineon/infineon-design-system-stencil';
 
-import { defineCustomElements } from '@infineon/infineon-design-system-stencil/loader';
 
-defineCustomElements();
+
 
 export const IfxAccordion = /*@__PURE__*/ defineContainer<JSX.IfxAccordion>('ifx-accordion', undefined, [
   'autoCollapse'
