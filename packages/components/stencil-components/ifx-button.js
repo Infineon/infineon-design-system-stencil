496d9dc8d926d0a37a5e6079b9c0147de75c678b
Yanchev Tihomir (IT ERP DXT / EE) <Tihomir.Yanchev-EE@infineon.com>
2023-12-20 09:41:53 +0100

    ran git rm -r stencil-components in packages/components

--- packages/components/stencil-components/ifx-button.js
+++
@@ -1,8 +0,0 @@
-import { B as Button, d as defineCustomElement$1 } from './button.js';
-
-const IfxButton = Button;
-const defineCustomElement = defineCustomElement$1;
-
-export { IfxButton, defineCustomElement };
-
-//# sourceMappingURL=ifx-button.js.map
