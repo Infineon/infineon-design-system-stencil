3151a8f08289021e0a95d86b3949ec401ead30b4
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2024-01-18 19:13:57 +0100

    font issue fixed for storybook, started to add theme (as solution for font-issue)

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -8,6 +8,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    '@storybook/addon-docs/web-components',
     {
       name: '@storybook/addon-docs',
       options: {
