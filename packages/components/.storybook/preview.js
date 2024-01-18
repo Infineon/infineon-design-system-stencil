23fcb7102b6307712f4fcd7119b759bbfbaa05f8
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2024-01-18 16:20:58 +0100

    workaround to set css custom property for storybook

--- packages/components/.storybook/preview.js
+++ packages/components/.storybook/preview.js
@@ -3,6 +3,9 @@ import { defineCustomElements } from '../loader';
 import prettier from 'prettier/standalone';
 import prettierBabel from 'prettier/parser-babel';
 
+//since font is defined via a CSS custom property that can be changed by the user, and storybook cant read those properties when defined via the :root selector, it needs to be set here
+export const decorators = [(Story) => `<div style="--ifx-font-family: 'Source Sans 3';">${Story().outerHTML || Story()}</div>`];
+
 defineCustomElements();
 
 export const parameters = {
