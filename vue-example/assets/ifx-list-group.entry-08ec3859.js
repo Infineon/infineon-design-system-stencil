94f2c2b416c43c145f860ce0cc623d77d2a82c3d
tishoyanchev <108734092+tishoyanchev@users.noreply.github.com>
2024-01-18 11:03:46 +0000

    Deploying to gh-pages from @ Infineon/infineon-design-system-stencil@3a230a359e3af9499c1d046796e48531bbff1732 🚀

--- vue-example/assets/ifx-list-group.entry-08ec3859.js
+++
@@ -1,3 +0,0 @@
-import{r as i,h as t,H as l,g as e}from"./index-394fadac.js";const n=":host{display:inline-flex}.list-group-container{display:inline-flex;flex-direction:column;gap:3px}",o=class{constructor(s){i(this,s),this.flush=!1,this.bulletpoint=!1,this.notification=!1}render(){return t(l,null,t("div",{class:`list-group-container 
-        ${this.flush?"flush":""} 
-        ${this.bulletpoint?"bulletpoint":""}`},t("slot",{name:"list-item"}),t("slot",{name:"list-notification"})))}get el(){return e(this)}};o.style=n;export{o as ifx_list_group};
