c28b8151bb29d7ca0767646505958140e30f5a5a
Akash Yeole <akashyeole02@gmail.com>
2024-01-19 14:57:11 +0530

    Added closable property to the alert component

--- packages/components-vue/lib/components.ts
+++ packages/components-vue/lib/components.ts
@@ -25,6 +25,7 @@ export const IfxAccordionItem = /*@__PURE__*/ defineContainer<JSX.IfxAccordionIt
 export const IfxAlert = /*@__PURE__*/ defineContainer<JSX.IfxAlert>('ifx-alert', undefined, [
   'variant',
   'icon',
+  'closable',
   'ifxClose'
 ]);
 
