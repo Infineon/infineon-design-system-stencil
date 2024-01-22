a5b4c7ff891f61a562abed279fff62b0ba6b26f4
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2024-01-22 17:08:31 +0100

    re-added index.ts to react wrapper library

---
+++ packages/components-react/lib/components/stencil-generated/index.ts
@@ -0,0 +1,70 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@infineon/infineon-design-system-stencil';
+
+
+
+export const IfxAccordion = /*@__PURE__*/createReactComponent<JSX.IfxAccordion, HTMLIfxAccordionElement>('ifx-accordion');
+export const IfxAccordionItem = /*@__PURE__*/createReactComponent<JSX.IfxAccordionItem, HTMLIfxAccordionItemElement>('ifx-accordion-item');
+export const IfxAlert = /*@__PURE__*/createReactComponent<JSX.IfxAlert, HTMLIfxAlertElement>('ifx-alert');
+export const IfxBadge = /*@__PURE__*/createReactComponent<JSX.IfxBadge, HTMLIfxBadgeElement>('ifx-badge');
+export const IfxBasicTable = /*@__PURE__*/createReactComponent<JSX.IfxBasicTable, HTMLIfxBasicTableElement>('ifx-basic-table');
+export const IfxBreadcrumb = /*@__PURE__*/createReactComponent<JSX.IfxBreadcrumb, HTMLIfxBreadcrumbElement>('ifx-breadcrumb');
+export const IfxBreadcrumbItem = /*@__PURE__*/createReactComponent<JSX.IfxBreadcrumbItem, HTMLIfxBreadcrumbItemElement>('ifx-breadcrumb-item');
+export const IfxBreadcrumbItemLabel = /*@__PURE__*/createReactComponent<JSX.IfxBreadcrumbItemLabel, HTMLIfxBreadcrumbItemLabelElement>('ifx-breadcrumb-item-label');
+export const IfxButton = /*@__PURE__*/createReactComponent<JSX.IfxButton, HTMLIfxButtonElement>('ifx-button');
+export const IfxCard = /*@__PURE__*/createReactComponent<JSX.IfxCard, HTMLIfxCardElement>('ifx-card');
+export const IfxCardHeadline = /*@__PURE__*/createReactComponent<JSX.IfxCardHeadline, HTMLIfxCardHeadlineElement>('ifx-card-headline');
+export const IfxCardImage = /*@__PURE__*/createReactComponent<JSX.IfxCardImage, HTMLIfxCardImageElement>('ifx-card-image');
+export const IfxCardLinks = /*@__PURE__*/createReactComponent<JSX.IfxCardLinks, HTMLIfxCardLinksElement>('ifx-card-links');
+export const IfxCardOverline = /*@__PURE__*/createReactComponent<JSX.IfxCardOverline, HTMLIfxCardOverlineElement>('ifx-card-overline');
+export const IfxCardText = /*@__PURE__*/createReactComponent<JSX.IfxCardText, HTMLIfxCardTextElement>('ifx-card-text');
+export const IfxCheckbox = /*@__PURE__*/createReactComponent<JSX.IfxCheckbox, HTMLIfxCheckboxElement>('ifx-checkbox');
+export const IfxChip = /*@__PURE__*/createReactComponent<JSX.IfxChip, HTMLIfxChipElement>('ifx-chip');
+export const IfxDropdown = /*@__PURE__*/createReactComponent<JSX.IfxDropdown, HTMLIfxDropdownElement>('ifx-dropdown');
+export const IfxDropdownHeader = /*@__PURE__*/createReactComponent<JSX.IfxDropdownHeader, HTMLIfxDropdownHeaderElement>('ifx-dropdown-header');
+export const IfxDropdownItem = /*@__PURE__*/createReactComponent<JSX.IfxDropdownItem, HTMLIfxDropdownItemElement>('ifx-dropdown-item');
+export const IfxDropdownMenu = /*@__PURE__*/createReactComponent<JSX.IfxDropdownMenu, HTMLIfxDropdownMenuElement>('ifx-dropdown-menu');
+export const IfxDropdownSeparator = /*@__PURE__*/createReactComponent<JSX.IfxDropdownSeparator, HTMLIfxDropdownSeparatorElement>('ifx-dropdown-separator');
+export const IfxDropdownTrigger = /*@__PURE__*/createReactComponent<JSX.IfxDropdownTrigger, HTMLIfxDropdownTriggerElement>('ifx-dropdown-trigger');
+export const IfxDropdownTriggerButton = /*@__PURE__*/createReactComponent<JSX.IfxDropdownTriggerButton, HTMLIfxDropdownTriggerButtonElement>('ifx-dropdown-trigger-button');
+export const IfxFaq = /*@__PURE__*/createReactComponent<JSX.IfxFaq, HTMLIfxFaqElement>('ifx-faq');
+export const IfxFooter = /*@__PURE__*/createReactComponent<JSX.IfxFooter, HTMLIfxFooterElement>('ifx-footer');
+export const IfxFooterColumn = /*@__PURE__*/createReactComponent<JSX.IfxFooterColumn, HTMLIfxFooterColumnElement>('ifx-footer-column');
+export const IfxIcon = /*@__PURE__*/createReactComponent<JSX.IfxIcon, HTMLIfxIconElement>('ifx-icon');
+export const IfxIconButton = /*@__PURE__*/createReactComponent<JSX.IfxIconButton, HTMLIfxIconButtonElement>('ifx-icon-button');
+export const IfxIconsPreview = /*@__PURE__*/createReactComponent<JSX.IfxIconsPreview, HTMLIfxIconsPreviewElement>('ifx-icons-preview');
+export const IfxLink = /*@__PURE__*/createReactComponent<JSX.IfxLink, HTMLIfxLinkElement>('ifx-link');
+export const IfxListGroup = /*@__PURE__*/createReactComponent<JSX.IfxListGroup, HTMLIfxListGroupElement>('ifx-list-group');
+export const IfxListItem = /*@__PURE__*/createReactComponent<JSX.IfxListItem, HTMLIfxListItemElement>('ifx-list-item');
+export const IfxListNotification = /*@__PURE__*/createReactComponent<JSX.IfxListNotification, HTMLIfxListNotificationElement>('ifx-list-notification');
+export const IfxModal = /*@__PURE__*/createReactComponent<JSX.IfxModal, HTMLIfxModalElement>('ifx-modal');
+export const IfxMultiselect = /*@__PURE__*/createReactComponent<JSX.IfxMultiselect, HTMLIfxMultiselectElement>('ifx-multiselect');
+export const IfxNavbar = /*@__PURE__*/createReactComponent<JSX.IfxNavbar, HTMLIfxNavbarElement>('ifx-navbar');
+export const IfxNavbarItem = /*@__PURE__*/createReactComponent<JSX.IfxNavbarItem, HTMLIfxNavbarItemElement>('ifx-navbar-item');
+export const IfxNavbarMenu = /*@__PURE__*/createReactComponent<JSX.IfxNavbarMenu, HTMLIfxNavbarMenuElement>('ifx-navbar-menu');
+export const IfxNavbarMenuItem = /*@__PURE__*/createReactComponent<JSX.IfxNavbarMenuItem, HTMLIfxNavbarMenuItemElement>('ifx-navbar-menu-item');
+export const IfxNumberIndicator = /*@__PURE__*/createReactComponent<JSX.IfxNumberIndicator, HTMLIfxNumberIndicatorElement>('ifx-number-indicator');
+export const IfxPagination = /*@__PURE__*/createReactComponent<JSX.IfxPagination, HTMLIfxPaginationElement>('ifx-pagination');
+export const IfxProgressBar = /*@__PURE__*/createReactComponent<JSX.IfxProgressBar, HTMLIfxProgressBarElement>('ifx-progress-bar');
+export const IfxRadioButton = /*@__PURE__*/createReactComponent<JSX.IfxRadioButton, HTMLIfxRadioButtonElement>('ifx-radio-button');
+export const IfxSearchBar = /*@__PURE__*/createReactComponent<JSX.IfxSearchBar, HTMLIfxSearchBarElement>('ifx-search-bar');
+export const IfxSearchField = /*@__PURE__*/createReactComponent<JSX.IfxSearchField, HTMLIfxSearchFieldElement>('ifx-search-field');
+export const IfxSelect = /*@__PURE__*/createReactComponent<JSX.IfxSelect, HTMLIfxSelectElement>('ifx-select');
+export const IfxSidebar = /*@__PURE__*/createReactComponent<JSX.IfxSidebar, HTMLIfxSidebarElement>('ifx-sidebar');
+export const IfxSidebarItem = /*@__PURE__*/createReactComponent<JSX.IfxSidebarItem, HTMLIfxSidebarItemElement>('ifx-sidebar-item');
+export const IfxSlider = /*@__PURE__*/createReactComponent<JSX.IfxSlider, HTMLIfxSliderElement>('ifx-slider');
+export const IfxSpinner = /*@__PURE__*/createReactComponent<JSX.IfxSpinner, HTMLIfxSpinnerElement>('ifx-spinner');
+export const IfxStatus = /*@__PURE__*/createReactComponent<JSX.IfxStatus, HTMLIfxStatusElement>('ifx-status');
+export const IfxStep = /*@__PURE__*/createReactComponent<JSX.IfxStep, HTMLIfxStepElement>('ifx-step');
+export const IfxStepper = /*@__PURE__*/createReactComponent<JSX.IfxStepper, HTMLIfxStepperElement>('ifx-stepper');
+export const IfxSwitch = /*@__PURE__*/createReactComponent<JSX.IfxSwitch, HTMLIfxSwitchElement>('ifx-switch');
+export const IfxTab = /*@__PURE__*/createReactComponent<JSX.IfxTab, HTMLIfxTabElement>('ifx-tab');
+export const IfxTable = /*@__PURE__*/createReactComponent<JSX.IfxTable, HTMLIfxTableElement>('ifx-table');
+export const IfxTabs = /*@__PURE__*/createReactComponent<JSX.IfxTabs, HTMLIfxTabsElement>('ifx-tabs');
+export const IfxTag = /*@__PURE__*/createReactComponent<JSX.IfxTag, HTMLIfxTagElement>('ifx-tag');
+export const IfxTextField = /*@__PURE__*/createReactComponent<JSX.IfxTextField, HTMLIfxTextFieldElement>('ifx-text-field');
+export const IfxTooltip = /*@__PURE__*/createReactComponent<JSX.IfxTooltip, HTMLIfxTooltipElement>('ifx-tooltip');
