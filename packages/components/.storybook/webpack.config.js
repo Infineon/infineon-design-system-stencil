2a77951977ef1c32d8883d1747d6ab299e533b7e
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2024-01-29 12:16:20 +0100

    config update for storybook

--- packages/components/.storybook/webpack.config.js
+++ packages/components/.storybook/webpack.config.js
@@ -1,10 +1,17 @@
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.scss$/,
     use: ['style-loader', 'css-loader', 'sass-loader'],
+    resolve: {
+      alias: {
+        // This tells Webpack to resolve "~" to the node_modules directory
+        '~': path.resolve(__dirname, 'node_modules'),
+      },
+    },
   });
+
 
 
   return config;
