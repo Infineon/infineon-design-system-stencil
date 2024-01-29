6a8ab2154a6481b7994cd18e8f16fd8068363651
verena-ifx <lechneve@ISCN5CG1201S7M.infineon.com>
2024-01-29 16:13:10 +0100

    webpack updated with file-loader for fonts

--- packages/components/.storybook/webpack.config.js
+++ packages/components/.storybook/webpack.config.js
@@ -13,6 +13,10 @@ module.exports = ({ config }) => {
     },
   });
 
+  config.module.rules.push({
+    test: /\.(woff|woff2|eot|ttf|otf)$/,
+    use: ['file-loader'],
+  });
 
   config.plugins.push(
     new CopyPlugin({
