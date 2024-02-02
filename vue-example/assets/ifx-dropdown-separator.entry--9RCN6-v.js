d1882fb75d4a61799febf6d158184250668fe333
tishoyanchev <108734092+tishoyanchev@users.noreply.github.com>
2024-02-02 14:45:59 +0000

    Deploying to gh-pages from @ Infineon/infineon-design-system-stencil@d5c4116b3acd4db2a637eda524bf9b1a665d851b 🚀

--- vue-example/assets/ifx-dropdown-separator.entry--9RCN6-v.js
+++
@@ -1,1 +0,0 @@
-import{r as o,h as s}from"./index-qhCF7uI5.js";const a=".dropdown-separator{height:1px;background-color:#EEEDED;margin:8px 16px}",t=class{constructor(r){o(this,r)}render(){return s("div",{class:"dropdown-separator"})}};t.style=a;export{t as ifx_dropdown_separator};
