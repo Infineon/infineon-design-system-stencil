65557ae4e58fcab4bceb1a13dd6f587cfec04f9f
Yanchev Tihomir (IT ERP DXT / EE) <Tihomir.Yanchev-EE@infineon.com>
2024-02-02 08:59:26 +0100

    next is adjusting the arrows

--- packages/components/src/components/navbar/navbar-item.tsx
+++ packages/components/src/components/navbar/navbar-item.tsx
@@ -43,24 +43,12 @@ export class NavbarItem {
   }
 
   componentDidLoad() { 
-    //this.setInternalContent()
-    //this.appendNavItemToMenu()
     if(this.hasChildNavItems) { 
       const navItems = this.getNavbarItems();
       this.appendNavItemToMenu(navItems)
     }
   }
 
-  // @Listen('mousedown', { target: 'document' })
-  // handleOutsideClick(event: MouseEvent) {
-  //   const target = event.target as HTMLElement;
-
-  //   if (NavbarItem.currentlyOpen.length > 0 && !NavbarItem.currentlyOpen.some(item => item.el.contains(target))) {
-  //     while (NavbarItem.currentlyOpen.length > 0) {
-  //       NavbarItem.currentlyOpen[0].toggleItemMenu();
-  //     }
-  //   }
-  // }
 
   handleClassList(el, type, className) {
     el.classList[type](className)
@@ -83,7 +71,6 @@ export class NavbarItem {
 
   appendNavItemToMenu(navItems) { 
     const menu = this.getItemMenu()
-    //const navItems = this.getNavbarItems()
     navItems.forEach((el: HTMLElement) => {
       const li = document.createElement('li')
       li.appendChild(el)
@@ -157,57 +144,21 @@ export class NavbarItem {
   }
 
   toggleItemMenu() {
-    //const hasChildNavItems = this.itemHasNestedItems()
+    if(this.isMenuItem && this.hasChildNavItems) { 
+      const itemMenu = this.getItemMenu()
+      this.handleClassList(itemMenu, 'add', 'right')
+    }
+ 
     if(this.hasChildNavItems) { 
       const itemMenu = this.getItemMenu();
       const menuItem = this.getNavBarItem()
 
-
       this.handleClassList(itemMenu, 'toggle', 'open');
       this.handleClassList(menuItem, 'toggle', 'open');
-      
-      // if(hasChildNavItems) {
-      //   if (NavbarItem.currentlyOpen.includes(this)) {
-      //     NavbarItem.currentlyOpen = NavbarItem.currentlyOpen.filter(item => item !== this);
-      //     this.handleClassList(itemMenu, 'remove', 'open');
-      //   } else {
-      //     NavbarItem.currentlyOpen = [...NavbarItem.currentlyOpen, this];
-      //     this.handleClassList(itemMenu, 'add', 'open');
-      //   }
-      // }
-    } else { 
-
-    }
+    } 
   }
 
   
-  // toggleSubmenu() {
-  //   if (this.isExpandable) {
-  //     const menuItem = this.getSidebarMenuItem();
-  //     const expandableMenu = this.getExpandableMenu();
-  //     this.handleClassList(expandableMenu, 'toggle', 'open');
-  //     this.handleClassList(menuItem, 'toggle', 'open');
-  //     // Emit an event with the current component
-  //     this.handleEventEmission();
-  //   } else {
-  //     // If the sidebar item is not expandable, it's a leaf item without a submenu.
-  //     // Emit an event to the parent `ifx-sidebar` component to notify it that a leaf item has been clicked.
-  //     if (this.isActionItem) { //its an action item that eg opens an overlay etc and should not influence the active state of the item
-  //       this.ifxSidebarActionItem.emit(this.el); // emit new event if isActionItem is true
-  //       return;
-  //     } else { //its a navigation item which becomes active after clicking it
-  //       this.handleActiveChange(true, this.internalActiveState)
-  //       this.ifxSidebarNavigationItem.emit(this.el);
-  //     }
-  //     // If the sidebar item is selectable (not expandable), then call the handler function with the current element.
-  //     if (this.handleItemClick) {
-  //       this.handleItemClick(this.el);
-  //     }
-  //   }
-  //   // // Emit an event with the current component
-  //   // this.handleEventEmission();
-  // }
-
   itemHasNestedItems() { 
     const childNavItem = this.el.shadowRoot.querySelector('ifx-navbar-item')
     if(childNavItem) { 
@@ -217,25 +168,27 @@ export class NavbarItem {
     }
   }
   
+  handleNestedLayerMenu(e) { 
+    if(this.isMenuItem && this.hasChildNavItems) { 
+      if(e.type.toUpperCase() === 'MOUSEENTER') { 
+        const itemMenu = this.getItemMenu()
+        this.handleClassList(itemMenu, 'add', 'open')
+        this.handleClassList(itemMenu, 'add', 'right')
+      }
 
-  // handleClick(event: MouseEvent) {
-  //   event.stopPropagation();
+      if(e.type.toUpperCase() === 'MOUSELEAVE') { 
+        const itemMenu = this.getItemMenu()
+        this.handleClassList(itemMenu, 'remove', 'open')
+        this.handleClassList(itemMenu, 'remove', 'right')
+      }
+    }
+  }
 
-  //   // If this is a top-level menu and it's not currently open, close the currently open top-level menus
-  //   if (!this.el.closest('ifx-navbar-item') && !NavbarItem.currentlyOpen.includes(this)) {
-  //     while (NavbarItem.currentlyOpen.length > 0) {
-  //       NavbarItem.currentlyOpen[0].toggleItemMenu();
-  //     }
-  //   }
-
-  //   // Toggle the submenu
-  //   this.toggleItemMenu();
-  // }
-
+  
   
   render() {
     return (
-      <div>
+      <div class="container" onMouseLeave={e => this.handleNestedLayerMenu(e)}  onMouseEnter={e => this.handleNestedLayerMenu(e)}>
         <a href={this.internalHref} target={this.target} onClick={() => this.toggleItemMenu()} class=   {`navbar__container-left-content-navigation-item ${this.hideLabel ? 'removeLabel' : ""} ${this. hideComponent ? 'hideElement' : ""} ${this.isMenuItem ? 'menuItem' : ""}`}>
           <div class={`navbar__container-right-content-navigation-item-icon-wrapper ${!this.icon ? "removeWrapper" : ""}`}>
             {this.icon && <ifx-icon icon={this.icon}></ifx-icon>}
@@ -243,6 +196,8 @@ export class NavbarItem {
           <span>
             <slot onSlotchange={(e) => this.handleSlotChange(e)} />
           </span>
+          {this.hasChildNavItems && !this.isMenuItem && <ifx-icon icon="chevron-up-12" />}
+          {this.hasChildNavItems && this.isMenuItem && <ifx-icon icon="chevron-right-12" />}
         </a>
         {this.hasChildNavItems && <ul class='navbar-menu'></ul>}
       </div>
