5e6fd67acd0fb0264a9f23323f232c0122761de2
verena-ifx <verena-ifx@users.noreply.github.com>
2024-02-12 15:16:23 +0000

    Deploy preview for PR 988 🛫

--- pr-preview-vue-example/pr-988/assets/ifx-tab.entry-C0svLzhk.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as s,g as r}from"./index-Bd8UiPxA.js";const n=":host{display:block}",a=class{constructor(t){e(this,t),this.header=void 0,this.disabled=void 0}render(){return s("slot",null)}get el(){return r(this)}};a.style=n;export{a as ifx_tab};
