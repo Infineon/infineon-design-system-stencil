30938cdd4a09c2656fa20050c8874e0aa980b9ce
tishoyanchev <108734092+tishoyanchev@users.noreply.github.com>
2024-02-23 05:00:16 +0000

    Deploying to gh-pages from @ Infineon/infineon-design-system-stencil@02765ce7e490fde64aa47555bf0d9d2a2695782b 🚀

--- react-example/static/js/7900.3125e770.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_javascript=self.webpackChunkreact_javascript||[]).push([[7900],{7900:(r,a,s)=>{s.r(a),s.d(a,{ifx_dropdown_separator:()=>e});var t=s(2216);const e=class{constructor(r){(0,t.r)(this,r)}render(){return(0,t.h)("div",{class:"dropdown-separator"})}};e.style=".dropdown-separator{height:1px;background-color:#EEEDED;margin:8px 16px}"}}]);
-//# sourceMappingURL=7900.3125e770.chunk.js.map
