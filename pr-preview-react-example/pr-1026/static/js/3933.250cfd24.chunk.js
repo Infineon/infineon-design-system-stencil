d1db956a262f559fb135c3f59a0eb7c97beece52
akashyeole <akashyeole@users.noreply.github.com>
2024-03-04 08:48:01 +0000

    Deploy preview for PR 1026 🛫

---
+++ pr-preview-react-example/pr-1026/static/js/3933.250cfd24.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_javascript=self.webpackChunkreact_javascript||[]).push([[3933],{3933:(r,a,s)=>{s.r(a),s.d(a,{ifx_dropdown_separator:()=>e});var t=s(1597);const e=class{constructor(r){(0,t.r)(this,r)}render(){return(0,t.h)("div",{class:"dropdown-separator"})}};e.style=".dropdown-separator{height:1px;background-color:#EEEDED;margin:8px 16px}"}}]);
+//# sourceMappingURL=3933.250cfd24.chunk.js.map
