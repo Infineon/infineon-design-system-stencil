a1f88d97de32b069aa9083f3f45dbe4ebb9c8e7e
akashyeole <akashyeole@users.noreply.github.com>
2024-03-05 11:49:58 +0000

    Deploy preview for PR 1032 🛫

---
+++ pr-preview-react-example/pr-1032/static/js/3563.2bc98256.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_javascript=self.webpackChunkreact_javascript||[]).push([[3563],{3563:(s,t,e)=>{e.r(t),e.d(t,{ifx_tab:()=>a});var r=e(1597);const a=class{constructor(s){(0,r.r)(this,s),this.header=void 0,this.disabled=void 0}render(){return(0,r.h)("slot",null)}get el(){return(0,r.g)(this)}};a.style=":host{display:block}"}}]);
+//# sourceMappingURL=3563.2bc98256.chunk.js.map
