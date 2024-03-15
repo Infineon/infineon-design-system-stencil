28eab0a7f33ea1cd96b7a0e90ccfeaf1ac565ca8
akashyeole <akashyeole@users.noreply.github.com>
2024-03-15 14:14:48 +0000

    Deploy preview for PR 1060 🛫

---
+++ pr-preview-vue-example/pr-1060/assets/ifx-dropdown-separator.entry-ngSu-Be-.js
@@ -0,0 +1,1 @@
+import{r as o,h as a}from"./index-D-JbrOHt.js";const e=".dropdown-separator{height:1px;background-color:#EEEDED;margin:8px 16px}",t=e,s=class{constructor(r){o(this,r)}render(){return a("div",{key:"d1b731254a1800cb52a8d2e208e7dc989e9aeffc",class:"dropdown-separator"})}};s.style=t;export{s as ifx_dropdown_separator};
