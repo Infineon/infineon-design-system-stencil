98ca0d045a248b1c0fc8e79ff5017299ec909101
SergejLust <SergejLust@users.noreply.github.com>
2024-03-22 12:34:51 +0000

    Deploy preview for PR 1095 🛫

---
+++ pr-preview-react-example/pr-1095/static/js/3563.b0c670cc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_javascript=self.webpackChunkreact_javascript||[]).push([[3563],{3563:(e,s,t)=>{t.r(s),t.d(s,{ifx_tab:()=>r});var a=t(6205);const r=class{constructor(e){(0,a.r)(this,e),this.header=void 0,this.disabled=void 0}render(){return(0,a.h)("slot",{key:"58026a59853031da20801d0ebe1d11a1821758a6"})}get el(){return(0,a.g)(this)}};r.style=":host{display:block}"}}]);
+//# sourceMappingURL=3563.b0c670cc.chunk.js.map
