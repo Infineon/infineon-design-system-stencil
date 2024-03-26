0ff2f284f168df87956b28e08a381076b87547e1
Yanchev Tihomir (IT ERP DXT / EE) <Tihomir.Yanchev-EE@infineon.com>
2024-03-26 11:03:18 +0100

    updated react navbar componentand did clean install on react

--- examples/wrapper-components/react-javascript/src/components/Navbar/Navbar.js
+++ examples/wrapper-components/react-javascript/src/components/Navbar/Navbar.js
@@ -1,27 +1,90 @@
 import React from 'react';
-import { IfxNavbar, IfxNavbarMenuItem, IfxSearchBar, IfxNavbarItem, IfxNavbarMenu } from '@infineon/infineon-design-system-react';
+import { IfxNavbar, IfxNavbarProfile, IfxSearchBar, IfxNavbarItem } from '@infineon/infineon-design-system-react';
 
 function Navbar() {
   return (
-    <div>
-      <IfxNavbar application-name="Application name" fixed="false" logo-href="https://google.com">
-        <IfxNavbarItem hide-label="false" icon="calendar16" slot="left-item" target="_blank" href="https://google.com">Menu Item1</IfxNavbarItem>
-        <IfxNavbarItem  slot="left-item">Menu Item</IfxNavbarItem>
-        <IfxNavbarItem  slot="left-item">Menu Item</IfxNavbarItem>
+  <IfxNavbar  show-logo-and-appname="true" application-name="App name" fixed="false" logo-href="http://google.com" logo-href-target="_self">
+    <IfxNavbarItem icon="calendar16" slot="left-item" target="" href="" >
+      Menu Item 1
+      <IfxNavbarItem icon="calendar16">
+        Layer 1 Nested Item 1
+        <IfxNavbarItem>
+          Layer 2 Nested Item 2
+          <IfxNavbarItem href="http://google.com" target="_blank">Link Layer 3 Nested Item 1</IfxNavbarItem>
+          <IfxNavbarItem>Layer 3 Nested Item 2</IfxNavbarItem>
+          <IfxNavbarItem>Layer 3 Nested Item 3</IfxNavbarItem>
+          <IfxNavbarItem>Layer 3 Nested Item 4</IfxNavbarItem>
+        </IfxNavbarItem>
+        <IfxNavbarItem >Layer 2 Nested Item 3</IfxNavbarItem>
+        <IfxNavbarItem>Layer 2 Nested Item 4</IfxNavbarItem>
+        <IfxNavbarItem>Layer 2 Nested Item 5</IfxNavbarItem>
+      </IfxNavbarItem>
 
-        <IfxNavbarMenu slot="menu">
-          <IfxNavbarMenuItem href="https://google.com" target="_blank">Item 1</IfxNavbarMenuItem>
-          <IfxNavbarMenuItem href="https://google.com">Item 2</IfxNavbarMenuItem>
-          <IfxNavbarMenuItem href="https://google.com">Item 3</IfxNavbarMenuItem>
-        </IfxNavbarMenu>
+      <IfxNavbarItem>
+        Layer 1 Nested Item 2
+        <IfxNavbarItem>Layer 2 Item 1</IfxNavbarItem>
+        <IfxNavbarItem>Layer 2 Item 2</IfxNavbarItem>
+        <IfxNavbarItem>Layer 2 Item 3</IfxNavbarItem>
+      </IfxNavbarItem>
 
-        <IfxSearchBar size="default" slot="search-bar-right" is-open="false" show-close-button="true"></IfxSearchBar>
+      <IfxNavbarItem>Nested Item 3</IfxNavbarItem>
 
-        <IfxNavbarItem hide-label="false" slot="right-item">Right One</IfxNavbarItem>
-        <IfxNavbarItem hide-label="false" slot="right-item">Right Two</IfxNavbarItem>
-        <IfxNavbarItem hide-label="false" slot="right-profile-item">Tisho</IfxNavbarItem>
-      </IfxNavbar>
-    </div>
+      <IfxNavbarItem>
+        Layer 1 Nested Item 4
+        <IfxNavbarItem>Nested Item 4</IfxNavbarItem>
+      </IfxNavbarItem>
+
+    </IfxNavbarItem>
+
+    <IfxNavbarItem href="http://google.com" target="_blank" slot="left-item" icon="calendar16" show-label="true">
+      Menu Item 2
+    </IfxNavbarItem>
+
+    <IfxNavbarItem slot="left-item">
+      More
+      <IfxNavbarItem>Item1</IfxNavbarItem>
+      <IfxNavbarItem>Item2</IfxNavbarItem>
+    </IfxNavbarItem>
+
+    <IfxSearchBar id="myBar" slot="search-bar-left" is-open="false"></IfxSearchBar>
+
+    <IfxNavbarItem slot="right-item" target="_blank" href="http://google.com" hide-on-mobile="false" show-label="false" icon="cartf16">Right Item</IfxNavbarItem>
+    <IfxNavbarItem slot="right-item" hide-on-mobile="true" show-label='true' icon="airplane16">
+      Right Item
+      <IfxNavbarItem>
+        Nested one
+        <IfxNavbarItem>
+          Nested one
+          <IfxNavbarItem>
+            Nested one
+            <IfxNavbarItem>Final one</IfxNavbarItem>
+          </IfxNavbarItem>
+        </IfxNavbarItem>
+      </IfxNavbarItem>
+    </IfxNavbarItem>
+
+    <IfxNavbarProfile slot="right-item" image-url="https://i.pinimg.com/originals/82/d4/92/82d4926dcf09dd4c73eb1a6c0300c135.jpg" show-label="true" href="" target="_blank">
+      Tisho
+      <IfxNavbarItem>
+        Item
+        <IfxNavbarItem>
+          Item
+          <IfxNavbarItem>
+            Item
+            <IfxNavbarItem>
+              Item
+              <IfxNavbarItem>
+                Item
+              </IfxNavbarItem>
+            </IfxNavbarItem>
+          </IfxNavbarItem>
+        </IfxNavbarItem>
+      </IfxNavbarItem>
+      <IfxNavbarItem>Item</IfxNavbarItem>
+      <IfxNavbarItem>Item</IfxNavbarItem>
+      <IfxNavbarItem>Item</IfxNavbarItem>
+    </IfxNavbarProfile>
+  </IfxNavbar>
   );
 }
 
