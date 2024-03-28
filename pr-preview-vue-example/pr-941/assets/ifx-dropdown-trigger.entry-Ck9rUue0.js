a3f1e85fcc7ba9e8142c54ce3fe7fb0eb280474b
tishoyanchev <tishoyanchev@users.noreply.github.com>
2024-03-28 08:59:38 +0000

    Deploy preview for PR 941 🛫

---
+++ pr-preview-vue-example/pr-941/assets/ifx-dropdown-trigger.entry-Ck9rUue0.js
@@ -0,0 +1,1 @@
+import{r as o,h as s}from"./index-Cv4Z94C7.js";const t=":host{display:inline-block;cursor:pointer}",e=t,n=class{constructor(r){o(this,r),this.isOpen=!1}render(){return s("slot",{key:"350c422b9a1d2ac76c2bfd771fb084c60ac53caf"})}};n.style=e;export{n as ifx_dropdown_trigger};
