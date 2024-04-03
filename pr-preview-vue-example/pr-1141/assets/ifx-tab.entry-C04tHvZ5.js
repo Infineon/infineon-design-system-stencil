17caaf763aaf092cd351e1fa1e3f5cca86cbc101
akashyeole <akashyeole@users.noreply.github.com>
2024-04-03 10:04:22 +0000

    Deploy preview for PR 1141 🛫

---
+++ pr-preview-vue-example/pr-1141/assets/ifx-tab.entry-C04tHvZ5.js
@@ -0,0 +1,1 @@
+import{r as t,h as s,g as r}from"./index-DhwMidoe.js";const a=":host{display:block}",n=a,d=class{constructor(e){t(this,e),this.header=void 0,this.disabled=void 0}render(){return s("slot",{key:"74dc5952487d804af10ea7451d40f0807dc5c59f"})}get el(){return r(this)}};d.style=n;export{d as ifx_tab};
