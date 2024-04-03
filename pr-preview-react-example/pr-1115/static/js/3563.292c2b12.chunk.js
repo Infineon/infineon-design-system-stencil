b2ce0dbedd26fdffeb9e2d937c3cfb1b0f69425f
akashyeole <akashyeole@users.noreply.github.com>
2024-04-03 19:28:09 +0000

    Deploy preview for PR 1115 🛫

---
+++ pr-preview-react-example/pr-1115/static/js/3563.292c2b12.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_javascript=self.webpackChunkreact_javascript||[]).push([[3563],{3563:(s,t,e)=>{e.r(t),e.d(t,{ifx_tab:()=>a});var r=e(7770);const a=class{constructor(s){(0,r.r)(this,s),this.header=void 0,this.disabled=void 0}render(){return(0,r.h)("slot",{key:"74dc5952487d804af10ea7451d40f0807dc5c59f"})}get el(){return(0,r.g)(this)}};a.style=":host{display:block}"}}]);
+//# sourceMappingURL=3563.292c2b12.chunk.js.map
