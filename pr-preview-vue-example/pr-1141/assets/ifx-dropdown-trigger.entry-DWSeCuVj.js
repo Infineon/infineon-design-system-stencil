4f8977e0473f4fd7d7568a0e0252033b15d703de
akashyeole <akashyeole@users.noreply.github.com>
2024-04-10 05:41:46 +0000

    Deploy preview for PR 1141 🛫

--- pr-preview-vue-example/pr-1141/assets/ifx-dropdown-trigger.entry-DWSeCuVj.js
+++
@@ -1,1 +0,0 @@
-import{r as o,h as s}from"./index-DhwMidoe.js";const t=":host{display:inline-block;cursor:pointer}",e=t,n=class{constructor(r){o(this,r),this.isOpen=!1}render(){return s("slot",{key:"350c422b9a1d2ac76c2bfd771fb084c60ac53caf"})}};n.style=e;export{n as ifx_dropdown_trigger};
