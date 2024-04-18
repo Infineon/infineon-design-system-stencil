823e2da91e8707cf938cbd6bea2359505c492ae4
akashyeole <akashyeole@users.noreply.github.com>
2024-04-18 12:36:46 +0000

    Deploy preview for PR 1115 🛫

---
+++ pr-preview-angular-example/pr-1115/4292.efeb5ceaf9deb7c9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[4292],{4292:(c,t,s)=>{s.r(t),s.d(t,{ifx_tab:()=>d});var e=s(3415);const d=class{constructor(n){(0,e.r)(this,n),this.header=void 0,this.disabled=void 0}render(){return(0,e.h)("slot",{key:"01e4b3075bb32ef8c4d08ab74440fad344dc214a"})}get el(){return(0,e.g)(this)}};d.style=":host{display:block}"}}]);
