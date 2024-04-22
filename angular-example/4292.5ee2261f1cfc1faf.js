25e81aef6c2a80a8879a2c3f44b61afe3c92787d
verena-ifx <93577764+verena-ifx@users.noreply.github.com>
2024-04-22 14:16:12 +0000

    Deploying to gh-pages from @ Infineon/infineon-design-system-stencil@c2dfb447fd4e91dd7ee9b4cb8493c9e45591e599 🚀

--- angular-example/4292.5ee2261f1cfc1faf.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[4292],{4292:(c,e,s)=>{s.r(e),s.d(e,{ifx_tab:()=>d});var t=s(3415);const d=class{constructor(a){(0,t.r)(this,a),this.header=void 0,this.disabled=void 0}render(){return(0,t.h)("slot",{key:"74dc5952487d804af10ea7451d40f0807dc5c59f"})}get el(){return(0,t.g)(this)}};d.style=":host{display:block}"}}]);
