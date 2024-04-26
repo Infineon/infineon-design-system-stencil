7e50ebf990f9fd5d289e6e3a8e4e68c37d869549
tishoyanchev <108734092+tishoyanchev@users.noreply.github.com>
2024-04-26 13:52:38 +0000

    Deploying to gh-pages from @ Infineon/infineon-design-system-stencil@c30e84e3970fc70fdd0926e530584a6b7fd33b66 🚀

---
+++ vue-example/assets/ifx-list-group.entry-CvzpW2Hr.js
@@ -0,0 +1,3 @@
+import{r as s,h as t,H as i,g as l}from"./index-DBKcWgja.js";const n=":host{display:inline-flex}.list-group-container{display:inline-flex;flex-direction:column;gap:3px}",o=n,a=class{constructor(e){s(this,e),this.flush=!1,this.bulletpoint=!1,this.notification=!1}render(){return t(i,{key:"1b8f74ed8187f5c61607d934e1618b7e8582bf73"},t("div",{key:"09f506598b1e4c6dcd596c91ea2c7816122e3b5d",class:`list-group-container 
+        ${this.flush?"flush":""} 
+        ${this.bulletpoint?"bulletpoint":""}`},t("slot",{key:"ad909528c9fa4a64a37c3cde58abcad35c858552",name:"list-item"}),t("slot",{key:"cd337a06af79510ce3cf729db16d2c0f19f78be6",name:"list-notification"})))}get el(){return l(this)}};a.style=o;export{a as ifx_list_group};
