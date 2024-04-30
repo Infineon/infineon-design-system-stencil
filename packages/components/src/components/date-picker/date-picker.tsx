ceb93549c973540f18877ec10a566aa48741a122
Yanchev Tihomir (IT ERP DXT / EE) <Tihomir.Yanchev-EE@infineon.com>
2024-04-30 15:11:19 +0200

    added the states

--- packages/components/src/components/date-picker/date-picker.tsx
+++ packages/components/src/components/date-picker/date-picker.tsx
@@ -14,32 +14,32 @@ export class DatePicker {
   @Prop() disabled: boolean = false;
 
   getDate(e) { 
-    //console.log(e.target.value)
-    var selectedDate = new Date(e.target.value);
-
-    // Extracting day, month, and year separately
-    var day = selectedDate.getDate();
-    var month = selectedDate.getMonth() + 1; // Adding 1 to get 1-12 range
-    var year = selectedDate.getFullYear();
+    const selectedDate = new Date(e.target.value);
+    const day = selectedDate.getDate();
+    const month = selectedDate.getMonth() + 1; 
+    const year = selectedDate.getFullYear();
 
     console.log('Day: ' + day);
     console.log('Month: ' + month);
     console.log('Year: ' + year);
   }
 
-  handleClick() { 
+  handleInputFocusOnIconClick() { 
     const input = this.el.shadowRoot.querySelector('.date__picker-input') as HTMLInputElement;
     if(input) { 
       input.focus()
-      input.click()
     }
   }
 
+
   render() {
     return (
-      <div tabindex="0" class={`input__wrapper ${this.size === 'l' ? 'large' : 'small'}`} onClick={() => this.handleClick()}>
-        <input class='date__picker-input' type="date" onChange={(e) => this.getDate(e)} />
-        <div class="icon__wrapper">
+      <div class={`input__wrapper ${this.size === 'l' ? 'large' : 'small'} ${this.disabled ? 'disabled' : ""}`} >
+        <input 
+        class={`date__picker-input ${this.error ? 'error' : ""} ${this.success ? "success" : ""}`} type="date" 
+        disabled={this.disabled}
+        onChange={(e) => this.getDate(e)} />
+        <div class="icon__wrapper" onClick={() => this.handleInputFocusOnIconClick()}>
           <ifx-icon icon='calendar16'></ifx-icon>
         </div>
       </div>
