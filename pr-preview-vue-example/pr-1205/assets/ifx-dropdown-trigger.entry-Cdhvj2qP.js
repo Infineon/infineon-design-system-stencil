e1827c9529154ae0d4870a0af74ecdddd65b64fe
tishoyanchev <tishoyanchev@users.noreply.github.com>
2024-05-02 12:37:06 +0000

    Deploy preview for PR 1205 🛫

---
+++ pr-preview-vue-example/pr-1205/assets/ifx-dropdown-trigger.entry-Cdhvj2qP.js
@@ -0,0 +1,1 @@
+import{r as o,h as s}from"./index-CrOJmYxW.js";const e=":host{display:inline-block;cursor:pointer}",t=e,n=class{constructor(r){o(this,r),this.isOpen=!1}render(){return s("slot",{key:"fa5c96b865666a44aab707b25102c1aefbd78969"})}};n.style=t;export{n as ifx_dropdown_trigger};
---
+++ pr-preview-vue-example/pr-1205/assets/ifx-tab.entry-Cu-4LNbp.js
@@ -0,0 +1,1 @@
+import{r as e,h as s,g as i}from"./index-CrOJmYxW.js";const n=":host{display:block}",a=n,o=class{constructor(t){e(this,t),this.header=void 0,this.disabled=void 0,this.icon="",this.iconPosition="left"}render(){return s("slot",{key:"bf3efc661047331d4b1faab80868346aea675bde"})}get el(){return i(this)}};o.style=a;export{o as ifx_tab};
