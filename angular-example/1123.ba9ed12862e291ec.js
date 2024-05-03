6c1d065e2bbda1b238a870ffe048181adbc03562
tishoyanchev <108734092+tishoyanchev@users.noreply.github.com>
2024-05-03 06:44:28 +0000

    Deploying to gh-pages from @ Infineon/infineon-design-system-stencil@c82f51e05b11faaa6c3ef24cf5bb201bd19232f7 🚀

--- angular-example/1123.ba9ed12862e291ec.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[1123],{1123:(d,r,s)=>{s.r(r),s.d(r,{ifx_dropdown_trigger:()=>n});var o=s(3415);const n=class{constructor(e){(0,o.r)(this,e),this.isOpen=!1}render(){return(0,o.h)("slot",{key:"350c422b9a1d2ac76c2bfd771fb084c60ac53caf"})}};n.style=":host{display:inline-block;cursor:pointer}"}}]);
