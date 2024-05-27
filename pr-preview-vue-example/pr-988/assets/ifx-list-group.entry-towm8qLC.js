f2c122233f57e4a619f2aef68d20206446a8eb9f
verena-ifx <verena-ifx@users.noreply.github.com>
2024-05-27 08:12:20 +0000

    Deploy preview for PR 988 🛫

---
+++ pr-preview-vue-example/pr-988/assets/ifx-list-group.entry-towm8qLC.js
@@ -0,0 +1,3 @@
+import{r as s,h as t,H as i,g as a}from"./index-b-pvXNyC.js";const l=":host{display:inline-flex}.list-group-container{display:inline-flex;flex-direction:column;gap:3px}",n=l,o=class{constructor(e){s(this,e),this.flush=!1,this.bulletpoint=!1,this.notification=!1}render(){return t(i,{key:"a60b8f79e3da628a3b3e6a0aaa76a549d6cece25"},t("div",{key:"9a15a7511fb00a89995374ce3775e3a90fda1b65",class:`list-group-container 
+        ${this.flush?"flush":""} 
+        ${this.bulletpoint?"bulletpoint":""}`},t("slot",{key:"eb7f38cc7b08df998bf1dda72e9bada603535912",name:"list-item"}),t("slot",{key:"73a68c0909bdb3e6c3ea8f538d9b0812e8f41056",name:"list-notification"})))}get el(){return a(this)}};o.style=n;export{o as ifx_list_group};
