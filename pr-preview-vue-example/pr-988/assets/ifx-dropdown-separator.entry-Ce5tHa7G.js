f5edc4fb46d157df22d09a8ea3729c1900da10ca
verena-ifx <verena-ifx@users.noreply.github.com>
2024-06-17 12:34:43 +0000

    Deploy preview for PR 988 🛫

---
+++ pr-preview-vue-example/pr-988/assets/ifx-dropdown-separator.entry-Ce5tHa7G.js
@@ -0,0 +1,1 @@
+import{r as o,h as a}from"./index-Beqz2nAW.js";const t=".dropdown-separator{height:1px;background-color:#EEEDED;margin:8px 16px}",e=t,s=class{constructor(r){o(this,r)}render(){return a("div",{key:"b2d40033813be83fc126165b6ed33f90366d6a39",class:"dropdown-separator"})}};s.style=e;export{s as ifx_dropdown_separator};
--- pr-preview-vue-example/pr-988/assets/ifx-tab.entry-CBuxe7vS.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as s,g as i}from"./index-b-pvXNyC.js";const n=":host{display:block}",a=n,o=class{constructor(t){e(this,t),this.header=void 0,this.disabled=void 0,this.icon="",this.iconPosition="left"}render(){return s("slot",{key:"8a7179098a1e8604c24e0231958252185a9c7866"})}get el(){return i(this)}};o.style=a;export{o as ifx_tab};
