fa7dbb0ac2023943f9bd8e87c65ff175b7772dbd
SergejLust <SergejLust@users.noreply.github.com>
2024-06-21 04:59:33 +0000

    Deploy preview for PR 1314 🛫

---
+++ pr-preview-vue-example/pr-1314/assets/ifx-list-group.entry-O7W2agWh.js
@@ -0,0 +1,3 @@
+import{r as e,h as t,H as i,g as l}from"./index-CqN2xTws.js";const a=":host{display:inline-flex}.list-group-container{display:inline-flex;flex-direction:column;gap:3px}",n=a,o=class{constructor(s){e(this,s),this.flush=!1,this.bulletpoint=!1,this.notification=!1}render(){return t(i,{key:"b28acf121f1ad4d0e8f6a7dcb0856535d3a83931"},t("div",{key:"0273d08b2e07bf5211f1c2c6e4ca358f42921584",class:`list-group-container 
+        ${this.flush?"flush":""} 
+        ${this.bulletpoint?"bulletpoint":""}`},t("slot",{key:"8322df06dbca6b6020ecbde405ae64679dd1621f",name:"list-item"}),t("slot",{key:"518ab9525682fdb4c23bedafbd58a0d30b13aab3",name:"list-notification"})))}get el(){return l(this)}};o.style=n;export{o as ifx_list_group};
