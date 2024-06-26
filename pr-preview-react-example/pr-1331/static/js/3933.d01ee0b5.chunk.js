8a8ac8af63d148bebd0a0a3d21423cd15788df54
akashyeole <akashyeole@users.noreply.github.com>
2024-06-26 12:57:47 +0000

    Deploy preview for PR 1331 🛫

---
+++ pr-preview-react-example/pr-1331/static/js/3933.d01ee0b5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_javascript=self.webpackChunkreact_javascript||[]).push([[3933],{3933:(r,a,e)=>{e.r(a),e.d(a,{ifx_dropdown_separator:()=>t});var s=e(7770);const t=class{constructor(r){(0,s.r)(this,r)}render(){return(0,s.h)("div",{key:"b2d40033813be83fc126165b6ed33f90366d6a39",class:"dropdown-separator"})}};t.style=".dropdown-separator{height:1px;background-color:#EEEDED;margin:8px 16px}"}}]);
+//# sourceMappingURL=3933.d01ee0b5.chunk.js.map
