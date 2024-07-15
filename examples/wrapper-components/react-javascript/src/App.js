9698082142abb5a1e42c3b8fc561f42ed19ed6f2
Akash Yeole <akashyeole02@gmail.com>
2024-07-15 13:28:14 +0530

    clean install + fixed react app error

--- examples/wrapper-components/react-javascript/src/App.js
+++ examples/wrapper-components/react-javascript/src/App.js
@@ -39,154 +39,151 @@ import Stepper from './components/Stepper/Stepper';
 function App() {
   return (
     <div>
-       <Stepper />
+
+      <Navbar />
+      <h1>Stencil Framework integration - React + JS</h1>
+
+      <h2>Accordion</h2>
+      <Accordion />
+      <br />
+
+      <h2>Alert</h2>
+      <Alert />
+      <br />
+
+      <h2>Badge</h2>
+      <Badge />
+      <br />
+
+      <h2>Breadcrumbs</h2>
+      <Breadcrumbs />
+      <br />
+
+      <h2>Button</h2>
+      <Button />
+      <br />
+
+      <h2>Card</h2>
+      <Card />
+      <br />
+
+      <h2>Checkbox</h2>
+      <Checkbox />
+      <br />
+
+      <h2>Chip</h2>
+      <Chip />
+      <br />
+
+      <h2>Date Picker</h2>
+      <DatePicker />
+      <br />
+
+      <h2>Dropdown</h2>
+      <Dropdown />
+      <br />
+
+      <h2>Icon</h2>
+      <Icon />
+      <br />
+
+      <h2>IconButton</h2>
+      <IconButton />
+      <br />
+
+      <h2>Link</h2>
+      <Link />
+      <br />
+
+      <h2>Modal</h2>
+      <Modal />
+      <br />
+
+      <h2>MultiSelect</h2>
+      <MultiSelect />
+      <br />
+
+      <h2>Number indicator</h2>
+      <NumberIndicator />
+      <br />
+
+      <h2>Pagination</h2>
+      <Pagination />
+      <br />
+
+      <h2>Progress Bar</h2>
+      <ProgressBar />
+      <br />
+
+      <h2>Radio Button</h2>
+      <RadioButton />
+      <br />
+
+
+      <h2>SingleSelect</h2>
+      <SingleSelect />
+      <br />
+
+
+      <h2>Spinner</h2>
+      <Link />
+      <br />
+
+      <h2>Search Bar</h2>
+      <SearchBar />
+      <br />
+
+      <h2>Search Field</h2>
+      <SearchField />
+
+      <h2>Spinner</h2>
+      <Spinner />
+      <br />
+
+      <h2>Slider</h2>
+      <Slider />
+      <br />
+
+      <h2>Sidebar</h2>
+      <Sidebar />
+      <br />
+
+      <h2>Switch</h2>
+      <Switch />
+      <br />
+
+      <h2>Status</h2>
+      <Status />
+      <br />
+
+      <h2>Stepper</h2>
+      <Stepper />
+      <br />
+
+      <h2>Tabs</h2>
+      <Tabs />
+      <br />
+
+      <h2>Tag</h2>
+      <Tag />
+      <br />
+
+      <h2>Text Field</h2>
+      <TextField />
+      <br />
+
+      <h2>Tooltip</h2>
+      <Tooltip />
+
+      <h2>Table (basic)</h2>
+      <BasicTable />
+      <br />
+
+      <h2>Footer</h2>
+      <Footer />
+      <br />
+
     </div>
-    // <div>
-
-    //   <Navbar />
-    //   <h1>Stencil Framework integration - React + JS</h1>
-
-    //   <h2>Accordion</h2>
-    //   <Accordion />
-    //   <br />
-
-    //   <h2>Alert</h2>
-    //   <Alert />
-    //   <br />
-
-    //   <h2>Badge</h2>
-    //   <Badge />
-    //   <br />
-
-    //   <h2>Breadcrumbs</h2>
-    //   <Breadcrumbs />
-    //   <br />
-
-    //   <h2>Button</h2>
-    //   <Button />
-    //   <br />
-
-    //   <h2>Card</h2>
-    //   <Card />
-    //   <br />
-
-    //   <h2>Checkbox</h2>
-    //   <Checkbox />
-    //   <br />
-
-    //   <h2>Chip</h2>
-    //   <Chip />
-    //   <br />
-
-    //   <h2>Date Picker</h2>
-    //   <DatePicker />
-    //   <br />
-
-    //   <h2>Dropdown</h2>
-    //   <Dropdown />
-    //   <br />
-
-    //   <h2>Icon</h2>
-    //   <Icon />
-    //   <br />
-
-    //   <h2>IconButton</h2>
-    //   <IconButton />
-    //   <br />
-
-    //   <h2>Link</h2>
-    //   <Link />
-    //   <br />
-
-    //   <h2>Modal</h2>
-    //   <Modal />
-    //   <br />
-
-    //   <h2>MultiSelect</h2>
-    //   <MultiSelect />
-    //   <br />
-
-    //   <h2>Number indicator</h2>
-    //   <NumberIndicator />
-    //   <br />
-
-    //   <h2>Pagination</h2>
-    //   <Pagination />
-    //   <br />
-
-    //   <h2>Progress Bar</h2>
-    //   <ProgressBar />
-    //   <br />
-
-    //   <h2>Radio Button</h2>
-    //   <RadioButton />
-    //   <br />
-
-
-    //   <h2>SingleSelect</h2>
-    //   <SingleSelect />
-    //   <br />
-
-
-    //   <h2>Spinner</h2>
-    //   <Link />
-    //   <br />
-
-    //   <h2>Search Bar</h2>
-    //   <SearchBar />
-    //   <br />
-
-    //   <h2>Search Field</h2>
-    //   <SearchField />
-
-    //   <h2>Spinner</h2>
-    //   <Spinner />
-    //   <br />
-
-    //   <h2>Slider</h2>
-    //   <Slider />
-    //   <br />
-
-    //   <h2>Sidebar</h2>
-    //   <Sidebar />
-    //   <br />
-
-    //   <h2>Switch</h2>
-    //   <Switch />
-    //   <br />
-
-    //   <h2>Status</h2>
-    //   <Status />
-    //   <br />
-
-    //   <h2>Stepper</h2>
-    //   <Stepper />
-    //   <br />
-
-    //   <h2>Tabs</h2>
-    //   <Tabs />
-    //   <br />
-
-    //   <h2>Tag</h2>
-    //   <Tag />
-    //   <br />
-
-    //   <h2>Text Field</h2>
-    //   <TextField />
-    //   <br />
-
-    //   <h2>Tooltip</h2>
-    //   <Tooltip />
-
-    //   <h2>Table (basic)</h2>
-    //   <BasicTable />
-    //   <br />
-
-    //   <h2>Footer</h2>
-    //   <Footer />
-    //   <br />
-
-    // </div>
 
   )
 }
