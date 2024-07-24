c1a9c0a6722df85f789721b5152b975c73e31b15
Pfaff Fabian <fabian.pfaff@infineon.com>
2024-07-24 11:05:54 +0200

    add maxlength to searchfield

--- packages/components/src/components/search-field/search-field.stories.ts
+++ packages/components/src/components/search-field/search-field.stories.ts
@@ -19,6 +19,10 @@ export default {
       description: 'Place holder text - default value: "Search..."',
       control: { type: 'text'}
     },
+    maxlength: {
+      description: 'Maximum input length',
+      control: {type: 'number'}
+    },
     ifxInput: {
       action: 'ifxInput',
       description: 'Custom event',
@@ -33,12 +37,13 @@ export default {
   },
 };
 
-const DefaultTemplate = ({ disabled, size, showDeleteIcon, placeholder }) => {
+const DefaultTemplate = ({ disabled, size, showDeleteIcon, placeholder, maxlength }) => {
   const element = document.createElement('ifx-search-field');
   element.setAttribute('size', size);
   element.setAttribute('disabled', disabled);
   element.setAttribute('show-delete-icon', showDeleteIcon);
   if(placeholder != undefined) element.setAttribute('placeholder', placeholder);
+  if(maxlength != undefined) element.setAttribute('maxlength', maxlength);
   element.addEventListener('ifxInput', action('ifxInput'));
 
   return element;
