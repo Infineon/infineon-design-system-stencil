526a13343929a958dea0d6be70377b8108bdb8ef
verena-ifx <verena.lechner@infineon.com>
2024-09-05 17:49:09 +0200

    fix(1191): react wrapper index.ts update

--- packages/components-react/lib/index.ts
+++ packages/components-react/lib/index.ts
@@ -1,2 +1,2 @@
 export * from './components/stencil-generated/components.js';
-export { defineCustomElements } from "@infineon/infineon-design-system-stencil/loader/";
+export { defineCustomElements } from "@infineon/infineon-design-system-stencil/loader/index.js";
