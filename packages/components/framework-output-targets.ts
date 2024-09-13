53043ff737012242e261a894cb264330e6d6fc01
verena-ifx <verena.lechner@infineon.com>
2024-09-13 18:48:43 +0200

    fix(1461): test includes build first

--- packages/components/framework-output-targets.ts
+++ packages/components/framework-output-targets.ts
@@ -42,6 +42,7 @@ export const frameworkTargets = [
     componentCorePackage: '@infineon/infineon-design-system-stencil',
     proxiesFile: '../components-vue/lib/components.ts',
     componentModels: vueComponentModels,
+    includePolyfills: true,
     // includeDefineCustomElements: true, // This is what tells the target to use the custom elements output
     excludeComponents,
 
