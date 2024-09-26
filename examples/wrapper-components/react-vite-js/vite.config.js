fb69363b98953d9f3bed43915a6cfa206ac9b8fd
Yanchev Tihomir (IT ERP DXT / EE) <Tihomir.Yanchev-EE@infineon.com>
2024-09-26 11:45:45 +0200

    made config default

--- examples/wrapper-components/react-vite-js/vite.config.js
+++ examples/wrapper-components/react-vite-js/vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@stencil/react-output-target/runtime': new URL('@stencil/react-output-target/runtime', import.meta.url).pathname
-    }
-  }
-});
+})
