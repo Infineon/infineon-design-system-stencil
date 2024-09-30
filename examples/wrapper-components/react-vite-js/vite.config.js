dba4f9dc2cd269df11dd3b39b56c88cde6ac9813
verena-ifx <verena.lechner@infineon.com>
2024-09-30 17:02:40 +0200

    fix(1514): removed commented out line

--- examples/wrapper-components/react-vite-js/vite.config.js
+++ examples/wrapper-components/react-vite-js/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: "./",
+  base: "./", // This is important for the github pages deployment
   plugins: [
     react(),
 
