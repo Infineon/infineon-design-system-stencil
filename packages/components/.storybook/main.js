0c3b414477df7d1a4516d8806365eed5395c3f6b
verena-ifx <verena.lechner@infineon.com>
2024-10-09 15:53:25 +0200

    feat(987): updated storybook deps, cleaned up main.js

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,26 +1,14 @@
 import { dirname, join } from "path";
 // .storybook/main.ts
 
-import remarkGfm from 'remark-gfm';
 
 module.exports = {
   "stories": ["../src/**/*.stories.@(js|jsx|ts|tsx)", "../src/**/*.mdx"],
   "addons": [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
-    // {
-    //   name: '@storybook/addon-docs',
-    //   options: {
-    //     mdxPluginOptions: {
-    //       mdxCompileOptions: {
-    //         remarkPlugins: [remarkGfm],
-    //       },
-    //     },
-    //   },
-    // },
     getAbsolutePath("@storybook/addon-interactions"),
-    getAbsolutePath("@storybook/addon-mdx-gfm"),
-    "@storybook/addon-webpack5-compiler-babel"
+    getAbsolutePath("@storybook/addon-webpack5-compiler-babel")
   ],
   "framework": {
     name: getAbsolutePath("@storybook/html-webpack5"),
