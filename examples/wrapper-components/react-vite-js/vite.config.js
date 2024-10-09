b5bd2bb0c52cf8394b5a4d7d10744f07fc1760ce
verena-ifx <verena.lechner@infineon.com>
2024-10-09 11:20:01 +0200

    feat(987): updated vite config of react example app

--- examples/wrapper-components/react-vite-js/vite.config.js
+++ examples/wrapper-components/react-vite-js/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [
     react(),
   ],
+  build: {
+    rollupOptions: {
+      external: ['@stencil/react-output-target/runtime']
+    }
+  }
 });
