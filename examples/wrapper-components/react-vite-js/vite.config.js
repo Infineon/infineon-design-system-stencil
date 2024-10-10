5c558ead0ae5e6249d4a04e8adf2dc0e6d66b983
verena-ifx <verena.lechner@infineon.com>
2024-10-10 11:13:00 +0200

    feat(987): updated react example app vite config + added missing dependency to react wrapper

--- examples/wrapper-components/react-vite-js/vite.config.js
+++ examples/wrapper-components/react-vite-js/vite.config.js
@@ -2,12 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "./",
   plugins: [
     react(),
   ],
-  build: {
-    rollupOptions: {
-      external: ['@stencil/react-output-target/runtime']
-    }
-  }
 });
