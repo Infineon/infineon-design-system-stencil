7961fa9a20c31fbbda64732fba55c940b396936d
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2024-10-10 08:47:33 +0000

    Deploy preview for PR 1450 🛫

---
+++ pr-preview-vue-example/pr-1450/assets/ifx-tab.entry-CSuoUQ5F.js
@@ -0,0 +1,1 @@
+import{r as e,h as s,g as i}from"./index-uUf70LI1.js";const n=":host{display:block}",a=n,o=class{constructor(t){e(this,t),this.header=void 0,this.disabled=void 0,this.icon="",this.iconPosition="left"}render(){return s("slot",{key:"5add3272fea794af3392843aef10209b3c552c23"})}get el(){return i(this)}};o.style=a;export{o as ifx_tab};
--- pr-preview-vue-example/pr-1450/assets/ifx-tab.entry-CTc-8vXC.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as s,g as i}from"./index-D4bSb9P0.js";const n=":host{display:block}",a=n,o=class{constructor(t){e(this,t),this.header=void 0,this.disabled=void 0,this.icon="",this.iconPosition="left"}render(){return s("slot",{key:"5add3272fea794af3392843aef10209b3c552c23"})}get el(){return i(this)}};o.style=a;export{o as ifx_tab};
