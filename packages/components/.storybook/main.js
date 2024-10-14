0866d473ba146a0628154869bed423f0d60d621f
Akash Yeole <akashyeole02@gmail.com>
2024-10-14 16:28:02 +0530

    refactor(1416): revert glob dependency removal

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,8 +1,14 @@
 import { dirname, join } from "path";
+import { glob } from 'glob'; 
+import path from 'path'
 
 module.exports = {
-  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)", "../src/**/*.mdx"],
-  addons: [
+  stories: async () => {
+    // Use the custom function to get the list of stories
+    const stories = findAndExcludeFiles();
+    return stories;
+  },
+    addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-interactions"),
@@ -24,3 +30,18 @@ module.exports = {
 function getAbsolutePath(value) {
   return dirname(require.resolve(join(value, "package.json")));
 }
+
+ const findAndExcludeFiles = () => {
+   const allStories = [
+    ...glob.sync(path.join(__dirname, '../src/**/*.stories.@(js|jsx|ts|tsx)').replaceAll('\\', '/')),
+    ...glob.sync(path.join(__dirname, '../src/**/*.mdx').replaceAll('\\', '/'))
+  ];
+  
+  // Exclude specific files or patterns
+  const filteredStories = allStories.filter(story => {
+    return !(story.includes('components/table-advanced-version/filter-type-group') || story.includes('components/table-advanced-version/list'));
+  });
+
+  const sortedStories = filteredStories.sort();
+  return sortedStories;
+};
