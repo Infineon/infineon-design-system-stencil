7d33d2df4b20f9cb679eeef1919ee1ba2cf7ad03
Akash Yeole <akashyeole02@gmail.com>
2024-10-15 17:26:29 +0530

    feat(1416): update example apps

--- examples/wrapper-components/react-vite-js/src/components/SegmentedControls/SegmentedControls.jsx
+++ examples/wrapper-components/react-vite-js/src/components/SegmentedControls/SegmentedControls.jsx
@@ -1,20 +1,11 @@
 import { IfxSegmentedControlGroup, IfxSegmentedControl } from '@infineon/infineon-design-system-react';
-import { useState } from 'react';
-
+import React from 'react';
 
 function SegmentedControls() {
-    const [selectedValue, setSelectedValue] = useState('value1')
-    const handleChange = (e) => {
-        setSelectedValue(e.detail.selectedValue);
-    }
-
     return (
         <div>
-            <IfxSegmentedControlGroup 
-            size='regular' 
-            selectedValue={selectedValue}
-            onIfxChange={(e) => {handleChange(e)}}>
-                <IfxSegmentedControl icon='star16' value='value1'> Label </IfxSegmentedControl>
+            <IfxSegmentedControlGroup caption='Caption text to describe the controls' groupLabel='Group Label' size='regular'>
+                <IfxSegmentedControl selected={true} icon='star16' value='value1'> Label </IfxSegmentedControl>
                 <IfxSegmentedControl icon='star16' value='value2'> Label </IfxSegmentedControl>
                 <IfxSegmentedControl icon='star16' value='value3'> Label </IfxSegmentedControl>
                 <IfxSegmentedControl icon='star16' value='value4'> Label </IfxSegmentedControl>
--- examples/wrapper-components/vue-javascript/src/components/SegmentedControls.vue
+++ examples/wrapper-components/vue-javascript/src/components/SegmentedControls.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
       <h2>Segmented Controls</h2>
-      <ifx-segmented-control-group :selected-value='selectedValue' @ifxChange="handleIfxChange">
-        <ifx-segmented-control icon='star-16' value='value1'> Label </ifx-segmented-control>
+      <ifx-segmented-control-group caption='Caption text to describe the controls' group-label='Group Label' size='regular'>
+        <ifx-segmented-control selected='true' icon='star-16' value='value1'> Label </ifx-segmented-control>
         <ifx-segmented-control icon='star-16' value='value2'> Label </ifx-segmented-control>
         <ifx-segmented-control icon='star-16' value='value3'> Label </ifx-segmented-control>
         <ifx-segmented-control icon='star-16' value='value4'> Label </ifx-segmented-control>
@@ -10,17 +10,6 @@
       <br />
       <br />
     </div>
-  </template>
-  
-  <script  setup>
-  import { ref } from 'vue'
-  let selectedValue = ref('value1');
-  
-  
-  function handleIfxChange(event) {
-    selectedValue.value = event.detail.selectedValue;
-  }
-  
-  </script>
+</template>
   
   <style scoped></style>
