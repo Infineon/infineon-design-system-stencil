50085c7500644eacd8696be303721daa529ec399
verena-ifx <verena.lechner@infineon.com>
2024-10-28 14:15:55 +0100

    fix(1560): add cdn link to css file

--- examples/stencil-components/vanilla-cdn/update-link.js
+++ examples/stencil-components/vanilla-cdn/update-link.js
@@ -1,27 +1,34 @@
 const fs = require('fs');
 const path = require('path');
 
+// Path to the index.html file
 const indexHtmlPath = path.join(__dirname, '/index.html');
+
+// Determine if the current branch is master
 const isMaster = process.env.GITHUB_REF === 'refs/heads/master';
 
-console.log("is local", process.env.GITHUB_REF === undefined);
-
+// Read the content of index.html
 const htmlContent = fs.readFileSync(indexHtmlPath, 'utf-8');
 
-const scriptRegex = /<\s*script\s+type="module"\s+src\s*=\s*['"][^'"]*infineon-design-system-stencil[^'"]*\.esm\.js['"]\s*><\/\s*script\s*>/gis;
-const stylesheetRegex = /<\s*script\s+type="module"\s+src\s*=\s*['"][^'"]*infineon-design-system-stencil[^'"]*\.css['"]\s*><\/\s*script\s*>/gis;
+// Define regex patterns to match both local dist and CDN script tags for JS and CSS files
+const scriptRegex = /<\s*script[^>]+src\s*=\s*['"]\s?(https:\/\/cdn\.jsdelivr\.net\/npm\/@infineon\/infineon-design-system-stencil[^'" ]*\.esm\.js|\.\/dist\/infineon-design-system-stencil\/infineon-design-system-stencil\.esm\.js)\s*["'][^>]*><\/script>/gis;
+const stylesheetRegex = /<\s*script[^>]+src\s*=\s*['"]\s?(https:\/\/cdn\.jsdelivr\.net\/npm\/@infineon\/infineon-design-system-stencil[^'" ]*\.css|\.\/dist\/infineon-design-system-stencil\/infineon-design-system-stencil\.css)\s*["'][^>]*><\/script>/gis;
 
+// Load the current version from the package.json file
 const version = require('../../../packages/components/package.json').version;
 
+// Define the CDN links
 const cdnLinkLatestJS = '<script type="module" src="https://cdn.jsdelivr.net/npm/@infineon/infineon-design-system-stencil/dist/infineon-design-system-stencil/infineon-design-system-stencil.esm.js"></script>';
 const cdnLinkCanaryJS = `<script type="module" src="https://cdn.jsdelivr.net/npm/@infineon/infineon-design-system-stencil@${version}/dist/infineon-design-system-stencil/infineon-design-system-stencil.esm.js"></script>`;
 
 const cdnLinkLatestCSS = '<script type="module" src="https://cdn.jsdelivr.net/npm/@infineon/infineon-design-system-stencil/dist/infineon-design-system-stencil/infineon-design-system-stencil.css"></script>';
 const cdnLinkCanaryCSS = `<script type="module" src="https://cdn.jsdelivr.net/npm/@infineon/infineon-design-system-stencil@${version}/dist/infineon-design-system-stencil/infineon-design-system-stencil.css"></script>`;
 
+// Define the local links
 const localLinkJS = '<script type="module" src="./dist/infineon-design-system-stencil/infineon-design-system-stencil.esm.js"></script>';
 const localLinkCSS = '<script type="module" src="./dist/infineon-design-system-stencil/infineon-design-system-stencil.css"></script>';
 
+// Determine the new source links based on the environment
 let newScriptSrc;
 let newStylesheetSrc;
 
@@ -38,10 +45,12 @@ if (process.env.GITHUB_REF === undefined) {
 console.log("newScriptSrc: ", newScriptSrc);
 console.log("newStylesheetSrc: ", newStylesheetSrc);
 
+// Replace local dist or CDN links with the appropriate new CDN links in the HTML content
 const updatedHtmlContent = htmlContent
   .replace(scriptRegex, newScriptSrc)
   .replace(stylesheetRegex, newStylesheetSrc);
 
+// Write the updated content back to index.html
 fs.writeFileSync(indexHtmlPath, updatedHtmlContent, 'utf-8');
 
 console.log("updated JS link: ", newScriptSrc);
