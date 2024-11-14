e868246e8bb0036aeabfaa7e2333fbe349c1e7ab
tishoyanchev <108734092+tishoyanchev@users.noreply.github.com>
2024-11-14 09:39:53 +0000

    Deploying to gh-pages from @ Infineon/infineon-design-system-stencil@60bb706dfee3642b926552dd07f2c34687811f71 🚀

--- react-example/assets/ifx-list-group.entry-D9NRoqnv.js
+++
@@ -1,3 +0,0 @@
-import{r as s,h as t,H as i,g as l}from"./index-BjwGDkwP.js";const n=":host{display:inline-flex}.list-group-container{display:inline-flex;flex-direction:column;gap:3px}",o=n,a=class{constructor(e){s(this,e),this.flush=!1,this.bulletpoint=!1,this.notification=!1}render(){return t(i,{key:"975fdc738715608c8b73834a2d10f5403c5bd375"},t("div",{key:"bb47b532a4c78049da6f22f9085725734f588474",class:`list-group-container 
-        ${this.flush?"flush":""} 
-        ${this.bulletpoint?"bulletpoint":""}`},t("slot",{key:"44de49a1def244c898e03a59602269a7d739cf4f",name:"list-item"}),t("slot",{key:"d77ee5166ea242b0c232467f8e873ea1dd1913c9",name:"list-notification"})))}get el(){return l(this)}};a.style=o;export{a as ifx_list_group};
