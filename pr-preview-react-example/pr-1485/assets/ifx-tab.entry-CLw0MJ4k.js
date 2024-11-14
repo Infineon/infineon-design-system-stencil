c9227ff69ddbb52a387f836adf00757e59bd7de2
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2024-11-14 08:19:24 +0000

    Deploy preview for PR 1485 🛫

--- pr-preview-react-example/pr-1485/assets/ifx-tab.entry-CLw0MJ4k.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as s,g as i}from"./index-DjC80i5L.js";const n=":host{display:block}",a=n,o=class{constructor(t){e(this,t),this.header=void 0,this.disabled=void 0,this.icon="",this.iconPosition="left"}render(){return s("slot",{key:"818debbe43aa58861b8bda559332ef1b9043a132"})}get el(){return i(this)}};o.style=a;export{o as ifx_tab};
