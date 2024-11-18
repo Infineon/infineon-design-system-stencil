9ee45f1584031adaa0b9336e505570ade4f4eb21
evangeliaPanteliadou <evangelia.panteliadou@infineon.com>
2024-11-18 09:14:17 +0100

    Enhance UI controls and descriptions in Number Indicator stories

--- packages/components/src/components/number-indicator/number-indicator.stories.ts
+++ packages/components/src/components/number-indicator/number-indicator.stories.ts
@@ -6,6 +6,24 @@ export default {
     label: 1,
     inverted: false,
   },
+  argTypes: {
+    label: {
+      description: 'Specifies the number to be displayed within the indicator.',
+      table: {
+        category: 'ifx-number-indicator props'
+      }
+    },
+    inverted: {
+      description: 'Toggles the color scheme of the number indicator.',
+      control: 'boolean',
+      table: {
+        category: 'ifx-number-indicator props',
+        defautlValue: {
+          summary: false
+        }
+      }
+    }
+  }
 };
 
 const DefaultTemplate = args => `<ifx-number-indicator inverted="${args.inverted}">${args.label}</ifx-number-indicator>`;
