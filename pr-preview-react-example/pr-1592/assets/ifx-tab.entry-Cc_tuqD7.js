4f5161c1dc06da6d139808c3713d5017e98e38fc
tishoyanchev <tishoyanchev@users.noreply.github.com>
2024-11-18 13:39:16 +0000

    Deploy preview for PR 1592 🛫

--- pr-preview-react-example/pr-1592/assets/ifx-tab.entry-Cc_tuqD7.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as s,g as a}from"./index-MwURpkYO.js";const i=":host{display:block}",n=i,o=class{constructor(t){e(this,t),this.header=void 0,this.disabled=void 0,this.icon="",this.iconPosition="left"}render(){return s("slot",{key:"aa3d7726d95d6c3977aeabff35595a41905625db"})}get el(){return a(this)}};o.style=n;export{o as ifx_tab};
