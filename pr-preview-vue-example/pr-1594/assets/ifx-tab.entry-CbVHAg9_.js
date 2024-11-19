ee908d6c16cb8663b06beb3bc4132d825f1eac39
akashyeole <akashyeole@users.noreply.github.com>
2024-11-19 06:03:55 +0000

    Deploy preview for PR 1594 🛫

---
+++ pr-preview-vue-example/pr-1594/assets/ifx-tab.entry-CbVHAg9_.js
@@ -0,0 +1,1 @@
+import{r as e,h as s,g as a}from"./index-BWU4QzNZ.js";const i=":host{display:block}",n=i,o=class{constructor(t){e(this,t),this.header=void 0,this.disabled=void 0,this.icon="",this.iconPosition="left"}render(){return s("slot",{key:"aa3d7726d95d6c3977aeabff35595a41905625db"})}get el(){return a(this)}};o.style=n;export{o as ifx_tab};
