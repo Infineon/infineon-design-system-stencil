5ab5cd4289d385d49e0c21b9a25dc4343db15185
tishoyanchev <tishoyanchev@users.noreply.github.com>
2024-12-06 09:13:47 +0000

    Deploy preview for PR 1630 🛫

--- pr-preview-vue-example/pr-1630/assets/ifx-list-group.entry-S_dAUqqE.js
+++
@@ -1,3 +0,0 @@
-import{r as s,h as e,H as i,g as l}from"./index-Dc0Yc3BU.js";const a=":host{display:inline-flex}.list-group-container{display:inline-flex;flex-direction:column;gap:3px}",n=a,o=class{constructor(t){s(this,t),this.flush=!1,this.bulletpoint=!1,this.notification=!1}render(){return e(i,{key:"5d89f65af5f1b8e1f78b88f70065c55281eb8c27"},e("div",{key:"0ae653b5307044468d507ee9be6539a430c2103c",class:`list-group-container 
-        ${this.flush?"flush":""} 
-        ${this.bulletpoint?"bulletpoint":""}`},e("slot",{key:"d7cd601c0b1bfd63aa5dee3f4f657082fa1e30e3",name:"list-item"}),e("slot",{key:"77efa54a094a8f97081f0d2dd26e527e96a3d79f",name:"list-notification"})))}get el(){return l(this)}};o.style=n;export{o as ifx_list_group};
