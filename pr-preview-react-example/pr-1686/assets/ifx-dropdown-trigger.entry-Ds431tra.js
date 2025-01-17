c9980a11490bb231a8680c1de49244381781b3b1
evangeliaPanteliadou <evangeliaPanteliadou@users.noreply.github.com>
2025-01-17 09:24:40 +0000

    Deploy preview for PR 1686 🛫

---
+++ pr-preview-react-example/pr-1686/assets/ifx-dropdown-trigger.entry-Ds431tra.js
@@ -0,0 +1,1 @@
+import{r as e,h as o}from"./index-BUjtH2tH.js";const s=":host{display:inline-block;cursor:pointer}",t=s,n=class{constructor(r){e(this,r),this.isOpen=!1}render(){return o("slot",{key:"3e51ceb827b5c52cb850e6433d3235e9400fe5c0"})}};n.style=t;export{n as ifx_dropdown_trigger};
