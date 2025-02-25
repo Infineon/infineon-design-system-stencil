b4a89c9e4b59b4e1f85d58b5436a95e967502db5
tishoyanchev <tishoyanchev@users.noreply.github.com>
2025-02-25 22:34:02 +0000

    Deploy preview for PR 1743 🛫

--- pr-preview-react-example/pr-1743/assets/ifx-download.entry-CWsusRcm.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as s}from"./index-D4z6enI5.js";const n="",t=n,a=class{constructor(o){e(this,o),this.tokens="css"}render(){const o=`infineon-tokens.${this.tokens}`;return s("a",{key:"bc1891eac53959231702ef4d20f8d21151a9c921",href:`/tokens/${o}`,download:o,class:"download__wrapper"},s("slot",{key:"7f524281dcf67529bc7fcb859d5b555801b15e73"}))}};a.style=t;export{a as ifx_download};
