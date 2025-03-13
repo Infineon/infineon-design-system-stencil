5681cc62b46e45f093c47fe0c8c75788d8b1c97a
Yanchev Tihomir (IT ERP DXT / EE) <Tihomir.Yanchev-EE@infineon.com>
2025-03-13 15:47:35 +0100

    fixed paths

--- packages/components/stencil.config.ts
+++ packages/components/stencil.config.ts
@@ -22,11 +22,6 @@ export const config: Config = {
       type: 'dist',
       esmLoaderPath: '../loader',
       copy: [
-        { 
-          src: './public-storybook/tokens', 
-          dest: 'storybook/tokens', 
-          warn: true 
-        },
         { src: '../../../node_modules/@infineon/design-system-tokens/dist/fonts', dest: './', warn: true },
       ]
     },
@@ -38,6 +33,11 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
        copy: [
+        { 
+          src: '../public-storybook/tokens', 
+          dest: 'storybook/tokens', 
+          warn: true 
+        },
         { src: '../../../node_modules/@infineon/design-system-tokens/dist/fonts', dest: './build/fonts', warn: true },
       ]
     }
