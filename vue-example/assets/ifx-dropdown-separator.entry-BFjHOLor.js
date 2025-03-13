104424271b199066ede155de85ed9cc7971e75e6
tishoyanchev <108734092+tishoyanchev@users.noreply.github.com>
2025-03-13 09:52:16 +0000

    Deploying to gh-pages from @ Infineon/infineon-design-system-stencil@140bb7be4f2b45db7645661ab079b5ed088c6b58 🚀

--- vue-example/assets/ifx-dropdown-separator.entry-BFjHOLor.js
+++
@@ -1,1 +0,0 @@
-import{r as o,h as e}from"./index-1mBPAEDX.js";const a=".dropdown-separator{height:1px;background-color:#EEEDED;margin:8px 16px}",t=a,s=class{constructor(r){o(this,r)}render(){return e("div",{key:"5d982d5c92e0d2d3064ebca6403eee845790d61e",class:"dropdown-separator"})}};s.style=t;export{s as ifx_dropdown_separator};
---
+++ vue-example/assets/ifx-dropdown-separator.entry-DHZBumu0.js
@@ -0,0 +1,1 @@
+import{r as o,h as e}from"./index-CAmdWVKc.js";const a=".dropdown-separator{height:1px;background-color:#EEEDED;margin:8px 16px}",t=a,s=class{constructor(r){o(this,r)}render(){return e("div",{key:"5d982d5c92e0d2d3064ebca6403eee845790d61e",class:"dropdown-separator"})}};s.style=t;export{s as ifx_dropdown_separator};
