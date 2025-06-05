8bc022a4bc50cc975cf8eb79e775d8415ba560eb
Yanchev Tihomir (IT ERP CDX / EE) <Tihomir.Yanchev-EE@infineon.com>
2025-06-05 11:37:36 +0200

    commiting latest update with an error buildtarget required

--- packages/components-angular/projects/my-app/src/app/app.module.ts
+++ packages/components-angular/projects/my-app/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { ComponentLibraryModule } from '@infineon/infineon-design-system-angular';
 
 @NgModule({
   declarations: [
@@ -13,7 +12,6 @@ import { ComponentLibraryModule } from '@infineon/infineon-design-system-angular
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ComponentLibraryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
