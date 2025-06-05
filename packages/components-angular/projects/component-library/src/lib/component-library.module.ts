928ea4ee205ff1b9e58f698956fbd087b537035c
Yanchev Tihomir (IT ERP CDX / EE) <Tihomir.Yanchev-EE@infineon.com>
2025-06-05 15:05:39 +0200

    clean install

--- packages/components-angular/projects/component-library/src/lib/component-library.module.ts
+++ packages/components-angular/projects/component-library/src/lib/component-library.module.ts
@@ -1,5 +1,4 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
-//import { DIRECTIVES } from './stencil-generated';
 import { defineCustomElements } from '@infineon/infineon-design-system-stencil/loader';
 
 
