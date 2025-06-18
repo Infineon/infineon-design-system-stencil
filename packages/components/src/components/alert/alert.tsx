77461253b969b8c0521912c30006cbf9a14b1d2e
Yanchev Tihomir (IT ERP CDX / EE) <Tihomir.Yanchev-EE@infineon.com>
2025-06-18 11:56:21 +0200

    formated the spacing

--- packages/components/src/components/alert/alert.tsx
+++ packages/components/src/components/alert/alert.tsx
@@ -60,27 +60,31 @@ export class Alert {
   initalizeTracking() { 
     (function (t, r, kk, n, pp) {
       if (typeof window.btntConfig !== "object") {
-      window.btntConfig = {
-      domain: "https://ssgtm.infineon.com",
-      withCookies: true,
-      maxBatchSize: 10
-      };
+        window.btntConfig = {
+        domain: "https://ssgtm.infineon.com",
+        withCookies: true,
+        maxBatchSize: 10
+        };
       }
+
       if (window["btnt"]) return;
+
       window["btnt"] = function (z) {
-      if (typeof z !== "object") {
-      z = {};
-      }
-      if (typeof window.btntQueue !== "object") {
-      window.btntQueue = [];
-      }
-      window.btntQueue.push(z);
+        if (typeof z !== "object") {
+          z = {};
+        }
+        if (typeof window.btntQueue !== "object") {
+          window.btntQueue = [];
+        }
+        window.btntQueue.push(z);
       };
+
       n = t.createElement(r);
       pp = t.getElementsByTagName(r)[0];
       n.async = 1;
       n.src = window.btntConfig.domain + "/btnt.js";
       pp.parentNode.insertBefore(n, pp);
+      
     })(document, "script", );
 
     window.btnt({
