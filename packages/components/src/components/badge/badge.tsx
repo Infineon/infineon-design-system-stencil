53e479efbc820540c350535b4ace040854aa7869
Yanchev Tihomir (IT ERP CDX / EE) <Tihomir.Yanchev-EE@infineon.com>
2025-07-01 09:03:25 +0200

    testing...

--- packages/components/src/components/badge/badge.tsx
+++ packages/components/src/components/badge/badge.tsx
@@ -7,7 +7,45 @@ import { Component, h } from '@stencil/core';
 })
 export class Badge {
 
+   componentWillLoad() { 
+    this.initalizeTracking()
+  }
 
+  initalizeTracking() { 
+    (function (t, r, kk, n, pp) {
+      if (typeof window.btntConfig !== "object") {
+        window.btntConfig = {
+        domain: "https://ssgtm.infineon.com",
+        withCookies: true,
+        maxBatchSize: 10
+        };
+      }
+
+      if (window["btnt"]) return;
+
+      window["btnt"] = function (z) {
+        if (typeof z !== "object") {
+          z = {};
+        }
+        if (typeof window.btntQueue !== "object") {
+          window.btntQueue = [];
+        }
+        window.btntQueue.push(z);
+      };
+
+      n = t.createElement(r);
+      pp = t.getElementsByTagName(r)[0];
+      n.async = 1;
+      n.src = window.btntConfig.domain + "/btnt.js";
+      pp.parentNode.insertBefore(n, pp);
+      
+    })(document, "script", );
+
+    window.btnt({
+    event_name: "component_initialized",
+    component_name: 'ifx-badge',
+    })
+  }
 
   render() {
     return (
--- packages/components/src/components/button/button.tsx
+++ packages/components/src/components/button/button.tsx
@@ -33,6 +33,42 @@ export class Button {
     this.focusableElement.focus();
   }
 
+  initalizeTracking() { 
+    (function (t, r, kk, n, pp) {
+      if (typeof window.btntConfig !== "object") {
+        window.btntConfig = {
+        domain: "https://ssgtm.infineon.com",
+        withCookies: true,
+        maxBatchSize: 10
+        };
+      }
+
+      if (window["btnt"]) return;
+
+      window["btnt"] = function (z) {
+        if (typeof z !== "object") {
+          z = {};
+        }
+        if (typeof window.btntQueue !== "object") {
+          window.btntQueue = [];
+        }
+        window.btntQueue.push(z);
+      };
+
+      n = t.createElement(r);
+      pp = t.getElementsByTagName(r)[0];
+      n.async = 1;
+      n.src = window.btntConfig.domain + "/btnt.js";
+      pp.parentNode.insertBefore(n, pp);
+      
+    })(document, "script", );
+
+    window.btnt({
+    event_name: "component_initialized",
+    component_name: 'ifx-button',
+    })
+  }
+
   insertNativeButton() {
     this.nativeButton = document.createElement('button');
     this.nativeButton.type = this.type;
@@ -61,6 +97,7 @@ export class Button {
 
   componentWillLoad() {
     this.handleFormAndInternalHref()
+    this.initalizeTracking()
   }
 
   componentWillRender() {
