21f87667955cc4785dcd821eedc1e736892529f3
Yanchev Tihomir (IT ERP CDX / EE) <Tihomir.Yanchev-EE@infineon.com>
2025-07-21 21:58:13 +0200

    adds vue verification

--- packages/components/src/global/utils/framework-detection.ts
+++ packages/components/src/global/utils/framework-detection.ts
@@ -10,35 +10,25 @@ export const detectFramework = (): string => {
     return 'React'
   }
   
-  if(!!document.querySelector('script[id=__NEXT_DATA__]')) {
-    console.log('Next.js');
-  }
  
-
-  if(!!document.querySelector('[id=___gatsby]')) {
-    console.log('Gatsby.js');
-  }
- 
-
   if(!!win.angular ||
     !!document.querySelector('.ng-binding, [ng-app], [data-ng-app], [ng-controller], [data-ng-controller], [ng-repeat], [data-ng-repeat]') ||
     !!document.querySelector('script[src*="angular.js"], script[src*="angular.min.js"]')
   ) {
     console.log('Angular.js');
+    return 'Angular'
   }
   
 
   if (!!win.getAllAngularRootElements || !!win.ng?.coreTokens?.NgZone) {
     console.log('Angular');
+    return 'Angular'
   }
   
-
-  if(!!win.Backbone) console.log('Backbone.js');
-  if(!!win.Ember) console.log('Ember.js');
-  if(!!win.__VUE__) console.log('Vue.js');
-  if(!!win.Meteor) console.log('Meteor.js');
-  if(!!win.Zepto) console.log('Zepto.js');
-  if(!!win.jQuery) console.log('jQuery.js');
+  if(!!win.__VUE__) {
+    console.log('Vue.js');
+    return 'Vue'
+  }
 
   console.log('vanilla')
   return 'vanilla'
