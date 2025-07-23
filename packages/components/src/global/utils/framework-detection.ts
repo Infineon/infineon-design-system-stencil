d035a0b6fcf9a9f4e4df28c8cc540887d265ec0b
Yanchev Tihomir (IT ERP CDX / EE) <Tihomir.Yanchev-EE@infineon.com>
2025-07-23 13:20:22 +0200

    adds angular framework detection code

--- packages/components/src/global/utils/framework-detection.ts
+++ packages/components/src/global/utils/framework-detection.ts
@@ -8,18 +8,14 @@ export const detectFramework = (): string => {
   }
   
  
-  if(!!win.angular ||
-    !!document.querySelector('.ng-binding, [ng-app], [data-ng-app], [ng-controller], [data-ng-controller], [ng-repeat], [data-ng-repeat]') ||
-    !!document.querySelector('script[src*="angular.js"], script[src*="angular.min.js"]')
-  ) {
-    return 'Angular'
-  }
+  if(!!win.getAllAngularRootElements || 
+   !!document.querySelector('[ng-version], [ng-app], [data-ng-app], [ng-controller], [data-ng-controller], [ng-repeat], [data-ng-repeat]') ||
+   !!document.querySelector('script[src*="angular.js"], script[src*="angular.min.js"]')
+) {
+  return 'Angular'
+}
   
 
-  if (!!win.getAllAngularRootElements || !!win.ng?.coreTokens?.NgZone) {
-    return 'Angular'
-  }
-  
   if(!!win.__VUE__) {
     return 'Vue'
   }
