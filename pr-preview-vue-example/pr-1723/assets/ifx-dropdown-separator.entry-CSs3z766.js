510021adc1e867113969fe509d0d0a2c67907f3a
evangeliaPanteliadou <evangeliaPanteliadou@users.noreply.github.com>
2025-07-29 11:01:41 +0000

    Deploy preview for PR 1723 🛫

--- pr-preview-vue-example/pr-1723/assets/ifx-dropdown-separator.entry-CSs3z766.js
+++
@@ -1,1 +0,0 @@
-import{r as o,h as a}from"./index-Dnx1G-qj.js";const t=".dropdown-separator{height:1px;background-color:#EEEDED;margin:8px 16px}",s=t,e=class{constructor(r){o(this,r)}render(){return a("div",{key:"4909b1b593f932f2a9492f06f699731c69d6dc0a",class:"dropdown-separator"})}};e.style=s;export{e as ifx_dropdown_separator};
