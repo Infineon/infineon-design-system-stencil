4134e4760b75a9d9729f3dbfc7267509233ad8ff
verena-ifx <93577764+verena-ifx@users.noreply.github.com>
2025-08-01 13:56:40 +0200

    Delete pr-preview-vue-example/pr-1358 directory

--- pr-preview-vue-example/pr-1358/assets/ifx-tab.entry-B7Pu840h.js
+++
@@ -1,1 +0,0 @@
-import{r as t,h as s,g as i}from"./index-o_ta55vV.js";const n=":host{display:block}",a=n,o=class{constructor(e){t(this,e),this.header=void 0,this.disabled=void 0,this.icon="",this.iconPosition="left"}render(){return s("slot",{key:"e6e88ae6a726c371c8ea4965516add63013e8e14"})}get el(){return i(this)}};o.style=a;export{o as ifx_tab};
