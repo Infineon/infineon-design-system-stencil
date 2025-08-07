cf2d445f6cf8f12a5d2efe86c89da9fee3b2ebae
evangeliaPanteliadou <evangelia.panteliadou@infineon.com>
2025-08-07 13:07:13 +0200

    update Button component to latest version and add toggle buttons to be able to test different states - Angular example app

--- packages/components-angular/projects/my-app/src/app/components/button/button.component.ts
+++ packages/components-angular/projects/my-app/src/app/components/button/button.component.ts
@@ -14,5 +14,42 @@ import { ComponentLibraryModule } from '@infineon/infineon-design-system-angular
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
+  buttonVariant = "primary";
+  buttonVariants = ["primary", "secondary", "tertiary"];
+  buttonVariantIndex = 0;
+
+  buttonTheme = "default";
+  buttonThemes = ["default", "danger", "inverse"];
+  buttonThemeIndex = 0;
+
+  buttonSize = "m";
+  buttonSizes = ["xs", "s", "m", "l"];
+  buttonSizeIndex = 0;
+
+  buttonFullWidth = false;
+  buttonDisabled = false;
+
+  toggleButtonVariant() {
+    this.buttonVariantIndex = (this.buttonVariantIndex + 1) % this.buttonVariants.length;
+    this.buttonVariant = this.buttonVariants[this.buttonVariantIndex];
+  }
+
+  toggleButtonTheme() {
+    this.buttonThemeIndex = (this.buttonThemeIndex + 1) % this.buttonThemes.length;
+    this.buttonTheme = this.buttonThemes[this.buttonThemeIndex];
+  }
+
+  toggleButtonSize() {
+    this.buttonSizeIndex = (this.buttonSizeIndex + 1) % this.buttonSizes.length;
+    this.buttonSize = this.buttonSizes[this.buttonSizeIndex];
+  }
+
+  toggleButtonFullWidth() {
+    this.buttonFullWidth = !this.buttonFullWidth;
+  }
+
+  toggleButtonDisabled() {
+    this.buttonDisabled = !this.buttonDisabled;
+  }
 
 }
