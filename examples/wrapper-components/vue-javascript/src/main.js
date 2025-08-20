ad6ffd047021e82cdc97387ce01d04ae31466be3
Yanchev Tihomir (IT ERP CDX / EE) <Tihomir.Yanchev-EE@infineon.com>
2025-08-20 15:54:18 +0200

    reverts back changes to main.js in vue

--- examples/wrapper-components/vue-javascript/src/main.js
+++ examples/wrapper-components/vue-javascript/src/main.js
@@ -1,9 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { ComponentLibrary, defineCustomElements } from '@infineon/infineon-design-system-vue';
+import { ComponentLibrary } from '@infineon/infineon-design-system-vue';
 import './assets/base.scss'
-
-// Define custom elements
-defineCustomElements();
 
 createApp(App).use(ComponentLibrary).mount('#app');
