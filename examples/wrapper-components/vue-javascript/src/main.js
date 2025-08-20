bf5f3422f8aaedb51ecb815058880804e4515316
Yanchev Tihomir (IT ERP CDX / EE) <Tihomir.Yanchev-EE@infineon.com>
2025-08-20 15:45:09 +0200

    adds defineCustomElements in vue entry file for testing purposes

--- examples/wrapper-components/vue-javascript/src/main.js
+++ examples/wrapper-components/vue-javascript/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { ComponentLibrary } from '@infineon/infineon-design-system-vue';
+import { ComponentLibrary, defineCustomElements } from '@infineon/infineon-design-system-vue';
 import './assets/base.scss'
+
+// Define custom elements
+defineCustomElements();
 
 createApp(App).use(ComponentLibrary).mount('#app');
