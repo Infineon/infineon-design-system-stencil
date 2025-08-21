55a7ca21a36fc3b062e2643e8da6e9f6fa2c2a20
Yanchev Tihomir (IT ERP CDX / EE) <Tihomir.Yanchev-EE@infineon.com>
2025-08-21 10:26:33 +0200

    adds defineCustomElements in vue entry file for testing purposes

--- examples/wrapper-components/vue-javascript/src/main.js
+++ examples/wrapper-components/vue-javascript/src/main.js
@@ -3,4 +3,12 @@ import App from './App.vue'
 import { ComponentLibrary } from '@infineon/infineon-design-system-vue';
 import './assets/base.scss'
 
+import { defineCustomElements } from '@infineon/infineon-design-system-stencil/loader';
+import { setAssetPath } from '@stencil/core';
+
+setAssetPath(new URL(import.meta.env.BASE_URL, window.location.href).href);
+
+// Ensure Stencil components are defined; this also brings the chunks into the Vite build
+defineCustomElements();
+
 createApp(App).use(ComponentLibrary).mount('#app');
