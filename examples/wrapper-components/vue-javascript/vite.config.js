ca423947bd3abc762a80c58824848b4e6d945432
verena-ifx <verena.lechner@infineon.com>
2025-08-21 11:33:33 +0200

    1684: disabling code splitting in vite.config

--- examples/wrapper-components/vue-javascript/vite.config.js
+++ examples/wrapper-components/vue-javascript/vite.config.js
@@ -5,10 +5,17 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: "./",
   exclude: [
     '@ionic/core/loader' //fix weird Vite error "outdated optimize dep"
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined // Disable code splitting for dynamic imports
+      }
+    }
+  },
   plugins: [
     vue({
       template: {
