5db46c7d8c003d981c6b44406deb9d541a16ce13
evangeliaPanteliadou <evangelia.panteliadou@infineon.com>
2025-08-21 12:46:13 +0200

    moving footer from static to nav-item

--- packages/components-angular/projects/my-app/src/app/app.routes.ts
+++ packages/components-angular/projects/my-app/src/app/app.routes.ts
@@ -13,13 +13,14 @@ import { ContentSwitcherComponent } from './components/content-switcher/content-
 import { DatePickerComponent }      from './components/date-picker/date-picker.component';
 import { DropdownComponent }        from './components/dropdown/dropdown.component';
 import { FileUpload }               from './components/file-upload/file-upload';
+import { FooterComponent }                   from './components/footer/footer.component';
 import { IconComponent }            from './components/icon/icon.component';
 import { IconButtonComponent }      from './components/icon-button/icon-button.component';
 import { LinkComponent }            from './components/link/link.component';
 import { ModalComponent }           from './components/modal/modal.component';
 import { MultiSelectComponent }     from './components/multi-select/multi-select.component';
 import { NotificationComponent }    from './components/notification/notification.component';
-import { IndicatorComponent } from './components/indicator/indicator.component';
+import { IndicatorComponent }       from './components/indicator/indicator.component';
 import { PaginationComponent }      from './components/pagination/pagination.component';
 import { ProgressBarComponent }     from './components/progress-bar/progress-bar.component';
 import { RadioButtonComponent }     from './components/radio-button/radio-button.component';
@@ -44,48 +45,49 @@ import { TooltipComponent }         from './components/tooltip/tooltip.component
 import { TreeView }                 from './components/tree-view/tree-view';
 
 export const routes: Route[] = [
-  { path: 'accordion',       component: AccordionComponent },
-  { path: 'alert',           component: AlertComponent },
-  { path: 'badge',           component: BadgeComponent },
-  { path: 'breadcrumb',      component: BreadcrumbComponent },
-  { path: 'button',          component: ButtonComponent },
-  { path: 'card',            component: CardComponent },
-  { path: 'checkbox',        component: CheckboxComponent },
-  { path: 'checkboxgroup',  component: CheckboxGroupComponent },
-  { path: 'chip',            component: ChipComponent },
-  { path: 'contentswitcher',component: ContentSwitcherComponent },
-  { path: 'datepicker',     component: DatePickerComponent },
-  { path: 'dropdown',        component: DropdownComponent },
+  { path: 'accordion',        component: AccordionComponent },
+  { path: 'alert',            component: AlertComponent },
+  { path: 'badge',            component: BadgeComponent },
+  { path: 'breadcrumb',       component: BreadcrumbComponent },
+  { path: 'button',           component: ButtonComponent },
+  { path: 'card',             component: CardComponent },
+  { path: 'checkbox',         component: CheckboxComponent },
+  { path: 'checkboxgroup',    component: CheckboxGroupComponent },
+  { path: 'chip',             component: ChipComponent },
+  { path: 'contentswitcher',  component: ContentSwitcherComponent },
+  { path: 'datepicker',       component: DatePickerComponent },
+  { path: 'dropdown',         component: DropdownComponent },
   { path: 'fileupload',       component: FileUpload},
-  { path: 'icon',            component: IconComponent },
-  { path: 'iconbutton',     component: IconButtonComponent },
-  { path: 'link',            component: LinkComponent },
-  { path: 'modal',           component: ModalComponent },
-  { path: 'multiselect',    component: MultiSelectComponent },
-  { path: 'notification',    component: NotificationComponent },
-  { path: 'indicator',      component: IndicatorComponent },
-  { path: 'pagination',      component: PaginationComponent },
-  { path: 'progressbar',    component: ProgressBarComponent },
-  { path: 'radiobutton',    component: RadioButtonComponent },
+  { path: 'footer',           component: FooterComponent},
+  { path: 'icon',             component: IconComponent },
+  { path: 'iconbutton',       component: IconButtonComponent },
+  { path: 'link',             component: LinkComponent },
+  { path: 'modal',            component: ModalComponent },
+  { path: 'multiselect',      component: MultiSelectComponent },
+  { path: 'notification',     component: NotificationComponent },
+  { path: 'indicator',        component: IndicatorComponent },
+  { path: 'pagination',       component: PaginationComponent },
+  { path: 'progressbar',      component: ProgressBarComponent },
+  { path: 'radiobutton',      component: RadioButtonComponent },
   { path: 'radiobuttongroup', component: RadioButtonGroupComponent },
-  { path: 'searchbar',      component: SearchBarComponent },
-  { path: 'searchfield',    component: SearchFieldComponent },
-  { path: 'segmentedcontrol',component: SegmentedControlComponent },
-  { path: 'sidebar',         component: SidebarComponent },
-  { path: 'singleselect',   component: SingleSelectComponent },
-  { path: 'slider',          component: SliderComponent },
-  { path: 'spinner',         component: SpinnerComponent },
-  { path: 'status',          component: StatusComponent },
-  { path: 'stepper',         component: StepperComponent },
-  { path: 'switch',          component: SwitchComponent },
-  { path: 'tabs',            component: TabsComponent },
-  { path: 'advancedtable',  component: AdvancedTableComponent },
-  { path: 'basictable',     component: BasicTableComponent },
-  { path: 'tag',             component: TagComponent },
-  { path: 'textfield',      component: TextFieldComponent },
-  { path: 'textarea',        component: TextareaComponent },
-  { path: 'tooltip',         component: TooltipComponent },
-  { path: 'treeview',     component: TreeView },
+  { path: 'searchbar',        component: SearchBarComponent },
+  { path: 'searchfield',      component: SearchFieldComponent },
+  { path: 'segmentedcontrol', component: SegmentedControlComponent },
+  { path: 'sidebar',          component: SidebarComponent },
+  { path: 'singleselect',     component: SingleSelectComponent },
+  { path: 'slider',           component: SliderComponent },
+  { path: 'spinner',          component: SpinnerComponent },
+  { path: 'status',           component: StatusComponent },
+  { path: 'stepper',          component: StepperComponent },
+  { path: 'switch',           component: SwitchComponent },
+  { path: 'tabs',             component: TabsComponent },
+  { path: 'advancedtable',    component: AdvancedTableComponent },
+  { path: 'basictable',       component: BasicTableComponent },
+  { path: 'tag',              component: TagComponent },
+  { path: 'textfield',        component: TextFieldComponent },
+  { path: 'textarea',         component: TextareaComponent },
+  { path: 'tooltip',          component: TooltipComponent },
+  { path: 'treeview',         component: TreeView },
 
   { path: '',   redirectTo: 'accordion', pathMatch: 'full' },
   { path: '**', redirectTo: 'accordion' }
