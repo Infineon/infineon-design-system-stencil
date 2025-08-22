676caeeb3fdca2a1e9b5939399f1f2c88adc79f4
verena-ifx <verena.lechner@infineon.com>
2025-08-22 13:37:17 +0200

    1986: using ncp command for the copy font script

--- packages/components/utils/copy-fonts.js
+++ packages/components/utils/copy-fonts.js
@@ -1,30 +1,21 @@
+const ncp = require('ncp').ncp;
 const path = require('path');
 const fs = require('fs');
-const { promisify } = require('util');
-const ncp = promisify(require('ncp').ncp);
 
-// Use absolute paths for maximum reliability
 const src = path.resolve(__dirname, '../node_modules/@infineon/design-system-tokens/dist/fonts');
 const dest = path.resolve(__dirname, '../www/build/fonts');
 
-console.log(`Copying fonts from ${src} to ${dest}`);
-
-// Create destination directory if it doesn't exist
+// Ensure destination directory exists
 if (!fs.existsSync(dest)) {
   fs.mkdirSync(dest, { recursive: true });
 }
 
-// Copy files
-ncp(src, dest)
-  .then(() => {
+ncp(src, dest, (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  } else {
     console.log('Fonts copied successfully');
-    process.exit(0); // Explicitly exit with success code
-  })
-  .catch((err) => {
-    console.warn('Font copy warning:', err);
-    // Create empty directory if source doesn't exist
-    if (!fs.existsSync(dest)) {
-      fs.mkdirSync(dest, { recursive: true });
-    }
-    process.exit(0); // Still exit with 0 to not fail builds
-  });
+    process.exit(0);
+  }
+});
