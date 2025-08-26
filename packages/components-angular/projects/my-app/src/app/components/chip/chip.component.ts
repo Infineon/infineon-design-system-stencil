4cacd81e57fa2c8443ecc140d8e326631e40d90b
evangeliaPanteliadou <evangelia.panteliadou@infineon.com>
2025-08-26 15:13:30 +0200

    added controls for Chip - Angular example app

--- packages/components-angular/projects/my-app/src/app/components/chip/chip.component.ts
+++ packages/components-angular/projects/my-app/src/app/components/chip/chip.component.ts
@@ -14,5 +14,27 @@ import { ComponentLibraryModule } from '@infineon/infineon-design-system-angular
   styleUrl: './chip.component.scss'
 })
 export class ChipComponent {
+  chipReadOnly = false;
+
+  chipVariant = "single";
+  chipVariants = ["single", "multi"];
+  chipVariantIndex = 0;
+
+  chipSize = "large";
+  chipSizes = ["large", "small"];
+  chipSizeIndex = 0;
+
+  toggleChipVariant() {
+    this.chipVariantIndex = (this.chipVariantIndex + 1) % this.chipVariants.length;
+    this.chipVariant = this.chipVariants[this.chipVariantIndex];
+  }
+
+  toggleChipReadOnly() {
+    this.chipReadOnly = !this.chipReadOnly;
+  }
+  toggleChipSize() {
+    this.chipSizeIndex = (this.chipSizeIndex + 1) % this.chipSizes.length;
+    this.chipSize = this.chipSizes[this.chipSizeIndex];
+  }
 
 }
