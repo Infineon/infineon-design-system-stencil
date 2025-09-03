33d2c157082c6acf1e25d7857e5963a2181540ac
evangeliaPanteliadou <Evangelia.Panteliadou@infineon.com>
2025-09-03 12:11:59 +0200

    Updated checkbox-group and radio-button-gorup on React example app

--- examples/wrapper-components/react-vite-js/src/components/RadioButtonGroup/RadioButtonGroup.jsx
+++ examples/wrapper-components/react-vite-js/src/components/RadioButtonGroup/RadioButtonGroup.jsx
@@ -1,15 +1,36 @@
-import { IfxRadioButtonGroup, IfxRadioButton } from '@infineon/infineon-design-system-react';
+import { useState } from 'react';
+import { IfxRadioButtonGroup, IfxRadioButton, IfxButton } from '@infineon/infineon-design-system-react';
 
 function RadioButtonGroup() {
 
+  const [required, setRequired] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+  const [error, setError] = useState(false);
+
+  const toggleRequired = () => {
+    setRequired(prevRequired => !prevRequired);
+  }
+
+  const toggleDisabled = () => {
+    setDisabled(prevDisabled => !prevDisabled);
+  }
+
+  const toggleError = () => {
+    setError(prevError => !prevError);
+  }
+
   return (
     <div>
-      <IfxRadioButtonGroup alignment="vertical" show-group-label="true" group-label-text="Group Label" show-caption="true" caption-text="Caption text, description, error notification" show-caption-icon="true">
-          <IfxRadioButton value="0" disabled="false" checked="false" size="m" error="false">Option 0</IfxRadioButton>
-          <IfxRadioButton value="1" disabled="false" checked="false" size="m" error="false">Option 1</IfxRadioButton>
-          <IfxRadioButton value="2" disabled="false" checked="false" size="m" error="false">Option 2</IfxRadioButton>
-          <IfxRadioButton value="3" disabled="false" checked="false" size="m" error="false">Option 3</IfxRadioButton>
+      <IfxRadioButtonGroup alignment="vertical" show-group-label={true} group-label-text="Group Label" show-caption={true} caption-text="Caption text, description, error notification" show-caption-icon={true} required={false}>
+          <IfxRadioButton value="0" disabled={false} checked={false} size="m" error={false}>Option 0</IfxRadioButton>
+          <IfxRadioButton value="1" disabled={false} checked={false} size="m" error={false}>Option 1</IfxRadioButton>
+          <IfxRadioButton value="2" disabled={false} checked={false} size="m" error={false}>Option 2</IfxRadioButton>
+          <IfxRadioButton value="3" disabled={false} checked={false} size="m" error={false}>Option 3</IfxRadioButton>
       </IfxRadioButtonGroup>
+
+      <IfxButton variant="primary" onClick={toggleRequired}>Toggle Required</IfxButton>
+      <IfxButton variant="primary" onClick={toggleDisabled}>Toggle Required</IfxButton>
+      <IfxButton variant="primary" onClick={toggleError}>Toggle Required</IfxButton>
     </div>
   );
 }
