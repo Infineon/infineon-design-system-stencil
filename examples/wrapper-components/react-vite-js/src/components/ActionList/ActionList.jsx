155c91e1da3279a6c417725bb5bd2705740530c2
Yanchev Tihomir (IT ERP CDX / EE) <Tihomir.Yanchev-EE@infineon.com>
2025-10-01 15:06:31 +0200

    clean install on react

--- examples/wrapper-components/react-vite-js/src/components/ActionList/ActionList.jsx
+++ examples/wrapper-components/react-vite-js/src/components/ActionList/ActionList.jsx
@@ -1,4 +1,4 @@
-import { IfxActionList, IfxActionListItem } from '@infineon/infineon-design-system-react';
+import { IfxActionList, IfxActionListItem, IfxIcon } from '@infineon/infineon-design-system-react';
 
 function ActionList() {
   
@@ -7,22 +7,22 @@ function ActionList() {
       <IfxActionList list-aria-label="Navigation menu">
           <IfxActionListItem item-title="Dashboard" description="View your main dashboard" value="dashboard" item-aria-label="Navigation item">
             
-            <ifx-icon slot="trailing" icon="chevron-right-16"></ifx-icon>
+            <IfxIcon slot="trailing" icon="chevron-right-16"></IfxIcon>
           </IfxActionListItem>
 
           <IfxActionListItem item-title="Settings" value="settings" item-aria-label="Navigation item">
             
-            <ifx-icon slot="trailing" icon="chevron-right-16"></ifx-icon>
+            <IfxIcon slot="trailing" icon="chevron-right-16"></IfxIcon>
           </IfxActionListItem>
 
           <IfxActionListItem item-title="Profile" description="Manage your profile information" value="profile" disabled="true" item-aria-label="Navigation item">
             
-            <ifx-icon slot="trailing" icon="chevron-right-16"></ifx-icon>
+            <IfxIcon slot="trailing" icon="chevron-right-16"></IfxIcon>
           </IfxActionListItem>
 
           <IfxActionListItem item-title="Advanced Analytics and Reporting Dashboard with Extended Functionality" description="This comprehensive analytics dashboard provides detailed insights into user behavior and system performance metrics." value="analytics" item-aria-label="Navigation item">
             
-            <ifx-icon slot="trailing" icon="chevron-right-16"></ifx-icon>
+            <IfxIcon slot="trailing" icon="chevron-right-16"></IfxIcon>
           </IfxActionListItem>
         </IfxActionList>
     </div>
