beab62c6e2067f0f7e0c097dcf11e56f06682923
Pfaff Fabian <fabian.pfaff@infineon.com>
2025-10-21 14:33:15 +0200

    feat: add action-list component
    This reverts commit 5d3d64965df798ddad79c2347cc7d266bf0a490d.

--- examples/wrapper-components/react-vite-js/src/App.jsx
+++ examples/wrapper-components/react-vite-js/src/App.jsx
@@ -42,6 +42,7 @@ import Textarea from './components/Textarea/Textarea';
 import TreeView from './components/TreeView/TreeView';
 import CheckboxGroup from './components/CheckboxGroup/CheckboxGroup';
 import FileUpload from './components/FileUpload/FileUpload';
+import ActionList from './components/ActionList/ActionList';
 
 function App() {
 
@@ -54,7 +55,11 @@ function App() {
       <h2>Accordion</h2>
       <Accordion />
       <br />
-      
+
+      <h2>Action List</h2>
+      <ActionList />
+      <br />
+
       <h2>Alert</h2>
       <Alert />
       <br />
