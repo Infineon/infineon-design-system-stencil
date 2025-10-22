0d3ebb3f268ef1270c5dfad8ea5de9a442bf3eba
Yanchev Tihomir (IT ERP CDX / EE) <Tihomir.Yanchev-EE@infineon.com>
2025-10-22 09:18:49 +0200

    remove examples folder

--- examples/wrapper-components/vue-javascript/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { ComponentLibrary } from '@infineon/infineon-design-system-vue';
-import './assets/base.scss'
-
-createApp(App).use(ComponentLibrary).mount('#app');
