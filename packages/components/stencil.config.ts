0b2120248851ee51518e2409ed8528eb0d56a995
Pfaff Fabian <fabian.pfaff@infineon.com>
2025-10-23 16:59:05 +0200

    feat(angular): add angular wrapper

--- packages/components/stencil.config.ts
+++ packages/components/stencil.config.ts
@@ -1,4 +1,5 @@
 import { Config } from '@stencil/core';
+import { angularOutputTarget } from '@stencil/angular-output-target';
 import { sass } from '@stencil/sass';
 
 export const config: Config = {
@@ -7,14 +8,32 @@ export const config: Config = {
   globalStyle: 'src/global/styles/index.scss',
   globalScript: 'src/global/setup.ts',
   outputTargets: [
+    /**
+     * This output target creates a small entry point that registers all components and lazy loads them on demand.
+     * It enables users to use the components from CDN or importing them in a bootstrap script.
+     * Usage:
+     * ```javascript
+     * import { defineCustomElements } from '@infineon/infineon-design-system-stencil/loader';
+      * defineCustomElements(window);
+     * ```
+     * or
+     * ```html
+     * <script type="module" src="https://cdn.jsdelivr.net/npm/@infineon/infineon-design-system-stencil"></script>
+     * ```
+     * See https://stenciljs.com/docs/distribution and https://stenciljs.com/docs/publishing#lazy-loading
+     */
     {
       type: 'dist',
-      esmLoaderPath: '../loader',
-      // TODO: Copy fonts?
     },
+    /**
+     * This output target creates standalone custom elements that directly extend HTMLElement without any lazy loading.
+     * This may be prefered for projects that already handle bundling, lazy-loading and defining the custom elements themselves.
+     * However, usage requires further configuration in the consuming project.
+     * See https://stenciljs.com/docs/custom-elements and https://stenciljs.com/docs/publishing#standalone
+     */
     {
       type: 'dist-custom-elements',
-      customElementsExportBehavior: 'auto-define-custom-elements',
+      customElementsExportBehavior: 'single-export-module',
       externalRuntime: false,
     },
     /**
@@ -27,11 +46,18 @@ export const config: Config = {
     {
       type: 'docs-readme',
     },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
-      // TODO: copy fonts?
-    },
+    angularOutputTarget({
+      componentCorePackage: '@infineon/infineon-design-system-stencil',
+      outputType: 'component',
+      directivesProxyFile: '../wrapper-angular/src/lib/stencil-generated/components.ts',
+      directivesArrayFile: '../wrapper-angular/src/lib/stencil-generated/index.ts',
+    }),
+    angularOutputTarget({
+      componentCorePackage: '@infineon/infineon-design-system-stencil',
+      outputType: 'standalone',
+      directivesProxyFile: '../wrapper-angular/standalone/src/lib/stencil-generated/components.ts',
+      directivesArrayFile: '../wrapper-angular/standalone/src/lib/stencil-generated/index.ts',
+    }),
   ],
   testing: {
     browserHeadless: "shell",
