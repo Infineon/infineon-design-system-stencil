27891b7d278dad9f47ef28a1c88981c0592d3a0c
Pfaff Fabian <fabian.pfaff@infineon.com>
2025-10-23 16:59:05 +0200

    feat(angular): add angular wrapper

---
+++ packages/wrapper-angular/src/lib/wrapper-angular.module.ts
@@ -0,0 +1,25 @@
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { APP_INITIALIZER, type ModuleWithProviders, NgModule, NgZone } from '@angular/core';
+import { DIRECTIVES } from './stencil-generated';
+import { defineCustomElements } from '@infineon/infineon-design-system-stencil/loader';
+
+@NgModule({
+  declarations: [...DIRECTIVES],
+  exports: [...DIRECTIVES],
+  imports: [ CommonModule ]
+})
+export class InfineonDesignSystemModule {
+  static forRoot(): ModuleWithProviders<InfineonDesignSystemModule> {
+    return {
+      ngModule: InfineonDesignSystemModule,
+      providers: [
+        {
+          provide: APP_INITIALIZER,
+          useFactory: () => defineCustomElements,
+          multi: true,
+          deps: [DOCUMENT, NgZone],
+        },
+      ],
+    };
+  }
+}
