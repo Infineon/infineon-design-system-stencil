c7a53fe2a4905f859c748b693fdffb0bda35fdf4
Pfaff Fabian <fabian.pfaff@infineon.com>
2025-10-29 14:31:10 +0100

    feat(examples): add angular standalone example

---
+++ apps/angular-standalone-example-app/README.md
@@ -0,0 +1,1 @@
+# Angular Standalone Example App
---
+++ apps/angular-standalone-example-app/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+import { IfxAlert } from '@infineon/infineon-design-system-angular/standalone';
+
+@Component({
+  selector: 'app-root',
+  imports: [ IfxAlert ],
+  templateUrl: './app.html',
+})
+export class App {
+  protected readonly title = signal('angular-standalone-example-app');
+}
