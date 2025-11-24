ef6abbd178761faa58b6fb86540ce2fafe48dae0
evangeliaPanteliadou <Evangelia.Panteliadou@infineon.com>
2025-11-24 19:49:23 +0100

    added controls for Stepper - Angular example app

--- packages/components-angular/projects/my-app/src/app/components/stepper/stepper.component.ts
+++ packages/components-angular/projects/my-app/src/app/components/stepper/stepper.component.ts
@@ -14,5 +14,37 @@ import { ComponentLibraryModule } from '@infineon/infineon-design-system-angular
   styleUrl: './stepper.component.scss'
 })
 export class StepperComponent {
+  stepNumber = false;
+  stepError = false;
+  stepDisable = false;
+  stepComplete = false;
 
+  sliderType = "single";
+  sliderTypes = ["single", "double"];
+  sliderTypeIndex = 0;
+
+  stepperVariant = "default";
+  stepperVariants = ["default", "compact", "vertical"];
+  stepperVariantIndex = 0;
+
+  toggleStepNumber() {
+    this.stepNumber = !this.stepNumber;
+  }
+
+  toggleStepperVariant() {
+    this.stepperVariantIndex = (this.stepperVariantIndex + 1) % this.stepperVariants.length;
+    this.stepperVariant = this.stepperVariants[this.stepperVariantIndex];
+  }
+
+  toggleStepError() {
+    this.stepError = !this.stepError;
+  }
+
+  toggleStepDisable() {
+    this.stepDisable = !this.stepDisable;
+  }
+
+  toggleStepComplete() {
+    this.stepComplete = !this.stepComplete;
+  }
 }
