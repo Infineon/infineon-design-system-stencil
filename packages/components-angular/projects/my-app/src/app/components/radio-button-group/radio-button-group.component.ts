ffcda1f49d5495fbb97300c36f4fe1b6108c98cf
evangeliaPanteliadou <Evangelia.Panteliadou@infineon.com>
2025-11-24 17:11:58 +0100

    added controls for Radio Button Group - Angular example app

--- packages/components-angular/projects/my-app/src/app/components/radio-button-group/radio-button-group.component.ts
+++ packages/components-angular/projects/my-app/src/app/components/radio-button-group/radio-button-group.component.ts
@@ -14,5 +14,31 @@ import { ComponentLibraryModule } from '@infineon/infineon-design-system-angular
   styleUrl: './radio-button-group.component.scss'
 })
 export class RadioButtonGroupComponent {
+  radioBtnGroupLabel = false;
+  radioBtnGroupCaption = false;
+  radioBtnGroupIcon = false;
+  radioBtnGroupRequired = false;
 
+  radioBtnGroupAlignment = "vertical";
+  radioBtnGroupAlignments = ["vertical", "horizontal"];
+  radioBtnGroupAlignmentIndex = 0;
+
+  toggleRadioBtnGroupLabel() {
+    this.radioBtnGroupLabel = !this.radioBtnGroupLabel;
+  }
+  toggleRadioBtnGroupCaption() {
+    this.radioBtnGroupCaption = !this.radioBtnGroupCaption;
+  }
+  toggleRadioBtnGroupIcon() {
+    this.radioBtnGroupIcon = !this.radioBtnGroupIcon;
+  }
+
+  toggleRadioBtnGroupRequired() {
+    this.radioBtnGroupRequired = !this.radioBtnGroupRequired;
+  }
+
+  toggleRadioBtnGroupAlignment() {
+    this.radioBtnGroupAlignmentIndex = (this.radioBtnGroupAlignmentIndex + 1) % this.radioBtnGroupAlignments.length;
+    this.radioBtnGroupAlignment = this.radioBtnGroupAlignments[this.radioBtnGroupAlignmentIndex];
+  }
 }
