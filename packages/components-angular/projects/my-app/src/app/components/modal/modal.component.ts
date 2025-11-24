4db7040f32b60664ef8d5394fb3d66a568d9a500
evangeliaPanteliadou <Evangelia.Panteliadou@infineon.com>
2025-11-24 14:59:54 +0100

    added controls for Modal - Angular example app

--- packages/components-angular/projects/my-app/src/app/components/modal/modal.component.ts
+++ packages/components-angular/projects/my-app/src/app/components/modal/modal.component.ts
@@ -15,18 +15,40 @@ import { ComponentLibraryModule } from '@infineon/infineon-design-system-angular
 })
 export class ModalComponent {
   handleModalButton1Click() {
-    // const modal = document.querySelector('ifx-modal') as HTMLIfxModalElement;
     console.log("modal button 1 click ")
   }
 
   handleModalButton2Click() {
-    // const modal = document.querySelector('ifx-modal') as HTMLIfxModalElement;
-    // modal.close();
+    console.log("modal button 2 click ")
   }
 
   handleModalOpen() {
     const modal = document.querySelector('ifx-modal') as HTMLIfxModalElement;
     console.log("modal open btn click ")
     modal.opened = true;
+  }
+
+  modalVariant = "default";
+  modalVariants = ["default", "alert-brand", "alert-danger"];
+  modalVariantIndex = 0;
+
+  modalSize = "s";
+  modalSizes = ["s", "m", "l"];
+  modalSizeIndex = 0;
+
+  modalCloseButton = true;
+
+  toggleModalVariant() {
+    this.modalVariantIndex = (this.modalVariantIndex + 1) % this.modalVariants.length;
+    this.modalVariant = this.modalVariants[this.modalVariantIndex];
+  }
+
+  toggleModalSize() {
+    this.modalSizeIndex = (this.modalSizeIndex + 1) % this.modalSizes.length;
+    this.modalSize = this.modalSizes[this.modalSizeIndex];
+  }
+
+  toggleModalCloseButton() {
+    this.modalCloseButton = !this.modalCloseButton;
   }
 }
