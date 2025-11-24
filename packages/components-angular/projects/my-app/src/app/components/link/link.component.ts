bb35fd4e2dde9138d79c5673f5724e13e48f4541
evangeliaPanteliadou <Evangelia.Panteliadou@infineon.com>
2025-11-24 14:13:05 +0100

    added controls for Link - Angular example app

--- packages/components-angular/projects/my-app/src/app/components/link/link.component.ts
+++ packages/components-angular/projects/my-app/src/app/components/link/link.component.ts
@@ -14,5 +14,27 @@ import { ComponentLibraryModule } from '@infineon/infineon-design-system-angular
   styleUrl: './link.component.scss'
 })
 export class LinkComponent {
+  linkVariant = "bold";
+  linkVariants = ["bold", "underlined", "title", "menu"];
+  linkVariantIndex = 0;
 
+  linkSize = "m";
+  linkSizes = ["s", "m", "l", "xl"];
+  linkSizeIndex = 1;
+
+  linkDisabled = false;
+
+  toggleLinkVariant() {
+    this.linkVariantIndex = (this.linkVariantIndex + 1) % this.linkVariants.length;
+    this.linkVariant = this.linkVariants[this.linkVariantIndex];
+  }
+
+  toggleLinkSize() {
+    this.linkSizeIndex = (this.linkSizeIndex + 1) % this.linkSizes.length;
+    this.linkSize = this.linkSizes[this.linkSizeIndex];
+  }
+
+  toggleLinkDisabled() {
+    this.linkDisabled = !this.linkDisabled;
+  }
 }
