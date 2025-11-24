ce6bfd885a862386e8dd99bb966b13eaef0dd35f
evangeliaPanteliadou <Evangelia.Panteliadou@infineon.com>
2025-11-24 17:32:44 +0100

    added controls for Search Field - Angular example app

--- packages/components-angular/projects/my-app/src/app/components/search-field/search-field.component.ts
+++ packages/components-angular/projects/my-app/src/app/components/search-field/search-field.component.ts
@@ -14,10 +14,23 @@ import { ComponentLibraryModule } from '@infineon/infineon-design-system-angular
   styleUrl: './search-field.component.scss'
 })
 export class SearchFieldComponent {
-  searchFieldValue = '';
+  searchFieldDisabled = false;
+  searchFieldDeleteIcon = true;
 
-  handleSearchField(event: any) {
-    console.log("searchField value change: ", event.detail)
-    this.searchFieldValue = event.detail;
+  searchFieldSize = "m";
+  searchFieldSizes = ["s", "m"];
+  searchFieldSizeIndex = 1;
+
+  toggleSerchFieldDisabled() {
+    this.searchFieldDisabled = !this.searchFieldDisabled;
+  }
+
+  toggleSearchFieldDeleteIcon() {
+    this.searchFieldDeleteIcon = !this.searchFieldDeleteIcon;
+  }
+
+  toggleSearchFieldSize() {
+    this.searchFieldSizeIndex = (this.searchFieldSizeIndex + 1) % this.searchFieldSizes.length;
+    this.searchFieldSize = this.searchFieldSizes[this.searchFieldSizeIndex];
   }
 }
