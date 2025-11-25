4a31b09f3889d3e0322ae225ea4279d72f3fcd53
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2025-11-25 10:10:55 +0000

    Deploy preview for PR 2100 🛫

--- pr-preview-angular-module-example/pr-2100/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModule } from './app/app-module';
-
-platformBrowser().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
-})
-  .catch(err => console.error(err));
