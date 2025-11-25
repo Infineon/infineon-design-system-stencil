d7c906840abb6eb89e4a658e26619d968d35e963
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2025-11-25 10:02:01 +0000

    Deploy preview for PR 2100 🛫

---
+++ pr-preview-vue-example/pr-2100/src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { ComponentLibrary } from '@infineon/infineon-design-system-vue';
+
+createApp(App).use(ComponentLibrary).mount('#app')
