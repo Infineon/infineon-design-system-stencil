0bacd3c10e7d948783cb9979f351079c7e8976f1
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2025-11-25 10:10:44 +0000

    Deploy preview for PR 2100 🛫

---
+++ pr-preview-vue-example/pr-2100/assets/ifx-dropdown-trigger.entry-B6IoZjbC.js
@@ -0,0 +1,1 @@
+import{r as s,h as e}from"./index-D15ceK5A.js";const o=":host{display:inline-block;cursor:pointer}",t=class{constructor(r){s(this,r)}isOpen=!1;render(){return e("slot",{key:"891e6f518150a88c2f51a7ce724c2343dc8af2bc"})}};t.style=o;export{t as ifx_dropdown_trigger};
--- pr-preview-vue-example/pr-2100/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { ComponentLibrary } from '@infineon/infineon-design-system-vue';
-
-createApp(App).use(ComponentLibrary).mount('#app')
