589de83d39aa62f710765ce4352f14a8ac71d174
evangeliaPanteliadou <Evangelia.Panteliadou@infineon.com>
2025-11-26 11:43:48 +0100

    Updated controls for Dropdown - Angular App

--- packages/components-angular/projects/my-app/src/app/components/dropdown/dropdown.component.ts
+++ packages/components-angular/projects/my-app/src/app/components/dropdown/dropdown.component.ts
@@ -15,6 +15,8 @@ import { ComponentLibraryModule } from '@infineon/infineon-design-system-angular
 })
 export class DropdownComponent {
   dropdownDisabled = false;
+  dropdownNoCloseOnOutsideClick = false;
+  dropdownNoCloseOnMenuClick = false;
 
   dropdownSize = "m";
   dropdownSizes = ["m", "s"];
@@ -23,6 +25,10 @@ export class DropdownComponent {
   dropdownVariant= "primary";
   dropdownVariants = ["primary", "secondary", "tertiary"];
   dropdownVariantIndex = 0;
+
+  dropdownPlacement= "bottom-start";
+  dropdownPlacements = ["bottom-start", "auto", "auto-start", "auto-end", "top", "top-start", "top-end", "bottom", "bottom-end", "right", "right-start", "right-end", "left", "left-start", "left-end"];
+  dropdownPlacementIndex = 0;
 
   toggleDropdownSize() {
     this.dropdownSizeIndex = (this.dropdownSizeIndex + 1) % this.dropdownSizes.length;
@@ -34,8 +40,21 @@ export class DropdownComponent {
     this.dropdownVariant = this.dropdownVariants[this.dropdownVariantIndex];
   }
 
+  toggleDropdownPlacement() {
+    this.dropdownPlacementIndex = (this.dropdownPlacementIndex + 1) % this.dropdownPlacements.length;
+    this.dropdownPlacement = this.dropdownPlacements[this.dropdownPlacementIndex];
+  }
+
   toggleDropdownDisabled() {
     this.dropdownDisabled = !this.dropdownDisabled;
+  }
+
+  toggleDropdownNoCloseOnOutsideClick() {
+    this.dropdownNoCloseOnOutsideClick = !this.dropdownNoCloseOnOutsideClick;
+  }
+
+  toggleDropdownNoCloseOnMenuClick() {
+    this.dropdownNoCloseOnMenuClick = !this.dropdownNoCloseOnMenuClick;
   }
 
 }
