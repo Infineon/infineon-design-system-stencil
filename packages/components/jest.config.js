c1426ceceb370e25c2186b0a3dfce04396e6c4f5
Pfaff Fabian <fabian.pfaff@infineon.com>
2025-11-28 14:41:02 +0100

    test: setup jest

---
+++ packages/components/jest.config.js
@@ -0,0 +1,11 @@
+const preset = require('jest-stencil-runner/preset');
+
+module.exports = {
+    ...preset,
+    rootDir: __dirname,
+    collectCoverageFrom: [
+        'src/**/*.{ts,tsx}',
+        '!src/**/*.d.ts',
+    ],
+    coverageDirectory: 'coverage',
+};
