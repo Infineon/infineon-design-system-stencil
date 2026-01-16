157ddb9c41614fbc7d16394c7617a3b8f9a3f29d
evangeliaPanteliadou <Evangelia.Panteliadou@infineon.com>
2026-01-16 13:49:35 +0100

    updated Textarea component - Angular

--- packages/components-angular/projects/my-app/src/app/components/textarea/textarea.component.ts
+++ packages/components-angular/projects/my-app/src/app/components/textarea/textarea.component.ts
@@ -13,18 +13,41 @@ import { ComponentLibraryModule } from '@infineon/infineon-design-system-angular
   templateUrl: './textarea.component.html',
   styleUrl: './textarea.component.scss'
 })
-export class TextareaComponent {
-  textareaValue = '';
-  textFieldValue = '';
-  checkboxChecked = false;
 
-  handleTextInput(event: any) {
-    console.log("textInput value change")
-    this.textFieldValue = event.detail;
-    this.textareaValue = event.detail
+export class TextareaComponent {
+  textareaDisabled = false;
+  textareaError = false;
+  textareaReadOnly = false;
+  textareaFullWidth = false;
+  textareaRequired = true;
+
+  textareaResizeIndex = 0;
+  textareaResizes = ['both', 'vertical', 'horizontal', 'none'];
+  textareaResize = this.textareaResizes[this.textareaResizeIndex];
+
+  toggleTextareaDisabled() {
+    this.textareaDisabled = !this.textareaDisabled;
   }
 
-  handleSubmit() {
-    console.log('Form submitted. Checkbox value:', this.checkboxChecked);
+  toggleTextareaError() {
+    this.textareaError = !this.textareaError;
+  }
+
+  toggleTextareaReadOnly() {
+    this.textareaReadOnly = !this.textareaReadOnly;
+  }
+
+  toggleTextareaFullWidth() {
+    this.textareaFullWidth = !this.textareaFullWidth;
+  }
+
+  toggleTextareaRequired() {
+    this.textareaRequired = !this.textareaRequired;
+  }
+
+  toggleTextareaResize() {
+    this.textareaResizeIndex =
+      (this.textareaResizeIndex + 1) % this.textareaResizes.length;
+    this.textareaResize = this.textareaResizes[this.textareaResizeIndex];
   }
 }
