f164d69dcb50a67d553213ee313cfb642e949441
evangeliaPanteliadou <Evangelia.Panteliadou@infineon.com>
2026-01-16 08:42:32 +0100

    updated Spiner component - React Vite

--- examples/wrapper-components/react-vite-js/src/components/Spinner/Spinner.jsx
+++ examples/wrapper-components/react-vite-js/src/components/Spinner/Spinner.jsx
@@ -1,14 +1,50 @@
-import { IfxSpinner } from '@infineon/infineon-design-system-react';
+import { useState } from 'react';
+import { IfxSpinner, IfxButton } from '@infineon/infineon-design-system-react';
 
 function Spinner() {
+  const [variantIndex, setVariantIndex] = useState(0);
+  const variants = ['default', 'brand'];
+
+  const [sizeIndex, setSizeIndex] = useState(1);
+  const sizes = ['s', 'm'];
+
+  const [inverted, setInverted] = useState(false);
+
+  const toggleSpinnerVariant = () => {
+    setVariantIndex((index) => (index + 1) % variants.length);
+  };
+
+  const toggleSpinnerSize = () => {
+    setSizeIndex((index) => (index + 1) % sizes.length);
+  };
+
+  const toggleSpinnerInverted = () => {
+    setInverted((prev) => !prev);
+  };
+
   return (
     <div>
       <h2>Spinner</h2>
-      <IfxSpinner></IfxSpinner>
+
+      <IfxSpinner
+        variant={variants[variantIndex]}
+        size={sizes[sizeIndex]}
+        inverted={inverted}
+      ></IfxSpinner>
+
+      <br />
+      <br />
+      <h3 style={{ textDecoration: 'underline' }}>Controls</h3>
+      <IfxButton variant="secondary" onClick={toggleSpinnerVariant}>Toggle Variant</IfxButton>
+      <IfxButton variant="secondary" onClick={toggleSpinnerInverted}>Toggle Inverted</IfxButton>
+      <IfxButton variant="secondary" onClick={toggleSpinnerSize}>Toggle Size</IfxButton>
+      <br />
+      <br />
+      <span><b>Variant:</b> {variants[variantIndex]}</span><br />
+      <span><b>Inverted:</b> {String(inverted)}</span><br />
+      <span><b>Size:</b> {sizes[sizeIndex]}</span><br />
     </div>
-  )
+  );
 }
 
 export default Spinner;
-
-
