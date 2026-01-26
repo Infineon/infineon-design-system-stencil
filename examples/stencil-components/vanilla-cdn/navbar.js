ea443455e7170c747340e2ae015749e2d5146847
evangeliaPanteliadou <Evangelia.Panteliadou@infineon.com>
2026-01-26 13:19:50 +0100

    added Navbar component - Vanilla

---
+++ examples/stencil-components/vanilla-cdn/navbar.js
@@ -0,0 +1,115 @@
+import { navigate } from './router.js';
+
+const navbar = document.getElementById('navbar');
+
+navbar.innerHTML = `
+  <ifx-navbar show-logo-and-appname="true" application-name="Stencil Components - Vanilla App" fixed="false" logo-href="http://google.com"
+    logo-href-target="_self">
+
+    <ifx-navbar-item icon="calendar16" slot="left-item" target="" href="">
+        Components A-E
+        <ifx-navbar-item onclick="window.__nav('accordion')">Accordion</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('action-list')">Action List</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('alert')">Alert</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('breadcrumb')">Breadcrumb</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('button')">Button</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('card')">Card</ifx-navbar-item>
+        <ifx-navbar-item>
+            Checkbox
+            <ifx-navbar-item onclick="window.__nav('checkbox')">Checkbox</ifx-navbar-item>
+            <ifx-navbar-item onclick="window.__nav('checkboxgroup')">Checkbox Group</ifx-navbar-item>
+        </ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('chip')">Chip</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('contentswitcher')">Content-Switcher</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('datepicker')">Date-Picker</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('dropdown')">Dropdown</ifx-navbar-item>
+    </ifx-navbar-item>
+
+    <ifx-navbar-item icon="calendar16" slot="left-item" target="" href="">
+        Components F-R
+        <ifx-navbar-item onclick="window.__nav('fileupload')">File Upload</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('footer')">Footer</ifx-navbar-item>
+        <ifx-navbar-item>
+            Icon
+            <ifx-navbar-item onclick="window.__nav('icon')">Icon</ifx-navbar-item>
+            <ifx-navbar-item onclick="window.__nav('iconbutton')">Icon Button</ifx-navbar-item>
+        </ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('indicator')">Indicator</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('link')">Link</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('modal')">Modal</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('notification')">Notification</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('pagination')">Pagination</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('progressbar')">Progress Bar</ifx-navbar-item>
+        <ifx-navbar-item>
+            Radio Button
+            <ifx-navbar-item onclick="window.__nav('radiobutton')">Radio Button</ifx-navbar-item>
+            <ifx-navbar-item onclick="window.__nav('radiobuttongroup')">Radio Button Group</ifx-navbar-item>
+        </ifx-navbar-item>
+    </ifx-navbar-item>
+
+    <ifx-navbar-item icon="calendar16" slot="left-item" target="" href="">
+        Components S-Z
+        <ifx-navbar-item>
+            Search
+            <ifx-navbar-item onclick="window.__nav('searchbar')">Search Bar</ifx-navbar-item>
+            <ifx-navbar-item onclick="window.__nav('searchfield')">Search Field</ifx-navbar-item>
+        </ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('segmentedcontrol')">Segmented Control</ifx-navbar-item>
+        <ifx-navbar-item>
+            Select
+            <ifx-navbar-item onclick="window.__nav('singleselect')">Single Select</ifx-navbar-item>
+            <ifx-navbar-item onclick="window.__nav('multiselect')">Multi Select</ifx-navbar-item>
+        </ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('sidebar')">Sidebar</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('slider')">Slider</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('spinner')">Spinner</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('status')">Status</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('stepper')">Stepper</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('switch')">Switch</ifx-navbar-item>
+        <ifx-navbar-item>
+            Table
+            <ifx-navbar-item onclick="window.__nav('basictable')">Basic Table</ifx-navbar-item>
+            <ifx-navbar-item onclick="window.__nav('advancedtable')">Advanced Table</ifx-navbar-item>
+        </ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('tabs')">Tabs</ifx-navbar-item>
+        <ifx-navbar-item>
+            Text
+            <ifx-navbar-item onclick="window.__nav('textfield')">Text Field</ifx-navbar-item>
+            <ifx-navbar-item onclick="window.__nav('textarea')">Textarea</ifx-navbar-item>
+        </ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('tooltip')">Tooltip</ifx-navbar-item>
+        <ifx-navbar-item onclick="window.__nav('treeview')">Tree View</ifx-navbar-item>
+    </ifx-navbar-item>
+
+    <ifx-search-bar id="myBar" slot="search-bar-left" is-open="false"></ifx-search-bar>
+
+    <ifx-navbar-item slot="right-item" target="_blank" href="http://google.com" hide-on-mobile="false"
+        show-label="false" icon="cartf16">Right Item</ifx-navbar-item>
+    <ifx-navbar-item slot="right-item" hide-on-mobile="true" show-label='true' icon="airplane16">
+        Right Item
+        <ifx-navbar-item>
+            Nested one
+            <ifx-navbar-item>
+                Nested one
+                <ifx-navbar-item>
+                    Nested one
+                    <ifx-navbar-item>Final one</ifx-navbar-item>
+                </ifx-navbar-item>
+            </ifx-navbar-item>
+        </ifx-navbar-item>
+    </ifx-navbar-item>
+
+    <ifx-navbar-profile slot="right-item"
+        image-url="https://i.pinimg.com/originals/82/d4/92/82d4926dcf09dd4c73eb1a6c0300c135.jpg" show-label="true"
+        href="" target="_blank">
+        User
+        <ifx-navbar-item>Item</ifx-navbar-item>
+        <ifx-navbar-item>Item</ifx-navbar-item>
+        <ifx-navbar-item>Item</ifx-navbar-item>
+        <ifx-navbar-item>Item</ifx-navbar-item>
+    </ifx-navbar-profile>
+</ifx-navbar>
+
+`;
+
+window.__nav = navigate;
