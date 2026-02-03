e9d010b4735167d75d9766e0c87635b99106f718
evangeliaPanteliadou <Evangelia.Panteliadou@infineon.com>
2026-02-03 11:51:46 +0100

    update Chip Component - Vanilla

--- examples/stencil-components/vanilla-cdn/pages/chip.js
+++ examples/stencil-components/vanilla-cdn/pages/chip.js
@@ -1,6 +1,6 @@
 export function render(container) {
 
-    container.innerHTML = `
+  container.innerHTML = `
     <h2>Chip</h2>
 
     <ifx-chip placeholder="Label" size="medium" variant="single" theme="outlined" icon="" read-only="false" aria-label="Chip" disabled="false">
@@ -18,6 +18,7 @@ export function render(container) {
     <ifx-button variant="secondary" id="toggle-disabled">Toggle Disabled</ifx-button>
     <ifx-button variant="secondary" id="toggle-readOnly">Toggle Read Only</ifx-button>
     <ifx-button variant="secondary" id="toggle-theme">Toggle Theme</ifx-button>
+    <ifx-button variant="secondary" id="toggle-icon">Toggle Icon</ifx-button>
 
     <br><br>
 
@@ -26,43 +27,68 @@ export function render(container) {
     <span><b>Disabled:</b> <span id="state-disabled"></span></span><br>
     <span><b>Read Only:</b> <span id="state-readOnly"></span></span><br>
     <span><b>Theme:</b> <span id="state-theme"></span></span><br>
+    <span><b>Icon:</b> <span id="state-icon"></span></span>
   `;
 
-    const chip = container.querySelector('ifx-chip');
-    const sizeState = container.querySelector('#state-size');
-    const variantState = container.querySelector('#state-variant');
-    const disabledState = container.querySelector('#state-disabled');
-    const readOnlyState = container.querySelector('#state-readOnly');
-    const themeState = container.querySelector('#state-theme');
+  const chip = container.querySelector('ifx-chip');;
+  const sizeState = container.querySelector('#state-size');
+  const variantState = container.querySelector('#state-variant');
+  const disabledState = container.querySelector('#state-disabled');
+  const readOnlyState = container.querySelector('#state-readOnly');
+  const themeState = container.querySelector('#state-theme');
+  const iconState = container.querySelector('#state-icon');
 
+  const sizes = ["small", "medium", "large"];
+  const variants = ["single", "multi"];
+  const themes = ["outlined", "filled-light", "filled-dark"];
+  const icons = ["", "windows16"];
+
+  sizeState.textContent = chip.size;
+  variantState.textContent = chip.variant;
+  disabledState.textContent = String(chip.disabled);
+  readOnlyState.textContent = String(chip.readOnly);
+  themeState.textContent = chip.theme;
+  iconState.textContent = chip.icon;
+
+  container.querySelector('#toggle-size').onclick = () => {
+    const currentIndex = sizes.indexOf(chip.size);
+    const nextIndex = (currentIndex + 1) % sizes.length;
+
+    chip.size = sizes[nextIndex];
     sizeState.textContent = chip.size;
+  }
+
+  container.querySelector('#toggle-variant').onclick = () => {
+    const currentIndex = variants.indexOf(chip.variant);
+    const nextIndex = (currentIndex + 1) % variants.length;
+
+    chip.variant = variants[nextIndex];
     variantState.textContent = chip.variant;
+  }
+
+  container.querySelector('#toggle-disabled').onclick = () => {
+    chip.disabled = !chip.disabled;
     disabledState.textContent = String(chip.disabled);
+  }
+
+  container.querySelector('#toggle-readOnly').onclick = () => {
+    chip.readOnly = !chip.readOnly;
     readOnlyState.textContent = String(chip.readOnly);
+  }
+
+  container.querySelector('#toggle-theme').onclick = () => {
+    const currentIndex = themes.indexOf(chip.theme);
+    const nextIndex = (currentIndex + 1) % themes.length;
+
+    chip.theme = themes[nextIndex];
     themeState.textContent = chip.theme;
+  }
 
-    container.querySelector('#toggle-size').onclick = () => {
-        chip.size = chip.size === 'medium' ? 'small' : 'medium';
-        sizeState.textContent = chip.size;
-    }
+  container.querySelector('#toggle-icon').onclick = () => {
+    const currentIndex = icons.indexOf(chip.icon);
+    const nextIndex = (currentIndex + 1) % icons.length;
 
-    container.querySelector('#toggle-variant').onclick = () => {
-        chip.variant = chip.variant === 'single' ? 'multiple' : 'single';
-        variantState.textContent = chip.variant;
-    }
-
-    container.querySelector('#toggle-disabled').onclick = () => {
-        chip.disabled = !chip.disabled;
-        disabledState.textContent = String(chip.disabled);
-    }
-
-    container.querySelector('#toggle-readOnly').onclick = () => {
-        chip.readOnly = !chip.readOnly;
-        readOnlyState.textContent = String(chip.readOnly);
-    }
-
-    container.querySelector('#toggle-theme').onclick = () => {
-        chip.theme = chip.theme === 'outlined' ? 'filled' : 'outlined';
-        themeState.textContent = chip.theme;
-    }
+    chip.icon = icons[nextIndex];
+    iconState.textContent = chip.icon;
+  }
 }
