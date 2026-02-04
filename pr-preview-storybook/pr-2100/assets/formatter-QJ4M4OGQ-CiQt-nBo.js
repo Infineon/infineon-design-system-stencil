a73a8ab41e528c56a7707b03ad4441e416dc9a83
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-02-04 16:24:17 +0000

    Deploy preview for PR 2100 🛫

---
+++ pr-preview-storybook/pr-2100/assets/formatter-QJ4M4OGQ-CiQt-nBo.js
@@ -0,0 +1,1 @@
+import{e as i,f as m}from"./blocks-BDXmLa5N.js";import{b as o}from"./iframe-CrOB37jY.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(m(),1),_=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{_ as formatter};
