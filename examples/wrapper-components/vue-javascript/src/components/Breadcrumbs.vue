25ac98df37516c045e113c528e00ca3db0725c55
evangeliaPanteliadou <Evangelia.Panteliadou@infineon.com>
2026-02-11 10:44:04 +0100

    updated Breadcrumb component - Vue app

--- examples/wrapper-components/vue-javascript/src/components/Breadcrumbs.vue
+++ examples/wrapper-components/vue-javascript/src/components/Breadcrumbs.vue
@@ -1,34 +1,31 @@
 <template>
   <div class="component">
-    <h2>Breadcrumbs</h2>
-    <div>
-      <ifx-breadcrumb>
-        <ifx-breadcrumb-item>
-          <ifx-breadcrumb-item-label slot="label">Breadcrumb 1</ifx-breadcrumb-item-label>
-          <ifx-dropdown-menu>
-            <ifx-dropdown-item href="http://google.com">Google</ifx-dropdown-item>
-            <ifx-dropdown-item href="http://bing.com">Bing</ifx-dropdown-item>
-            <ifx-dropdown-item href="http://yahoo.com">Yahoo</ifx-dropdown-item>
-            <ifx-dropdown-item href="http://naver.com">Naver</ifx-dropdown-item>
-          </ifx-dropdown-menu>
-        </ifx-breadcrumb-item>
+    <h2>Breadcrumb</h2>
+    <ifx-breadcrumb>
+      <ifx-breadcrumb-item>
+        <ifx-breadcrumb-item-label slot="label">Breadcrumb 1</ifx-breadcrumb-item-label>
+        <ifx-dropdown-menu>
+          <ifx-dropdown-item href="http://google.com">Google</ifx-dropdown-item>
+          <ifx-dropdown-item href="http://bing.com">Bing</ifx-dropdown-item>
+          <ifx-dropdown-item href="http://yahoo.com">Yahoo</ifx-dropdown-item>
+          <ifx-dropdown-item href="http://naver.com">Naver</ifx-dropdown-item>
+        </ifx-dropdown-menu>
+      </ifx-breadcrumb-item>
 
-        <ifx-breadcrumb-item>
-          <ifx-breadcrumb-item-label slot="label">Breadcrumb 2</ifx-breadcrumb-item-label>
-          <ifx-dropdown-menu>
-            <ifx-dropdown-item href="http://google.com">Google</ifx-dropdown-item>
-            <ifx-dropdown-item href="http://bing.com">Bing</ifx-dropdown-item>
-            <ifx-dropdown-item href="http://yahoo.com">Yahoo</ifx-dropdown-item>
-            <ifx-dropdown-item href="http://naver.com">Naver</ifx-dropdown-item>
-          </ifx-dropdown-menu>
-        </ifx-breadcrumb-item>
+      <ifx-breadcrumb-item>
+        <ifx-breadcrumb-item-label slot="label">Breadcrumb 2</ifx-breadcrumb-item-label>
+        <ifx-dropdown-menu>
+          <ifx-dropdown-item href="http://google.com">Google</ifx-dropdown-item>
+          <ifx-dropdown-item href="http://bing.com">Bing</ifx-dropdown-item>
+          <ifx-dropdown-item href="http://yahoo.com">Yahoo</ifx-dropdown-item>
+          <ifx-dropdown-item href="http://naver.com">Naver</ifx-dropdown-item>
+        </ifx-dropdown-menu>
+      </ifx-breadcrumb-item>
 
-        <ifx-breadcrumb-item>
-          <ifx-breadcrumb-item-label slot="label" href="http://google.com">Breadcrumb 3</ifx-breadcrumb-item-label>
-        </ifx-breadcrumb-item>
-      </ifx-breadcrumb>
-    </div>
-    <br />
-    <br />
+      <ifx-breadcrumb-item>
+        <ifx-breadcrumb-item-label slot="label" icon="chipAi16" href="http://google.com">Breadcrumb
+          3</ifx-breadcrumb-item-label>
+      </ifx-breadcrumb-item>
+    </ifx-breadcrumb>
   </div>
 </template>
