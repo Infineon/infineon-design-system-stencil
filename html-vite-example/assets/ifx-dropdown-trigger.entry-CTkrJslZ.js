7cd9cd0a0a48b9f4a406bcbb87bc6cf78945cddb
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-02-13 13:54:41 +0000

    deploy: 41e192e564376fb710009da9d2b295153f37faaf

---
+++ html-vite-example/assets/ifx-dropdown-trigger.entry-CTkrJslZ.js
@@ -0,0 +1,2 @@
+import{r as s,h as e}from"./index-BfJORvcO.js";const o=":host{display:inline-block;cursor:pointer}",t=class{constructor(r){s(this,r)}isOpen=!1;render(){return e("slot",{key:"b60e214835055b86c02d42eb28a71ac0ad629c7c"})}};t.style=o;export{t as ifx_dropdown_trigger};
+//# sourceMappingURL=ifx-dropdown-trigger.entry-CTkrJslZ.js.map
