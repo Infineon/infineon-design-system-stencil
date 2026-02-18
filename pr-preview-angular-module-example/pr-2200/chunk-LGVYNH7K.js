dbf8c78591cb97e696280e3ab9100d96a534b462
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-02-18 14:20:18 +0000

    Deploy preview for PR 2200 🛫

---
+++ pr-preview-angular-module-example/pr-2200/chunk-LGVYNH7K.js
@@ -0,0 +1,1 @@
+import{h as r,j as s}from"./chunk-TOXWW4ZF.js";var o=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(e){r(this,e)}isOpen=!1;render(){return s("slot",{key:"b60e214835055b86c02d42eb28a71ac0ad629c7c"})}};t.style=o();export{t as ifx_dropdown_trigger};
