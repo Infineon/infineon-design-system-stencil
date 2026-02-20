d6fbaebb519b304804069e834bf75016b3754861
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-02-20 09:22:15 +0000

    Deploy preview for PR 2200 🛫

--- pr-preview-html-vite-example/pr-2200/assets/ifx-dropdown-trigger.entry-TEZRbuI3.js
+++
@@ -1,2 +0,0 @@
-import{r as s,h as e}from"./index-CVUwPgPZ.js";const o=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){s(this,r)}isOpen=!1;render(){return e("slot",{key:"5332d5520c2d722d24f6823b637b5968be867e50"})}};t.style=o();export{t as ifx_dropdown_trigger};
-//# sourceMappingURL=ifx-dropdown-trigger.entry-TEZRbuI3.js.map
