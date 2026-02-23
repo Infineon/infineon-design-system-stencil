aaf36f0c1639f03de098d0424e17cc37b79373ee
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-02-23 09:04:28 +0000

    Deploy preview for PR 2146 🛫

---
+++ pr-preview-storybook/pr-2146/assets/BorderRadius-DQavdohh.js
@@ -0,0 +1,13 @@
+import{j as e,M as o}from"./blocks-DnfXKuNL.js";import{useMDXComponents as s}from"./index-fASNx1Lf.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CAkk9qRm.js";const a=""+new URL("border-radius-1px-cxxFftCw.png",import.meta.url).href,i=""+new URL("border-radius-9999px-BGUrk0do.png",import.meta.url).href;function t(n){const r={h1:"h1",h2:"h2",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"Foundations/Border radius"}),`
+`,e.jsx(r.h1,{id:"border-radius",children:"Border radius"}),`
+`,e.jsx(r.p,{children:"A fundamental aspect of our visual design. It refers to the curvature applied to the corners of elements, such as buttons, cards, or containers. It shapes the overall aesthetic and feel of our interfaces, influencing user perception and interaction."}),`
+`,e.jsx(r.h2,{id:"1px-border-radius",children:"1px Border radius"}),`
+`,e.jsx("img",{src:a,alt:"BorderRadius1px",width:"50%"}),`
+`,e.jsx(r.p,{children:`We use a 1px border radius for all rectangular objects in our design system.
+This includes buttons, text inputs, checkboxes, radio buttons, and other rectangular elements.
+A 1px border radius creates a slight curve at the corners of these elements, which softens their appearance and helps creating a more friendly and approachable design.`}),`
+`,e.jsx(r.h2,{id:"9999px-border-radius",children:"9999px Border radius"}),`
+`,e.jsx("img",{src:i,alt:"BorderRadius9999px",width:"50%"}),`
+`,e.jsx(r.p,{children:`In addition to the 1px border radius, we use a 9999px border radius for certain components in our design system.
+These components include chips, tags, toggles, and filter components.
+This style creates a pill shape for these elements, which helps distinguish them from other rectangular elements on the page and makes them more visually appealing.`})]})}function l(n={}){const{wrapper:r}={...s(),...n.components};return r?e.jsx(r,{...n,children:e.jsx(t,{...n})}):t(n)}export{l as default};
