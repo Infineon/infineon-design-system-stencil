eb603625c23daf1875f6b99ea9f72f12a436a7c8
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-02-25 14:50:27 +0000

    Deploy preview for PR 2211 🛫

---
+++ pr-preview-html-vite-example/pr-2211/assets/ifx-dropdown-separator.entry-DNQJtBZ0.js
@@ -0,0 +1,2 @@
+import{r as o,h as a}from"./index-DxashIJq.js";const s=()=>".dropdown-separator{height:1px;background-color:#EEEDED;margin:8px 16px}",e=class{constructor(r){o(this,r)}render(){return a("div",{key:"3d64099049093804bc208578d458843e264964ae",class:"dropdown-separator"})}};e.style=s();export{e as ifx_dropdown_separator};
+//# sourceMappingURL=ifx-dropdown-separator.entry-DNQJtBZ0.js.map
---
+++ pr-preview-html-vite-example/pr-2211/assets/ifx-dropdown-trigger.entry-BxPWtFOW.js
@@ -0,0 +1,2 @@
+import{r as s,h as e}from"./index-DxashIJq.js";const o=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){s(this,r)}isOpen=!1;render(){return e("slot",{key:"fd4c8d12bb2c39de18355967438a36b2adc866ef"})}};t.style=o();export{t as ifx_dropdown_trigger};
+//# sourceMappingURL=ifx-dropdown-trigger.entry-BxPWtFOW.js.map
