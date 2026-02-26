a730226af9e471d0d44467ff0d1747cc92415495
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-02-26 14:43:31 +0000

    Deploy preview for PR 2146 🛫

---
+++ pr-preview-storybook/pr-2146/assets/Development-D4NUtlDu.js
@@ -0,0 +1,5 @@
+import{j as n,M as e,P as a,a as i}from"./blocks-CERx25n9.js";import{useMDXComponents as s}from"./index-HlHJfxpl.js";import{N as m}from"./navbar.stories-CJDdo1FG.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-B2pKNNw0.js";import"./icons-DyNdhNdc.js";function r(t){const o={h1:"h1",h2:"h2",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(e,{of:m}),`
+`,n.jsx(o.h1,{id:"navbar",children:"Navbar"}),`
+`,n.jsx(a,{}),`
+`,n.jsx(o.h2,{id:"props",children:"Props"}),`
+`,n.jsx(i,{})]})}function f(t={}){const{wrapper:o}={...s(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(r,{...t})}):r(t)}export{f as default};
