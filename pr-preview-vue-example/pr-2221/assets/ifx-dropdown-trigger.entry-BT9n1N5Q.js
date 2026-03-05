1c65000d564912d348b6b21467af8640111dec62
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-03-05 16:00:57 +0000

    Deploy preview for PR 2221 🛫

--- pr-preview-vue-example/pr-2221/assets/ifx-dropdown-trigger.entry-BT9n1N5Q.js
+++
@@ -1,1 +0,0 @@
-import{r as s,h as o}from"./index-DqpOvhIF.js";const e=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){s(this,r)}isOpen=!1;render(){return o("slot",{key:"bddf7c934f0271e9440519fcd95266d6cf6c8a13"})}};t.style=e();export{t as ifx_dropdown_trigger};
