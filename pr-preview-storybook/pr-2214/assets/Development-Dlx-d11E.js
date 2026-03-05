b315fedc1a89d184585160dac469d638a53aa11c
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-03-05 10:33:18 +0000

    Deploy preview for PR 2214 🛫

--- pr-preview-storybook/pr-2214/assets/Development-Dlx-d11E.js
+++
@@ -1,5 +0,0 @@
-import{j as n,M as e,P as a,a as i}from"./blocks-HfOkDWyX.js";import{useMDXComponents as s}from"./index-DFTCxDqk.js";import{N as m}from"./navbar.stories-XC0hAtgq.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-B6bDZb6P.js";import"./icons-DxYTBSFo.js";function r(t){const o={h1:"h1",h2:"h2",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(e,{of:m}),`
-`,n.jsx(o.h1,{id:"navbar",children:"Navbar"}),`
-`,n.jsx(a,{}),`
-`,n.jsx(o.h2,{id:"props",children:"Props"}),`
-`,n.jsx(i,{})]})}function f(t={}){const{wrapper:o}={...s(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(r,{...t})}):r(t)}export{f as default};
