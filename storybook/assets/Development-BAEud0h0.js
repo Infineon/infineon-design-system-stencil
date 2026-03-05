0a0e693a42caeed72059749bbe25203d9696fb74
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-03-05 15:08:05 +0000

    deploy: a0cf02043b01f9495c6a003ddcabcde86e48b5da

--- storybook/assets/Development-BAEud0h0.js
+++
@@ -1,5 +0,0 @@
-import{j as n,M as e,P as a,a as i}from"./blocks-BRaCSHdN.js";import{useMDXComponents as s}from"./index-Br3yKi2q.js";import{N as m}from"./navbar.stories-C7xxwRZl.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BGzbY2nt.js";import"./icons-DxYTBSFo.js";function r(t){const o={h1:"h1",h2:"h2",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(e,{of:m}),`
-`,n.jsx(o.h1,{id:"navbar",children:"Navbar"}),`
-`,n.jsx(a,{}),`
-`,n.jsx(o.h2,{id:"props",children:"Props"}),`
-`,n.jsx(i,{})]})}function f(t={}){const{wrapper:o}={...s(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(r,{...t})}):r(t)}export{f as default};
