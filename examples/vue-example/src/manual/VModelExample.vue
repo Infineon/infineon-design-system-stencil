895e34ed053e6e8f8457df36f3d6fbd9c9ac9539
Pfaff Fabian <fabian.pfaff@infineon.com>
2026-03-05 16:06:04 +0100

    docs: add v-model, ngModel  examples

---
+++ examples/vue-example/src/manual/VModelExample.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import { onMounted, nextTick, ref } from 'vue';
+import Prism from 'prismjs';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-markup-templating';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-typescript';
+import { IfxCheckbox, IfxSwitch, IfxSearchBar, IfxSearchField } from '@infineon/infineon-design-system-vue';
+
+
+const booleanValue = ref(false);
+
+const textValue = ref('');
+
+const codeString = `<script setup lang="ts">
+import { IfxCheckbox, IfxSwitch, IfxSearchBar, IfxSearchField } from '@infineon/infineon-design-system-vue';
+
+const booleanValue = ref(false);
+const textValue = ref('');
+
+${'</'}script>
+
+<template>
+  <div>
+    <h3>Boolean inputs (bound to same value)</h3>
+    <ifx-checkbox v-model="booleanValue">
+        Checkbox 1
+    </ifx-checkbox>
+    <ifx-switch v-model="booleanValue">
+        Switch 1
+    </ifx-switch>
+
+    <h3>Text inputs (bound to same value)</h3>
+    <ifx-search-field v-model="textValue" size="s" :show-delete-icon="true"></ifx-search-field>
+    <ifx-search-bar v-model="textValue" :is-open="true"></ifx-search-bar>
+  </div>
+${'</'}template>`;
+
+onMounted(() => {
+  nextTick(() => {
+    Prism.highlightAll();
+  });
+});
+</script>
+
+<template>
+  <div class="checkbox-container">
+    <h3>Boolean inputs (bound to same value)</h3>
+    <ifx-checkbox v-model="booleanValue">
+        Checkbox 1
+    </ifx-checkbox>
+    <ifx-checkbox v-model="booleanValue">
+        Checkbox 2
+    </ifx-checkbox>
+    <ifx-switch v-model="booleanValue">
+        Switch 1
+    </ifx-switch>
+
+    <h3>Text inputs (bound to same value)</h3>
+    <ifx-search-field v-model="textValue" size="s" :show-delete-icon="true"></ifx-search-field>
+    <ifx-search-bar v-model="textValue" :is-open="true"></ifx-search-bar>
+
+    <details class="code-details">
+      <summary>View Code</summary>
+      <pre><code class="language-markup">{{ codeString }}</code></pre>
+    </details>
+  </div>
+</template>
+<style>
+h2 {
+    margin-bottom: 0rem;
+}
+.checkbox-container {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
