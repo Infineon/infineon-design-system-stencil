13b1477e5b0d6a0307f63a51759f0482ed79e1f6
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-03-06 09:21:21 +0000

    Deploy preview for PR 2225 🛫

---
+++ pr-preview-angular-module-example/pr-2225/chunk-DBYP5WRS.js
@@ -0,0 +1,1 @@
+import{h as r,j as o}from"./chunk-AOUN5CLL.js";var e=()=>".dropdown-separator{height:1px;background-color:#EEEDED;margin:8px 16px}",s=class{constructor(a){r(this,a)}render(){return o("div",{key:"3a28835dcd7dd300d9b7409e7e84fe4388767222",class:"dropdown-separator"})}};s.style=e();export{s as ifx_dropdown_separator};
