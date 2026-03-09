1724f80524e4ed2909cba999610be45435e7b9cc
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-03-09 13:17:17 +0000

    Deploy preview for PR 2226 🛫

--- pr-preview-storybook/pr-2226/assets/Development-DBbz_L99.js
+++
@@ -1,5 +0,0 @@
-import{j as o,M as i,a as m}from"./blocks-mzwZX8d9.js";import{useMDXComponents as a}from"./index-CBgOKRyQ.js";import{N as c,D as r}from"./navbar.stories-C7jHdyDW.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-J6CsxBZd.js";import"./icons-DxYTBSFo.js";function s(n){const t={h1:"h1",h2:"h2",...a(),...n.components},{Canvas:e}=t;return e||p("Canvas"),o.jsxs(o.Fragment,{children:[o.jsx(i,{of:c}),`
-`,o.jsx(t.h1,{id:"navbar",children:"Navbar"}),`
-`,o.jsx(e,{of:r,sourceState:"shown"}),`
-`,o.jsx(t.h2,{id:"props",children:"Props"}),`
-`,o.jsx(m,{of:r})]})}function M(n={}){const{wrapper:t}={...a(),...n.components};return t?o.jsx(t,{...n,children:o.jsx(s,{...n})}):s(n)}function p(n,t){throw new Error("Expected component `"+n+"` to be defined: you likely forgot to import, pass, or provide it.")}export{M as default};
