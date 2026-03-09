33afa7b6a654cc67a2d36045b249470dd82fc884
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-03-09 09:51:57 +0000

    Deploy preview for PR 2226 🛫

--- pr-preview-storybook/pr-2226/assets/Icons-cXXn07qW.js
+++
@@ -1,2 +0,0 @@
-import{j as e,M as o}from"./blocks-CPYYb0kv.js";import{useMDXComponents as s}from"./index-BATT00nt.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-d_2_vCsi.js";function r(t){return e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"Icon library",parameters:{previewTabs:{canvas:{hidden:!0},"storybook/docs/panel":{hidden:!0},"usage-tab/panel":{hidden:!0}}}}),`
-`,e.jsx("ifx-icons-preview",{})]})}function c(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(r,{...t})}):r()}export{c as default};
