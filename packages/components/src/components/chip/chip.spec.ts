6f6a7b3d5cffd6f83a5f5b0762b5d3210165fc95
Yanchev Tihomir (IT ERP CDX / EE) <Tihomir.Yanchev-EE@infineon.com>
2026-03-10 10:35:40 +0100

    updates the spec file

--- packages/components/src/components/chip/chip.spec.ts
+++ packages/components/src/components/chip/chip.spec.ts
@@ -34,7 +34,7 @@ describe("ifx-chip", () => {
   <template shadowrootmode="open">
     <div class="chip">
       <div aria-controls="dropdown" aria-expanded="false" aria-haspopup="listbox" aria-value="" class="chip__wrapper chip__wrapper--medium chip__wrapper--single outlined" role="combobox" tabindex="0">
-        <div class="wrapper__label">
+        <div class="wrapper__label" part="label">
           Select an option
         </div>
         <div class="wrapper__open-button">
