5095c2e2d8a40cbb6c2e8fb24f457df24ceeaee4
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-03-11 13:53:20 +0000

    Deploy preview for PR 2230 🛫

---
+++ pr-preview-storybook/pr-2230/assets/FAQ-CvTGqSC2.js
@@ -0,0 +1,3 @@
+import{j as t,M as r}from"./blocks-CS8Kj_oA.js";import{useMDXComponents as s}from"./index-Cm8x8D-m.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-LWOP6J3v.js";function o(n){const e={h1:"h1",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{title:"Setup & installation/FAQ"}),`
+`,t.jsx(e.h1,{id:"frequently-asked-questions",children:"Frequently asked questions"}),`
+`,t.jsx("ifx-faq",{})]})}function x(n={}){const{wrapper:e}={...s(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(o,{...n})}):o(n)}export{x as default};
