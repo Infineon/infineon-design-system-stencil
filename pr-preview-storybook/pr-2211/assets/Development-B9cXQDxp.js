8d5d2bd270e804f7e3e0ea84c1e5402a1e15df83
halmleticia <halmleticia@users.noreply.github.com>
2026-03-16 14:05:09 +0000

    Deploy preview for PR 2211 🛫

---
+++ pr-preview-storybook/pr-2211/assets/Development-B9cXQDxp.js
@@ -0,0 +1,7 @@
+import{j as t,M as a,C as s,a as c}from"./blocks-58SCzr0p.js";import{useMDXComponents as i}from"./index-BKVFcMP4.js";import{S as h,D as o}from"./multiselect.stories-Dy6Kihgm.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CkozM5K6.js";function r(e){const n={h1:"h1",h2:"h2",h3:"h3",...i(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:h}),`
+`,t.jsx(n.h1,{id:"multi-select",children:"Multi Select"}),`
+`,t.jsx(s,{of:o,sourceState:"shown"}),`
+`,t.jsx(c,{of:o}),`
+`,t.jsx(n.h2,{id:"stories",children:"Stories"}),`
+`,t.jsx(n.h3,{id:"default",children:"Default"}),`
+`,t.jsx(s,{of:o,sourceState:"shown"})]})}function d(e={}){const{wrapper:n}={...i(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(r,{...e})}):r(e)}export{d as default};
