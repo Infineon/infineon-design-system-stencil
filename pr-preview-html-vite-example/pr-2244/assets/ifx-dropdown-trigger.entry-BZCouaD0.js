13cf3c5bdc0c06d5cd057c5b48cf911bad7aca1d
halmleticia <halmleticia@users.noreply.github.com>
2026-03-16 14:30:18 +0000

    Deploy preview for PR 2244 🛫

--- pr-preview-html-vite-example/pr-2244/assets/ifx-dropdown-trigger.entry-BZCouaD0.js
+++
@@ -1,2 +0,0 @@
-import{r as s,h as o}from"./index-R4-lZkrm.js";const e=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){s(this,r)}isOpen=!1;render(){return o("slot",{key:"d643400d88cc186178923aadc449cbf90a3b6e83"})}};t.style=e();export{t as ifx_dropdown_trigger};
-//# sourceMappingURL=ifx-dropdown-trigger.entry-BZCouaD0.js.map
---
+++ pr-preview-html-vite-example/pr-2244/assets/ifx-dropdown-trigger.entry-e0FPwUjS.js
@@ -0,0 +1,2 @@
+import{r as s,h as o}from"./index-CdQulq8k.js";const e=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){s(this,r)}isOpen=!1;render(){return o("slot",{key:"d643400d88cc186178923aadc449cbf90a3b6e83"})}};t.style=e();export{t as ifx_dropdown_trigger};
+//# sourceMappingURL=ifx-dropdown-trigger.entry-e0FPwUjS.js.map
