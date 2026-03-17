33f543982a5e7ce3f36aaf5e016186164827eb13
halmleticia <halmleticia@users.noreply.github.com>
2026-03-17 08:25:03 +0000

    Deploy preview for PR 2244 🛫

---
+++ pr-preview-html-vite-example/pr-2244/assets/ifx-dropdown-trigger.entry-CrL6uLVa.js
@@ -0,0 +1,2 @@
+import{r as s,h as o}from"./index--g-cSpJp.js";const e=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){s(this,r)}isOpen=!1;render(){return o("slot",{key:"d643400d88cc186178923aadc449cbf90a3b6e83"})}};t.style=e();export{t as ifx_dropdown_trigger};
+//# sourceMappingURL=ifx-dropdown-trigger.entry-CrL6uLVa.js.map
