a0959d8da59a5c97e549b7a36cb33f1051e2ed5c
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-03-18 10:37:23 +0000

    Deploy preview for PR 2229 🛫

---
+++ pr-preview-storybook/pr-2229/assets/formatter-EIJCOSYU-BFHfgvrc.js
@@ -0,0 +1,1 @@
+import{d as i,e as m}from"./blocks-zFMMK8rW.js";import{c as o}from"./iframe-Ckkkxb3f.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(m(),1),s=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{s as formatter};
