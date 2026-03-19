9924a043ebb2a92dd5de4f9eb0048083a8fa1cd5
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-03-19 12:59:54 +0000

    Deploy preview for PR 2241 🛫

--- pr-preview-storybook/pr-2241/assets/formatter-EIJCOSYU-CjNAZG1A.js
+++
@@ -1,1 +0,0 @@
-import{d as i,e as m}from"./blocks--G9ML-ll.js";import{c as o}from"./iframe-BfN4AYNq.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(m(),1),s=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{s as formatter};
