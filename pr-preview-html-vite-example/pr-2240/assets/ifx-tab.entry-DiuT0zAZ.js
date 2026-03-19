a7c72b26ffbcb45fda9d3e3e8e7a4b02f82e616f
halmleticia <halmleticia@users.noreply.github.com>
2026-03-19 11:23:24 +0000

    Deploy preview for PR 2240 🛫

--- pr-preview-html-vite-example/pr-2240/assets/ifx-tab.entry-DiuT0zAZ.js
+++
@@ -1,2 +0,0 @@
-import{r as t,c as a,g as s,h as n}from"./index-B6cfZio4.js";const r=()=>":host{display:block}",i=class{constructor(e){t(this,e),this.tabHeaderChange=a(this,"tabHeaderChange",7)}get el(){return s(this)}header;disabled;icon="";iconPosition="left";subline;label;number=0;positionSticky=!1;tabHeaderChange;componentWillUpdate(){this.tabHeaderChange.emit(this.header)}render(){return n("slot",{key:"3bcdb64788b4f7f7f0fc54c40ae4945178f18d25"})}};i.style=r();export{i as ifx_tab};
-//# sourceMappingURL=ifx-tab.entry-DiuT0zAZ.js.map
