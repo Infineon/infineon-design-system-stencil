2e4bf779e7b052959596ad811ce8bce388056958
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-03-19 13:21:43 +0000

    Deploy preview for PR 2240 🛫

--- pr-preview-storybook/pr-2240/assets/Development-BUT6s-uY.js
+++
@@ -1,5 +0,0 @@
-import{u as a,j as o,M as i,a as c}from"./blocks-CrlG3NP4.js";import{N as m,D as r}from"./navbar.stories-BcajG-NE.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-C8xM4lar.js";import"./icons-CBa5BGDq.js";function s(n){const t={h1:"h1",h2:"h2",...a(),...n.components},{Canvas:e}=t;return e||p("Canvas"),o.jsxs(o.Fragment,{children:[o.jsx(i,{of:m}),`
-`,o.jsx(t.h1,{id:"navbar",children:"Navbar"}),`
-`,o.jsx(e,{of:r,sourceState:"shown"}),`
-`,o.jsx(t.h2,{id:"props",children:"Props"}),`
-`,o.jsx(c,{of:r})]})}function l(n={}){const{wrapper:t}={...a(),...n.components};return t?o.jsx(t,{...n,children:o.jsx(s,{...n})}):s(n)}function p(n,t){throw new Error("Expected component `"+n+"` to be defined: you likely forgot to import, pass, or provide it.")}export{l as default};
