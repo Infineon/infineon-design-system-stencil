35ccc982875bdddb44bb1350d871b1f97b7d0dd3
Pfaff Fabian <fabian.pfaff@infineon.com>
2026-03-23 13:56:41 +0100

    fix(examples): generate examples

--- examples/angular-standalone-example/src/app/generated/ifx-pagination-example/ifx-pagination-example.ts
+++ examples/angular-standalone-example/src/app/generated/ifx-pagination-example/ifx-pagination-example.ts
@@ -33,6 +33,7 @@ export class IfxPaginationExample {
     current-page=&quot;1&quot;
     [showItemsPerPage]=&quot;true&quot;
     items-per-page=&#039;[{&quot;value&quot;:&quot;10&quot;,&quot;selected&quot;:true}, {&quot;value&quot;:&quot;20&quot;,&quot;selected&quot;:false}, {&quot;value&quot;:&quot;30&quot;,&quot;selected&quot;:false}, {&quot;value&quot;:&quot;all&quot;,&quot;selected&quot;:false}]&#039;
+    items-per-page-label=&quot;Result per Pages&quot;
     (ifxItemsPerPageChange)=&quot;handleItemsPerPageChange(\$any(\$event))&quot;
     (ifxPageChange)=&quot;handlePageChange(\$any(\$event))&quot;&gt;&lt;/ifx-pagination&gt;`;
 
--- examples/vue-example/src/generated/IfxPaginationExample.vue
+++ examples/vue-example/src/generated/IfxPaginationExample.vue
@@ -31,6 +31,7 @@ ${'</'}script>
       :current-page=1
       show-items-per-page=""
       items-per-page='[{"value":"10","selected":true}, {"value":"20","selected":false}, {"value":"30","selected":false}, {"value":"all","selected":false}]'
+      items-per-page-label="Result per Pages"
       @ifxItemsPerPageChange="handleItemsPerPageChange"
       @ifxPageChange="handlePageChange" />
   </div>
@@ -45,6 +46,7 @@ ${'</'}template>`;
       :current-page=1
       show-items-per-page=""
       items-per-page='[{"value":"10","selected":true}, {"value":"20","selected":false}, {"value":"30","selected":false}, {"value":"all","selected":false}]'
+      items-per-page-label="Result per Pages"
       @ifxItemsPerPageChange="handleItemsPerPageChange"
       @ifxPageChange="handlePageChange" />
     <details class="code-details">
