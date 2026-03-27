8e33ebccf636a0e1cf1fd87dc05e257d93cdd33f
halmleticia <halmleticia@users.noreply.github.com>
2026-03-27 12:57:15 +0000

    Deploy preview for PR 2268 🛫

--- pr-preview-storybook/pr-2268/assets/FAQ-G1k6FgZB.js
+++
@@ -1,3 +0,0 @@
-import{u as o,j as t,M as i}from"./blocks-CnDXgbz_.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-HRE5TKDJ.js";function s(n){const e={h1:"h1",...o(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Setup & installation/FAQ"}),`
-`,t.jsx(e.h1,{id:"frequently-asked-questions",children:"Frequently asked questions"}),`
-`,t.jsx("ifx-faq",{})]})}function m(n={}){const{wrapper:e}={...o(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(s,{...n})}):s(n)}export{m as default};
