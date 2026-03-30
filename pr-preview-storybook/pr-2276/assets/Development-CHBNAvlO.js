c9cb5f9dc8c218d3da443f32f3758718106f3cb8
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-03-30 12:55:09 +0000

    Deploy preview for PR 2276 🛫

---
+++ pr-preview-storybook/pr-2276/assets/Development-CHBNAvlO.js
@@ -0,0 +1,5 @@
+import{j as n,M as a,C as i,a as m}from"./blocks-D6PanKgZ.js";import{useMDXComponents as e}from"./index-CwrQJG_s.js";import{N as c,D as r}from"./navbar.stories-j98Rm8_o.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-uLISxhkE.js";import"./icons-DxYTBSFo.js";function s(o){const t={h1:"h1",h2:"h2",...e(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{of:c}),`
+`,n.jsx(t.h1,{id:"navbar",children:"Navbar"}),`
+`,n.jsx(i,{of:r,sourceState:"shown"}),`
+`,n.jsx(t.h2,{id:"props",children:"Props"}),`
+`,n.jsx(m,{of:r})]})}function l(o={}){const{wrapper:t}={...e(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(s,{...o})}):s(o)}export{l as default};
