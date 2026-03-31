dd77ef21d97f9f0ab93203736154adebf7f361c5
bestcatarina <bestcatarina@users.noreply.github.com>
2026-03-31 09:47:06 +0000

    Deploy preview for PR 2262 🛫

--- pr-preview-html-vite-example/pr-2262/assets/ifx-tab.entry-qC12ZfUS.js
+++
@@ -1,2 +0,0 @@
-import{r as t,c as a,g as s,h as n}from"./index-BbmUNzhS.js";const r=()=>":host{display:block}",i=class{constructor(e){t(this,e),this.tabHeaderChange=a(this,"tabHeaderChange",7)}get el(){return s(this)}header;disabled;icon="";iconPosition="left";subline;label;number=0;positionSticky=!1;tabHeaderChange;componentWillUpdate(){this.tabHeaderChange.emit(this.header)}render(){return n("slot",{key:"b5efdaad552ca58cdfa76aff6634c29772115818"})}};i.style=r();export{i as ifx_tab};
-//# sourceMappingURL=ifx-tab.entry-qC12ZfUS.js.map
