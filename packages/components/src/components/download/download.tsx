1dacc1034a913fa2e9e00d20506583045978492b
Yanchev Tihomir (IT ERP CDX / EE) <Tihomir.Yanchev-EE@infineon.com>
2026-04-01 16:16:06 +0200

    reverts back changes

--- packages/components/src/components/download/download.tsx
+++ packages/components/src/components/download/download.tsx
@@ -1,49 +1,50 @@
 import { Component, h, Prop } from "@stencil/core";
+
 @Component({
-  tag: "ifx-download",
-  styleUrl: "./download.scss",
-  shadow: true,
+	tag: "ifx-download",
+	styleUrl: "./download.scss",
+	shadow: true,
 })
 export class Download {
-  /** Type of file to download. */
-  @Prop() readonly fileType: "css" | "scss" | "json" | "js" = "css";
-	
-  private readonly fileMap = {
-    css: "https://raw.githubusercontent.com/Infineon/public-assets/main/ifx-tokens/css-variables.css",
-    scss: "https://raw.githubusercontent.com/Infineon/public-assets/main/ifx-tokens/scss-variables.scss",
-    json: "https://raw.githubusercontent.com/Infineon/public-assets/main/ifx-echarts-theme/dds-echarts-theme.json",
-    js: "https://raw.githubusercontent.com/Infineon/public-assets/main/ifx-echarts-theme/dds-echarts-theme.js",
-  };
-
-  private get fileName(): string {
-    return this.fileType === "json" || this.fileType === "js"
-      ? `dds-echarts-theme.${this.fileType}`
-      : `infineon-tokens.${this.fileType}`;
-  }
+/** Type of file to download. */
+  @Prop() readonly tokens: "css" | "scss" | "json" | "js" = "css";
 
   render() {
-    const fileUrl = this.fileMap[this.fileType];
-    return (
-      <ifx-link
-        onClick={(e) => {
-          e.preventDefault();
-          fetch(fileUrl)
-            .then((res) => res.blob())
-            .then((blob) => {
-              const link = document.createElement("a");
-              link.href = URL.createObjectURL(blob);
-              link.download = this.fileName;
-              link.click();
-            });
-        }}
-        href={fileUrl}
-        download={this.fileName}
-        class="download__wrapper"
-        size="m"
-        variant="bold"
-      >
-        <slot />
-      </ifx-link>
-    );
-  }
+    const fileMap = {
+      css: "https://raw.githubusercontent.com/Infineon/public-assets/main/ifx-tokens/css-variables.css",
+      scss: "https://raw.githubusercontent.com/Infineon/public-assets/main/ifx-tokens/scss-variables.scss",
+      json: "https://raw.githubusercontent.com/Infineon/public-assets/main/ifx-echarts-theme/dds-echarts-theme.json",
+      js: "https://raw.githubusercontent.com/Infineon/public-assets/main/ifx-echarts-theme/dds-echarts-theme.js",
+    };
+
+    const fileUrl = fileMap[this.tokens];
+
+    const fileName =
+      this.tokens === "json" || this.tokens === "js"
+        ? `dds-echarts-theme.${this.tokens}`
+        : `infineon-tokens.${this.tokens}`;
+
+		return (
+			<ifx-link
+				onClick={(e) => {
+					e.preventDefault();
+					fetch(fileUrl)
+						.then((res) => res.blob())
+						.then((blob) => {
+							const link = document.createElement("a");
+							link.href = URL.createObjectURL(blob);
+							link.download = fileName;
+							link.click();
+						});
+				}}
+				href={fileUrl}
+				download={fileName}
+				class="download__wrapper"
+				size="m"
+				variant="bold"
+			>
+				<slot />
+			</ifx-link>
+		);
+	}
 }
