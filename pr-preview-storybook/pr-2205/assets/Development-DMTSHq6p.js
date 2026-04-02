776cf9bd9813e1156c21b153f1520afc531f4bbb
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-04-02 13:47:23 +0000

    Deploy preview for PR 2205 🛫

--- pr-preview-storybook/pr-2205/assets/Development-DMTSHq6p.js
+++
@@ -1,5 +0,0 @@
-import{j as n,M as a,C as i,a as m}from"./blocks-3l8IQYv4.js";import{useMDXComponents as e}from"./index-2uHTXlBr.js";import{N as c,D as r}from"./navbar.stories-C61rYaI0.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-AMooc2zo.js";import"./icons-CBa5BGDq.js";function s(o){const t={h1:"h1",h2:"h2",...e(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{of:c}),`
-`,n.jsx(t.h1,{id:"navbar",children:"Navbar"}),`
-`,n.jsx(i,{of:r,sourceState:"shown"}),`
-`,n.jsx(t.h2,{id:"props",children:"Props"}),`
-`,n.jsx(m,{of:r})]})}function l(o={}){const{wrapper:t}={...e(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(s,{...o})}):s(o)}export{l as default};
---
+++ pr-preview-storybook/pr-2205/assets/Development-DV5CILzK.js
@@ -0,0 +1,5 @@
+import{j as n,M as a,C as i,a as m}from"./blocks-DBoT1nPl.js";import{useMDXComponents as e}from"./index-DNwPMls8.js";import{N as c,D as r}from"./navbar.stories-CT5YAFmO.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CmfF9EKj.js";import"./icons-CBa5BGDq.js";function s(o){const t={h1:"h1",h2:"h2",...e(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{of:c}),`
+`,n.jsx(t.h1,{id:"navbar",children:"Navbar"}),`
+`,n.jsx(i,{of:r,sourceState:"shown"}),`
+`,n.jsx(t.h2,{id:"props",children:"Props"}),`
+`,n.jsx(m,{of:r})]})}function l(o={}){const{wrapper:t}={...e(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(s,{...o})}):s(o)}export{l as default};
