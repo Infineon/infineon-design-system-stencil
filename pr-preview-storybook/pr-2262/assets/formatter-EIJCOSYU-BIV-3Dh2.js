40fc59590ca9e265f0822021578b0ad9337a7c95
bestcatarina <bestcatarina@users.noreply.github.com>
2026-04-10 11:42:08 +0000

    Deploy preview for PR 2262 🛫

---
+++ pr-preview-storybook/pr-2262/assets/formatter-EIJCOSYU-BIV-3Dh2.js
@@ -0,0 +1,1 @@
+import{d as i,e as m}from"./blocks-Nb9V_opJ.js";import{c as o}from"./iframe-CEknyqrr.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(m(),1),s=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{s as formatter};
