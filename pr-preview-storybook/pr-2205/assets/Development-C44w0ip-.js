39e04a2b1d9fd25a7e4941b010351d62139c99c6
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-04-15 08:27:52 +0000

    Deploy preview for PR 2205 🛫

--- pr-preview-storybook/pr-2205/assets/Development-C44w0ip-.js
+++
@@ -1,5 +0,0 @@
-import{j as n,M as a,C as i,a as m}from"./blocks-CQSmLtFo.js";import{useMDXComponents as e}from"./index-DWBK0yCI.js";import{N as c,D as r}from"./navbar.stories-DFC1vdKM.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-B8hNebI8.js";import"./icons-CBa5BGDq.js";function s(o){const t={h1:"h1",h2:"h2",...e(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{of:c}),`
-`,n.jsx(t.h1,{id:"navbar",children:"Navbar"}),`
-`,n.jsx(i,{of:r,sourceState:"shown"}),`
-`,n.jsx(t.h2,{id:"props",children:"Props"}),`
-`,n.jsx(m,{of:r})]})}function l(o={}){const{wrapper:t}={...e(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(s,{...o})}):s(o)}export{l as default};
