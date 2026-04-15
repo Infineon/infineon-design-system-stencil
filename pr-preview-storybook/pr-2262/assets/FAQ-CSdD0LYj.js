705a047fe64c72e05c360c8b75507bc5cee6c0ad
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-04-15 07:34:19 +0000

    Deploy preview for PR 2262 🛫

---
+++ pr-preview-storybook/pr-2262/assets/FAQ-CSdD0LYj.js
@@ -0,0 +1,3 @@
+import{u as o,j as t,M as i}from"./blocks-CmVhLKXt.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-C0VfoxTL.js";function s(n){const e={h1:"h1",...o(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Setup & installation/FAQ"}),`
+`,t.jsx(e.h1,{id:"frequently-asked-questions",children:"Frequently asked questions"}),`
+`,t.jsx("ifx-faq",{})]})}function m(n={}){const{wrapper:e}={...o(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(s,{...n})}):s(n)}export{m as default};
