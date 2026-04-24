fc0c08ddaac52326420061facab870ef02e94b73
sofiacarvalho-ifx <sofiacarvalho-ifx@users.noreply.github.com>
2026-04-24 15:28:29 +0000

    Deploy preview for PR 2311 🛫

---
+++ pr-preview-angular-module-example/pr-2311/chunk-J7TJ65QV.js
@@ -0,0 +1,1 @@
+var n=t=>{let e=t.parentNode;for(;e;){if(e instanceof ShadowRoot){e=e.host;continue}if(e instanceof HTMLElement&&e.tagName.toLowerCase().startsWith("ifx"))return!0;e=e.parentNode}return!1};export{n as a};
