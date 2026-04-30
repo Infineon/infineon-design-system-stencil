3b420c99120079e90154ec70d85d4b649c476964
evangeliaPanteliadou <evangeliaPanteliadou@users.noreply.github.com>
2026-04-30 08:35:38 +0000

    Deploy preview for PR 2213 🛫

--- pr-preview-storybook/pr-2213/assets/Development-C2VPvu6d.js
+++
@@ -1,5 +0,0 @@
-import{u as a,j as n,M as e,C as i,a as m}from"./blocks-ChQICO6H.js";import{N as c,D as s}from"./navbar.stories-Dc9voV65.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DXJHQe3t.js";import"./icons-CBa5BGDq.js";function r(t){const o={h1:"h1",h2:"h2",...a(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(e,{of:c}),`
-`,n.jsx(o.h1,{id:"navbar",children:"Navbar"}),`
-`,n.jsx(i,{of:s,sourceState:"shown"}),`
-`,n.jsx(o.h2,{id:"props",children:"Props"}),`
-`,n.jsx(m,{of:s})]})}function d(t={}){const{wrapper:o}={...a(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(r,{...t})}):r(t)}export{d as default};
