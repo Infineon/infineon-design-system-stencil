18641b96221008444741c90b721d7c60b2b0fb65
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-05-04 07:36:56 +0000

    deploy: 18c7a0127e3edc3fade66cfab7ec161c7b4e1b8a

--- storybook/assets/Development-CrjQS00s.js
+++
@@ -1,9 +0,0 @@
-import{u as i,j as e,M as r,C as s,a as h}from"./blocks-CFDEZ05C.js";import{T as x,D as o,F as c}from"./table.stories-GelQDXXW.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-A3HwJ4O3.js";function a(n){const t={h1:"h1",h2:"h2",h3:"h3",...i(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:x}),`
-`,e.jsx(t.h1,{id:"table",children:"Table"}),`
-`,e.jsx(s,{of:o,sourceState:"shown"}),`
-`,e.jsx(h,{of:o}),`
-`,e.jsx(t.h2,{id:"stories",children:"Stories"}),`
-`,e.jsx(t.h3,{id:"default",children:"Default"}),`
-`,e.jsx(s,{of:o,sourceState:"shown"}),`
-`,e.jsx(t.h3,{id:"fixed-table-height",children:"Fixed table height"}),`
-`,e.jsx(s,{of:c,sourceState:"shown"})]})}function m(n={}){const{wrapper:t}={...i(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(a,{...n})}):a(n)}export{m as default};
