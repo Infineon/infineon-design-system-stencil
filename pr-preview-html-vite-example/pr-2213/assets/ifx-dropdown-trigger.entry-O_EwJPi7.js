6190a741f32b0ebab78fff8729211e7933ec572b
evangeliaPanteliadou <evangeliaPanteliadou@users.noreply.github.com>
2026-05-06 08:55:08 +0000

    Deploy preview for PR 2213 🛫

---
+++ pr-preview-html-vite-example/pr-2213/assets/ifx-dropdown-trigger.entry-O_EwJPi7.js
@@ -0,0 +1,2 @@
+import{r as s,h as o}from"./index-CZSMXk91.js";const e=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){s(this,r)}isOpen=!1;render(){return o("slot",{key:"59cae183f64fd5f9c422ffa9486db002c1b67098"})}};t.style=e();export{t as ifx_dropdown_trigger};
+//# sourceMappingURL=ifx-dropdown-trigger.entry-O_EwJPi7.js.map
