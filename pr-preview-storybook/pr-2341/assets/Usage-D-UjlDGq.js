123546fcdb9b33c9b7f4e46d2a7aff6901b5ece4
SergejLust <SergejLust@users.noreply.github.com>
2026-06-02 10:20:03 +0000

    Deploy preview for PR 2341 🛫

---
+++ pr-preview-storybook/pr-2341/assets/Usage-D-UjlDGq.js
@@ -0,0 +1,56 @@
+import{u as o,j as n,M as r}from"./blocks-B9yrnNbg.js";import{I as s}from"./icon-button.stories-Bwu9Wqeu.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-CKYHBmnH.js";import"./icons-CBa5BGDq.js";const a=""+new URL("icon-button-grouping-by-function-3vr0c6Vq.png",import.meta.url).href,c=""+new URL("icon-button-grouping-by-importance-BLZ-q1ST.png",import.meta.url).href,h=""+new URL("icon-button-interaction-B2wVIZxF.png",import.meta.url).href,l=""+new URL("icon-button-primary-states--rY9OqZo.png",import.meta.url).href,u=""+new URL("icon-button-secondary-states-5ZwfBu5p.png",import.meta.url).href,d=""+new URL("icon-button-tertiary-states-DxyYVBBy.png",import.meta.url).href,p=""+new URL("icon-button-sizes-DKtGgfCI.png",import.meta.url).href,m=""+new URL("icon-button-styles-CvVB1oPA.png",import.meta.url).href,y=""+new URL("icon-button-variant-primary-Boyw9NkR.png",import.meta.url).href,x=""+new URL("icon-button-variant-secondary-DFRaqiaO.png",import.meta.url).href,g=""+new URL("icon-button-variant-tertiary-DDx42C3U.png",import.meta.url).href;function i(e){const t={a:"a",h1:"h1",h2:"h2",h3:"h3",h4:"h4",li:"li",p:"p",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...o(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:s}),`
+`,n.jsx(t.h1,{id:"icon-button",children:"Icon Button"}),`
+`,n.jsx(t.p,{children:"Icon buttons can be more space-efficient compared to text buttons, especially when you have limited screen real estate or need to display multiple actions in a compact area. By using icons instead of text labels, you can fit more buttons within a given space without sacrificing usability."}),`
+`,n.jsxs(t.table,{children:[n.jsx(t.thead,{children:n.jsx(t.tr,{children:n.jsx(t.th,{children:"Resources"})})}),n.jsx(t.tbody,{children:n.jsx(t.tr,{children:n.jsx(t.td,{children:n.jsx(t.a,{href:"https://www.figma.com/design/zPvPwPgb4EsYFXKC2nhzSG/Infineon-DDS-%7C-Main?node-id=33465-25094",rel:"nofollow",children:"Figma icon button component →"})})})})]}),`
+`,n.jsx(t.h2,{id:"usage",children:"Usage"}),`
+`,n.jsx(t.h3,{id:"when-to-use",children:"When to use"}),`
+`,n.jsxs(t.ul,{children:[`
+`,n.jsx(t.li,{children:"When you have limited screen space and need to prioritize visual clarity and simplicity."}),`
+`,n.jsx(t.li,{children:'When you want to provide additional functions inside a component like "Delete", "Edit", or just to trigger a dropdown menu.'}),`
+`]}),`
+`,n.jsx(t.h3,{id:"when-not-to-use",children:"When not to use"}),`
+`,n.jsxs(t.ul,{children:[`
+`,n.jsx(t.li,{children:"Avoid using icon-buttons as navigational items."}),`
+`,n.jsx(t.li,{children:"When the user needs to send a message with forms. Instead use a button with label."}),`
+`,n.jsx(t.li,{children:"If the action associated with the button is not self-explanatory or requires additional context. Instead use a button with label."}),`
+`]}),`
+`,n.jsx(t.h2,{id:"variants",children:"Variants"}),`
+`,n.jsx(t.h3,{id:"primary",children:"Primary"}),`
+`,n.jsx(t.p,{children:'Used for CTA-like actions like "Add to cart".'}),`
+`,n.jsx("img",{src:y,alt:"Icon Button Variant Primary",width:"50%"}),`
+`,n.jsx(t.h4,{id:"states",children:"States"}),`
+`,n.jsx("img",{src:l,alt:"Icon Button Primary States"}),`
+`,n.jsx(t.h3,{id:"secondary",children:"Secondary"}),`
+`,n.jsx(t.p,{children:"Mostly used as neighbour icon button beside the primary one."}),`
+`,n.jsx("img",{src:x,alt:"Icon Button Variant Secondary",width:"50%"}),`
+`,n.jsx(t.h4,{id:"states-1",children:"States"}),`
+`,n.jsx("img",{src:u,alt:"Icon Button Secondary States"}),`
+`,n.jsx(t.h3,{id:"tertiary",children:"Tertiary"}),`
+`,n.jsx(t.p,{children:"Mostly used for other supportive actions where visual dominance is not the case."}),`
+`,n.jsx("img",{src:g,alt:"Icon Button Variant Tertiary",width:"50%"}),`
+`,n.jsx(t.h4,{id:"states-2",children:"States"}),`
+`,n.jsx("img",{src:d,alt:"Icon Button Tertiary States"}),`
+`,n.jsx(t.h2,{id:"style",children:"Style"}),`
+`,n.jsx(t.p,{children:"Our design system provides two different styles: round and square."}),`
+`,n.jsx("img",{src:m,alt:"Icon Button Styles"}),`
+`,n.jsx(t.h2,{id:"size",children:"Size"}),`
+`,n.jsx("img",{src:p,alt:"Icon Button Sizes"}),`
+`,n.jsx(t.h3,{id:"xs",children:"XS"}),`
+`,n.jsx(t.p,{children:"Our smallest size which should be handled as a function item. It can be used alone or in an icon group."}),`
+`,n.jsx(t.h3,{id:"s",children:"S"}),`
+`,n.jsx(t.p,{children:"From this size it is possible to provide an icon group which can be grouped by importance. This is our default size."}),`
+`,n.jsx(t.h3,{id:"m",children:"M"}),`
+`,n.jsx(t.p,{children:"To complement other DDS components that have the same size, we offer the same capabilities as the 32px icon button."}),`
+`,n.jsx(t.h3,{id:"l",children:"L"}),`
+`,n.jsx(t.p,{children:"Our largest icon button to be used for single actions such as pagination arrows or a support icon button that has a fixed position when scrolling."}),`
+`,n.jsx(t.h2,{id:"mouse-interaction",children:"Mouse interaction"}),`
+`,n.jsx(t.p,{children:"While icon buttons like the primary and secondary have a visible clicking area, the tertiary one is invisible."}),`
+`,n.jsx("img",{src:h,alt:"Icon Button Interaction"}),`
+`,n.jsx(t.h2,{id:"grouping-icon-buttons",children:"Grouping icon buttons"}),`
+`,n.jsx(t.p,{children:"It is often necessary to provide several icons in a central location. This helps users to recognise and distinguish the actions they can perform on a page or screen. Grouping icon buttons also improves the visual hierarchy of a design, making it clearer and more user-friendly."}),`
+`,n.jsx(t.h3,{id:"group-by-importance",children:"Group by importance"}),`
+`,n.jsx(t.p,{children:'Group icon buttons by importance if you want to help users quickly identify the primary action they should perform. You can use a primary icon button for an important action such as "Add to cart" and a secondary one for "Download PDF". You can also place a tertiary icon button next to the others, e.g. to provide a "More" icon where a drop-down menu with supplementary options can be accessed after clicking.'}),`
+`,n.jsx("img",{src:c,alt:"Icon Button Group by Importacne"}),`
+`,n.jsx(t.h3,{id:"group-by-function",children:"Group by function"}),`
+`,n.jsx(t.p,{children:'Group buttons by function to help users quickly find the action they need to take based on the task they are trying to complete. For example this can be useful in tables to manage a single row with actions like "Download", "Share", "Mark as favorite".'}),`
+`,n.jsx("img",{src:a,alt:"Icon Button Group by Function"})]})}function v(e={}){const{wrapper:t}={...o(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(i,{...e})}):i(e)}export{v as default};
