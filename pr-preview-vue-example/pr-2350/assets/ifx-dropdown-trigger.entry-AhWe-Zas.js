2d9203ba6f5e56869e2ccb66e9953e2833a6af63
limingz20 <limingz20@users.noreply.github.com>
2026-06-03 12:35:45 +0000

    Deploy preview for PR 2350 🛫

---
+++ pr-preview-vue-example/pr-2350/assets/ifx-dropdown-trigger.entry-AhWe-Zas.js
@@ -0,0 +1,1 @@
+import{r as e,h as s}from"./index-Ddo__U79.js";const o=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){e(this,r)}isOpen=!1;render(){return s("slot",{key:"f6c9dde55b37da8315e127a50ae1f8b5e34b0693"})}};t.style=o();export{t as ifx_dropdown_trigger};
