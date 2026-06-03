687373804e097280a8a31e7a576a97d764b2bcb8
limingz20 <limingz20@users.noreply.github.com>
2026-06-03 12:36:53 +0000

    Deploy preview for PR 2350 🛫

---
+++ pr-preview-html-vite-example/pr-2350/assets/ifx-dropdown-separator.entry-BTCrDqv5.js
@@ -0,0 +1,2 @@
+import{r as o,h as e}from"./index-DofwfAlb.js";const a=()=>".dropdown-separator{height:1px;background-color:#eeeded;margin:8px 16px}",s=class{constructor(r){o(this,r)}render(){return e("div",{key:"3d82e65261eabd0e3a8d507cae682c78bc54ea0f",class:"dropdown-separator"})}};s.style=a();export{s as ifx_dropdown_separator};
+//# sourceMappingURL=ifx-dropdown-separator.entry-BTCrDqv5.js.map
---
+++ pr-preview-html-vite-example/pr-2350/assets/ifx-dropdown-trigger.entry-CRCrGWRI.js
@@ -0,0 +1,2 @@
+import{r as e,h as s}from"./index-DofwfAlb.js";const o=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){e(this,r)}isOpen=!1;render(){return s("slot",{key:"f6c9dde55b37da8315e127a50ae1f8b5e34b0693"})}};t.style=o();export{t as ifx_dropdown_trigger};
+//# sourceMappingURL=ifx-dropdown-trigger.entry-CRCrGWRI.js.map
