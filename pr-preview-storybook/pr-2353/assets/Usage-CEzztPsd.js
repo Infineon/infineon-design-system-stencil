905d10f05b0417fa18d3a354d86e6227a96851fb
sofiacarvalho-ifx <sofiacarvalho-ifx@users.noreply.github.com>
2026-06-08 10:34:44 +0000

    Deploy preview for PR 2353 🛫

---
+++ pr-preview-storybook/pr-2353/assets/Usage-CEzztPsd.js
@@ -0,0 +1,24 @@
+import{u as t,j as n,M as o}from"./blocks-dHVMZ2us.js";import{I as a}from"./icon.stories-CbmVAAwo.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DGsNR08F.js";import"./icons-CBa5BGDq.js";function s(i){const e={a:"a",code:"code",h1:"h1",h2:"h2",h3:"h3",h4:"h4",li:"li",p:"p",pre:"pre",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...t(),...i.components};return n.jsxs(n.Fragment,{children:[n.jsx(o,{of:a}),`
+`,n.jsx(e.h1,{id:"icon",children:"Icon"}),`
+`,n.jsx(e.p,{children:"Icons play a crucial role in our design system, providing visual representation and enhancing user experiences. We primarily utilize the Nucleo icon library for our icons, ensuring consistency and a wide range of options."}),`
+`,n.jsxs(e.table,{children:[n.jsx(e.thead,{children:n.jsx(e.tr,{children:n.jsx(e.th,{children:"Resources"})})}),n.jsx(e.tbody,{children:n.jsx(e.tr,{children:n.jsx(e.td,{children:n.jsx(e.a,{href:"https://www.figma.com/design/yWwaLoqsWLWygDxXfvdym9/Infineon-DDS-%7C-UI-icon-library?node-id=6-6",rel:"nofollow",children:"Figma UI icon library →"})})})})]}),`
+`,n.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,n.jsx(e.h3,{id:"when-to-use",children:"When to use"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"Icons can be used alongside text to aid navigation and improve usability. They can represent different sections, categories, or actions, making it easier for users to identify and interact with them."}),`
+`,n.jsx(e.li,{children:"If you want to communicate actions, such as saving, deleting, editing, or sharing. By utilizing recognizable icons, users can quickly understand the purpose or functionality associated with a specific action."}),`
+`]}),`
+`,n.jsx(e.h3,{id:"when-not-to-use",children:"When not to use"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsx(e.li,{children:"If an icon's meaning is unclear or open to interpretation, it is best to use descriptive text instead. Clarity and understanding should always take precedence over aesthetic considerations."}),`
+`,n.jsx(e.li,{children:"Avoid overcrowding interfaces with too many icons, as this can lead to visual clutter and confusion. Strive for a balanced composition that maintains a clean and intuitive design."}),`
+`]}),`
+`,n.jsx(e.h2,{id:"notes",children:"Notes"}),`
+`,n.jsx(e.p,{children:`We provide our icons in a single standard size of 16px. When your application requires different icon dimensions, you can adjust the icon size using simple CSS.
+This approach allows you to scale icons up or down as needed without requiring separate icon variants for each size.`}),`
+`,n.jsx(e.h4,{id:"example",children:"Example:"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-css",children:`ifx-icon svg {
+  width: 50px;
+  height: 50px;
+}
+`})})]})}function p(i={}){const{wrapper:e}={...t(),...i.components};return e?n.jsx(e,{...i,children:n.jsx(s,{...i})}):s(i)}export{p as default};
