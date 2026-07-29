ec2effeb80ea30fc7067d6eaa76cf58a100337c8
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-07-29 10:08:24 +0000

    Deploy preview for PR 2303 🛫

--- pr-preview-storybook/pr-2303/assets/formatter-EIJCOSYU-BuoADP1p.js
+++
@@ -1,1 +0,0 @@
-import{d as i,e as m}from"./blocks-CFDEZ05C.js";import{c as o}from"./iframe-A3HwJ4O3.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(m(),1),s=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{s as formatter};
