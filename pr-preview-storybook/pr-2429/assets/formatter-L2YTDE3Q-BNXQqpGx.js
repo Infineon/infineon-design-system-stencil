caac8bf51fbd18b41919ba032181f5e13a77fe52
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-07-29 08:01:11 +0000

    Deploy preview for PR 2429 🛫

---
+++ pr-preview-storybook/pr-2429/assets/formatter-L2YTDE3Q-BNXQqpGx.js
@@ -0,0 +1,1 @@
+import{d as m}from"./blocks-DNuAlktO.js";import{d as e,c as o}from"./iframe-WxlD5AVZ.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(m(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:e(r));export{s as formatter};
