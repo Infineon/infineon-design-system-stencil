3ceb43c1782351f19721cc744c3924decfa5d266
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-07-30 06:56:31 +0000

    deploy: a9e9fc6879463425cd1f02c410a94a53addc4a99

--- html-vite-example/assets/ifx-dropdown-trigger.entry-SrN_G-ci.js
+++
@@ -1,2 +0,0 @@
-import{r as s,h as o}from"./index-Ci7_vos-.js";const e=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){s(this,r)}isOpen=!1;render(){return o("slot",{key:"59cae183f64fd5f9c422ffa9486db002c1b67098"})}};t.style=e();export{t as ifx_dropdown_trigger};
-//# sourceMappingURL=ifx-dropdown-trigger.entry-SrN_G-ci.js.map
