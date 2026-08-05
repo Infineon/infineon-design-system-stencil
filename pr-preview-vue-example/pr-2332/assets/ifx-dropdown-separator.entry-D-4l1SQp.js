5ec9a1689fbf30ad50e4c00570f7ec2ca83b34d8
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-08-05 18:59:56 +0000

    Deploy preview for PR 2332 🛫

--- pr-preview-vue-example/pr-2332/assets/ifx-dropdown-separator.entry-D-4l1SQp.js
+++
@@ -1,1 +0,0 @@
-import{r as o,h as e}from"./index-7kdlWMz8.js";const a=()=>".dropdown-separator{height:1px;background-color:#eeeded;margin:8px 16px}",s=class{constructor(r){o(this,r)}render(){return e("div",{key:"57aef269019522a1c28f6e839e809917e7cb3f2f",class:"dropdown-separator"})}};s.style=a();export{s as ifx_dropdown_separator};
