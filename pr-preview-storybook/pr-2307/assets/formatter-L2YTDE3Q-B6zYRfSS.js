aaac1459f83d15fd991ffd0d40e09537c6422f46
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-08-05 11:57:54 +0000

    Deploy preview for PR 2307 🛫

---
+++ pr-preview-storybook/pr-2307/assets/formatter-L2YTDE3Q-B6zYRfSS.js
@@ -0,0 +1,1 @@
+import{d as m}from"./blocks-BslSmOjN.js";import{d as e,c as o}from"./iframe-BALjX1ph.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(m(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:e(r));export{s as formatter};
