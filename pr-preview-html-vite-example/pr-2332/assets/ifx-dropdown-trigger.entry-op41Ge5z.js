f28ca9178d75b5f5204387e4a08e5593a86bbc72
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-08-05 19:01:34 +0000

    Deploy preview for PR 2332 🛫

--- pr-preview-html-vite-example/pr-2332/assets/ifx-dropdown-trigger.entry-op41Ge5z.js
+++
@@ -1,2 +0,0 @@
-import{r as e,h as s}from"./index-DZ2aYB3C.js";const o=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){e(this,r)}isOpen=!1;render(){return s("slot",{key:"6a28fc612c250bae40b7160e12eaca698dc6d3bc"})}};t.style=o();export{t as ifx_dropdown_trigger};
-//# sourceMappingURL=ifx-dropdown-trigger.entry-op41Ge5z.js.map
