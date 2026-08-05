d9cb6aa751cd31a4abd26cf62acc5e97389cfbee
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-08-05 11:49:49 +0000

    Deploy preview for PR 2307 🛫

---
+++ pr-preview-vue-example/pr-2307/assets/ifx-multiselect-separator.entry-o7seuVr8.js
@@ -0,0 +1,1 @@
+import{r as t,h as r}from"./index-CvGHlkvm.js";const s=()=>".multiselect-separator{height:1px;background-color:#eeeded;margin:8px 16px}",a=class{constructor(e){t(this,e)}render(){return r("div",{key:"f2ec612b258eedc05acdfc5d936210d6f8999765",class:"multiselect-separator"})}};a.style=s();export{a as ifx_multiselect_separator};
