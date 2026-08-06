301f7171d0913f59599ddf30d5db485d2a422c9c
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-08-06 05:57:10 +0000

    Deploy preview for PR 2437 🛫

--- pr-preview-vue-example/pr-2437/assets/ifx-dropdown-trigger.entry-B25kXFJc.js
+++
@@ -1,1 +0,0 @@
-import{r as s,h as o}from"./index-uM5CH0dP.js";const e=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){s(this,r)}isOpen=!1;render(){return o("slot",{key:"a147a0b8452c34ca3833da40d0e22dab814d4223"})}};t.style=e();export{t as ifx_dropdown_trigger};
---
+++ pr-preview-vue-example/pr-2437/assets/ifx-dropdown-trigger.entry-DiCbGICu.js
@@ -0,0 +1,1 @@
+import{r as s,h as o}from"./index-LPb2cr2K.js";const e=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){s(this,r)}isOpen=!1;render(){return o("slot",{key:"a147a0b8452c34ca3833da40d0e22dab814d4223"})}};t.style=e();export{t as ifx_dropdown_trigger};
