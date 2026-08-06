119f203cb79448788531ce20e33330a4af216a03
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-08-06 05:37:24 +0000

    Deploy preview for PR 2437 🛫

---
+++ pr-preview-html-vite-example/pr-2437/assets/ifx-dropdown-trigger.entry-lJNeQvQy.js
@@ -0,0 +1,2 @@
+import{r as s,h as o}from"./index-DdGVt2J_.js";const e=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){s(this,r)}isOpen=!1;render(){return o("slot",{key:"a147a0b8452c34ca3833da40d0e22dab814d4223"})}};t.style=e();export{t as ifx_dropdown_trigger};
+//# sourceMappingURL=ifx-dropdown-trigger.entry-lJNeQvQy.js.map
