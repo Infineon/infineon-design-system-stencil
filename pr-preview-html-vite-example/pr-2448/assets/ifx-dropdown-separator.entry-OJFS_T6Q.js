2960b644e540a793f75f0098374cc51c08fea5d7
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-08-06 12:16:37 +0000

    Deploy preview for PR 2448 🛫

--- pr-preview-html-vite-example/pr-2448/assets/ifx-dropdown-separator.entry-OJFS_T6Q.js
+++
@@ -1,2 +0,0 @@
-import{r as o,h as e}from"./index-CSfS27N8.js";const d=()=>".dropdown-separator{height:1px;background-color:#eeeded;margin:8px 16px}",a=class{constructor(r){o(this,r)}render(){return e("div",{key:"3a28835dcd7dd300d9b7409e7e84fe4388767222",class:"dropdown-separator"})}};a.style=d();export{a as ifx_dropdown_separator};
-//# sourceMappingURL=ifx-dropdown-separator.entry-OJFS_T6Q.js.map
