1d9929b001c56c732666c826daf2ed9a8a5c3728
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-08-06 05:52:09 +0000

    Deploy preview for PR 2437 🛫

---
+++ pr-preview-html-vite-example/pr-2437/assets/ifx-dropdown-separator.entry-lKQUQIrc.js
@@ -0,0 +1,2 @@
+import{r as o,h as e}from"./index-Cu_GS7QN.js";const a=()=>".dropdown-separator{height:1px;background-color:#eeeded;margin:8px 16px}",s=class{constructor(r){o(this,r)}render(){return e("div",{key:"cacbec655b435fea6bce35ba06ae45bdb6219d46",class:"dropdown-separator"})}};s.style=a();export{s as ifx_dropdown_separator};
+//# sourceMappingURL=ifx-dropdown-separator.entry-lKQUQIrc.js.map
--- pr-preview-html-vite-example/pr-2437/assets/ifx-dropdown-separator.entry-rBJB7KhZ.js
+++
@@ -1,2 +0,0 @@
-import{r as o,h as e}from"./index-DdGVt2J_.js";const a=()=>".dropdown-separator{height:1px;background-color:#eeeded;margin:8px 16px}",s=class{constructor(r){o(this,r)}render(){return e("div",{key:"cacbec655b435fea6bce35ba06ae45bdb6219d46",class:"dropdown-separator"})}};s.style=a();export{s as ifx_dropdown_separator};
-//# sourceMappingURL=ifx-dropdown-separator.entry-rBJB7KhZ.js.map
