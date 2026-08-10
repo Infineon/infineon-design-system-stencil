2206b2612f4aa83c46c23e3187647fe52ec8a934
Pfaff Fabian <fabian.pfaff@infineon.com>
2026-08-10 16:06:52 +0200

    test(migrations): add inline component tests and update packaging test for Angular compiler resolution

--- packages/wrapper-angular/tests/fixtures/module-consumer/src/app/app.component.ts
+++ packages/wrapper-angular/tests/fixtures/module-consumer/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
 @Component({
   selector: "app-root",
@@ -8,3 +8,13 @@ import { Component } from "@angular/core";
 export class AppComponent {
   value = "";
 }
+
+@Component({
+  selector: "app-inline",
+  standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  template: `
+    <ifx-text-field show-delete-icon></ifx-text-field>
+  `,
+})
+export class InlineComponent {}
