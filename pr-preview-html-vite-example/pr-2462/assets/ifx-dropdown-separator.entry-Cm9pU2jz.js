2b438fa7590d9aea14b57038e1f241f073783300
bestcatarina <bestcatarina@users.noreply.github.com>
2026-08-13 15:54:22 +0000

    Deploy preview for PR 2462 🛫

--- pr-preview-html-vite-example/pr-2462/assets/ifx-dropdown-separator.entry-Cm9pU2jz.js
+++
@@ -1,2 +0,0 @@
-import{r as o,h as e}from"./index-CeM232dd.js";const a=()=>".dropdown-separator{height:1px;background-color:#eeeded;margin:8px 16px}",s=class{constructor(r){o(this,r)}render(){return e("div",{key:"3d64099049093804bc208578d458843e264964ae",class:"dropdown-separator"})}};s.style=a();export{s as ifx_dropdown_separator};
-//# sourceMappingURL=ifx-dropdown-separator.entry-Cm9pU2jz.js.map
