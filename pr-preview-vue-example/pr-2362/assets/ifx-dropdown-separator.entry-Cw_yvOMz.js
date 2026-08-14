8bf7937789a83e16ea05a186203d247c46441c41
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-08-14 09:32:18 +0000

    Deploy preview for PR 2362 🛫

---
+++ pr-preview-vue-example/pr-2362/assets/ifx-dropdown-separator.entry-Cw_yvOMz.js
@@ -0,0 +1,1 @@
+import{r as o,h as e}from"./index-CFgGJqXH.js";const a=()=>".dropdown-separator{height:1px;background-color:#eeeded;margin:8px 16px}",s=class{constructor(r){o(this,r)}render(){return e("div",{key:"3d64099049093804bc208578d458843e264964ae",class:"dropdown-separator"})}};s.style=a();export{s as ifx_dropdown_separator};
