69b82cd772731fc3db558f33cf2cf57c47912284
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-08-17 08:40:55 +0000

    deploy: 1f312db86de1280c69f45e970caf42f1a15037a2

---
+++ angular-module-example/chunk-4T6N46JU.js
@@ -0,0 +1,1 @@
+import{h as r,j as o}from"./chunk-OV3GYEP6.js";var a=()=>".dropdown-separator{height:1px;background-color:#eeeded;margin:8px 16px}",s=class{constructor(e){r(this,e)}render(){return o("div",{key:"3d64099049093804bc208578d458843e264964ae",class:"dropdown-separator"})}};s.style=a();export{s as ifx_dropdown_separator};
