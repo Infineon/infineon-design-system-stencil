f1343767e65612fd483cf935af86e7a18e62a0fc
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-08-18 08:01:07 +0000

    Deploy preview for PR 2396 🛫

--- pr-preview-storybook/pr-2396/assets/formatter-L2YTDE3Q-DCGNa7rO.js
+++
@@ -1,1 +0,0 @@
-import{d as m}from"./blocks-2P0Y8Pxh.js";import{d as e,c as o}from"./iframe-BGGcq6eW.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(m(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:e(r));export{s as formatter};
