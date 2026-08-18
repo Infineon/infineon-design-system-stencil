96416690ac54fc6f4febd2a5a603773c631d2732
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-08-18 10:02:21 +0000

    deploy: 304563b20c70b97a5cd57bdf27a2f4341309f5a5

---
+++ vue-example/assets/ifx-dropdown-separator.entry-AkqEcyLi.js
@@ -0,0 +1,1 @@
+import{r as o,h as e}from"./index-DaurAHrn.js";const a=()=>".dropdown-separator{height:1px;background-color:#eeeded;margin:8px 16px}",d=class{constructor(r){o(this,r)}render(){return e("div",{key:"fd88ddeb11f79fc2b0c032573e4a91a63ed7880f",class:"dropdown-separator"})}};d.style=a();export{d as ifx_dropdown_separator};
