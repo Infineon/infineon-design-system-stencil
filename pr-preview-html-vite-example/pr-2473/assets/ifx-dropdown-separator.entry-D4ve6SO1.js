0825115493a2b74f618efc41ab31d35017cf9921
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-08-20 13:48:39 +0000

    Deploy preview for PR 2473 🛫

--- pr-preview-html-vite-example/pr-2473/assets/ifx-dropdown-separator.entry-D4ve6SO1.js
+++
@@ -1,2 +0,0 @@
-import{r as o,h as e}from"./index-C0-Zr7JW.js";const a=()=>".dropdown-separator{height:1px;background-color:#eeeded;margin:8px 16px}",d=class{constructor(r){o(this,r)}render(){return e("div",{key:"fd88ddeb11f79fc2b0c032573e4a91a63ed7880f",class:"dropdown-separator"})}};d.style=a();export{d as ifx_dropdown_separator};
-//# sourceMappingURL=ifx-dropdown-separator.entry-D4ve6SO1.js.map
