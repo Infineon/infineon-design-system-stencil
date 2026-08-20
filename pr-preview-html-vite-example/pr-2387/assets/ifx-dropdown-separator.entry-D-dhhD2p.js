9fb74ad48eb1f391798de913b3ede9687bd4bd10
evangeliaPanteliadou <evangeliaPanteliadou@users.noreply.github.com>
2026-08-20 12:27:26 +0000

    Deploy preview for PR 2387 🛫

---
+++ pr-preview-html-vite-example/pr-2387/assets/ifx-dropdown-separator.entry-D-dhhD2p.js
@@ -0,0 +1,2 @@
+import{r as o,h as e}from"./index-CO_IeeM2.js";const a=()=>".dropdown-separator{height:1px;background-color:#eeeded;margin:8px 16px}",d=class{constructor(r){o(this,r)}render(){return e("div",{key:"fd88ddeb11f79fc2b0c032573e4a91a63ed7880f",class:"dropdown-separator"})}};d.style=a();export{d as ifx_dropdown_separator};
+//# sourceMappingURL=ifx-dropdown-separator.entry-D-dhhD2p.js.map
