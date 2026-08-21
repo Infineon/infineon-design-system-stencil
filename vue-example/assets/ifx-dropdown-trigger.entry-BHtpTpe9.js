3db71f8ecee943a8fbe91330f7f2e3788652a096
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-08-21 05:59:08 +0000

    deploy: ed16bde61d2e812d073de98be11e36e0bda7e281

---
+++ vue-example/assets/ifx-dropdown-trigger.entry-BHtpTpe9.js
@@ -0,0 +1,1 @@
+import{r as s,h as o}from"./index-BDN3n-1H.js";const e=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){s(this,r)}isOpen=!1;render(){return o("slot",{key:"c16509f761f38f9c51f060564df372cec2f4f873"})}};t.style=e();export{t as ifx_dropdown_trigger};
