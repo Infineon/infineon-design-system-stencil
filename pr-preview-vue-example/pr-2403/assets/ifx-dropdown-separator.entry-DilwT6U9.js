0df559986b9882e2fc05919d0b288b9da95a3f83
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-08-26 08:31:38 +0000

    Deploy preview for PR 2403 🛫

---
+++ pr-preview-vue-example/pr-2403/assets/ifx-dropdown-separator.entry-DilwT6U9.js
@@ -0,0 +1,1 @@
+import{r as o,h as a}from"./index-CUkBzLzH.js";const e=()=>".dropdown-separator{height:1px;background-color:#eeeded;margin:8px 16px}",s=class{constructor(r){o(this,r)}render(){return a("div",{key:"797260eacaac73037f09f4be747bd10ab01fd6a1",class:"dropdown-separator"})}};s.style=e();export{s as ifx_dropdown_separator};
---
+++ pr-preview-vue-example/pr-2403/assets/ifx-dropdown-trigger.entry-CZe_V8AW.js
@@ -0,0 +1,1 @@
+import{r,h as s}from"./index-CUkBzLzH.js";const o=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(e){r(this,e)}isOpen=!1;render(){return s("slot",{key:"dee6511e95d0e33478fe1f89442e7f46a0e0a6e7"})}};t.style=o();export{t as ifx_dropdown_trigger};
