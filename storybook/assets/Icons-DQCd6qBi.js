bc2922bb49e7300c72c8247b944f69b5306c325d
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-08-26 14:27:57 +0000

    deploy: 1f5bd2b9fa0304969406e44ee113a287afc28b95

--- storybook/assets/Icons-DQCd6qBi.js
+++
@@ -1,2 +0,0 @@
-import{u as o,j as e,M as s}from"./blocks-XeQ7HLC8.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-DM3OyxfQ.js";function r(t){return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Icon library",parameters:{previewTabs:{canvas:{hidden:!0},"storybook/docs/panel":{hidden:!0},"usage-tab/panel":{hidden:!0}}}}),`
-`,e.jsx("ifx-icons-preview",{})]})}function c(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(r,{...t})}):r()}export{c as default};
