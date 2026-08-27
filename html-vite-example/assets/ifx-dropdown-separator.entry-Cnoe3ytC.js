4e33235b40ea440dac6f7cf58248fc9ee92bbd36
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-08-27 12:36:30 +0000

    deploy: 86eb6fe07d136cd5144b941164a19761c147c174

---
+++ html-vite-example/assets/ifx-dropdown-separator.entry-Cnoe3ytC.js
@@ -0,0 +1,2 @@
+import{r as o,h as e}from"./index-CX9v27Im.js";const a=()=>".dropdown-separator{height:1px;background-color:#eeeded;margin:8px 16px}",s=class{constructor(r){o(this,r)}render(){return e("div",{key:"9e1be36738a7172158aa8089578f30c059854880",class:"dropdown-separator"})}};s.style=a();export{s as ifx_dropdown_separator};
+//# sourceMappingURL=ifx-dropdown-separator.entry-Cnoe3ytC.js.map
