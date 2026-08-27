a50230fa7fddba8396efef409ba9a95cc6400d6f
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-08-27 10:18:37 +0000

    Deploy preview for PR 2484 🛫

--- pr-preview-vue-example/pr-2484/assets/ifx-dropdown-trigger.entry-CO9-wHIy.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as s}from"./index-CID21nIg.js";const o=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){e(this,r)}isOpen=!1;render(){return s("slot",{key:"0c589eca9089f54232fb9b58f2a3e8309aeee49a"})}};t.style=o();export{t as ifx_dropdown_trigger};
