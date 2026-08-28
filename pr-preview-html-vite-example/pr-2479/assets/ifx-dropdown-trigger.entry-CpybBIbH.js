78ab683e6ce8eed70c24e7ae048516fb2f2cf929
sofiacarvalho-ifx <sofiacarvalho-ifx@users.noreply.github.com>
2026-08-28 11:31:11 +0000

    Deploy preview for PR 2479 🛫

---
+++ pr-preview-html-vite-example/pr-2479/assets/ifx-dropdown-trigger.entry-CpybBIbH.js
@@ -0,0 +1,2 @@
+import{r as e,h as s}from"./index-7hEw-5ym.js";const o=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){e(this,r)}isOpen=!1;render(){return s("slot",{key:"0c589eca9089f54232fb9b58f2a3e8309aeee49a"})}};t.style=o();export{t as ifx_dropdown_trigger};
+//# sourceMappingURL=ifx-dropdown-trigger.entry-CpybBIbH.js.map
