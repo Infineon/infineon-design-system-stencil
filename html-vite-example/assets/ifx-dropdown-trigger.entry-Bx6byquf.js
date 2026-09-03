141ae746976264b7b40926dbec4d9d87872991ed
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-09-03 08:42:09 +0000

    deploy: babe05a171813895db54d4517948d1e107bcee85

---
+++ html-vite-example/assets/ifx-dropdown-trigger.entry-Bx6byquf.js
@@ -0,0 +1,2 @@
+import{r,h as s}from"./index-Dyceu_Xc.js";const o=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(e){r(this,e)}isOpen=!1;render(){return s("slot",{key:"9cc34e6eeea48f64d8e091ed8cf2ee1e543d7b67"})}};t.style=o();export{t as ifx_dropdown_trigger};
+//# sourceMappingURL=ifx-dropdown-trigger.entry-Bx6byquf.js.map
---
+++ html-vite-example/assets/ifx-tab.entry-mTcBuCqS.js
@@ -0,0 +1,2 @@
+import{r as t,c as a,g as s,h as i}from"./index-Dyceu_Xc.js";const n=()=>":host{display:block}",r=class{constructor(e){t(this,e),this.tabHeaderChange=a(this,"tabHeaderChange",7)}get el(){return s(this)}header;disabled;icon="";iconPosition="left";subline;sublinePosition="left";label;number=0;positionSticky=!1;tabHeaderChange;componentWillUpdate(){this.tabHeaderChange.emit(this.header)}render(){return i("slot",{key:"101db8c2837202fc5feafb990dc71051f604339d"})}};r.style=n();export{r as ifx_tab};
+//# sourceMappingURL=ifx-tab.entry-mTcBuCqS.js.map
