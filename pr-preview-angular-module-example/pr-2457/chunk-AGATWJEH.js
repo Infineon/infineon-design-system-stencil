b13517c745cdd44dd313ff5e911800e25de70634
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-09-03 07:40:30 +0000

    Deploy preview for PR 2457 🛫

--- pr-preview-angular-module-example/pr-2457/chunk-AGATWJEH.js
+++
@@ -1,1 +0,0 @@
-import{h as e,j as r}from"./chunk-U3WWQJKW.js";var o=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(s){e(this,s)}isOpen=!1;render(){return r("slot",{key:"9cc34e6eeea48f64d8e091ed8cf2ee1e543d7b67"})}};t.style=o();export{t as ifx_dropdown_trigger};
