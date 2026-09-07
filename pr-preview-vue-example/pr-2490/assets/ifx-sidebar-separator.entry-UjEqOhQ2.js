9a4f810a5c99eb677cc50b6640d98fbd6f3c8480
sofiacarvalho-ifx <sofiacarvalho-ifx@users.noreply.github.com>
2026-09-07 16:26:10 +0000

    Deploy preview for PR 2490 🛫

---
+++ pr-preview-vue-example/pr-2490/assets/ifx-sidebar-separator.entry-UjEqOhQ2.js
@@ -0,0 +1,1 @@
+import{r as e,h as a}from"./index-zdO4A5vZ.js";const s=()=>".sidebar-separator{height:1px;background-color:#eeeded;margin:8px 0px}",t=class{constructor(r){e(this,r)}render(){return a("div",{key:"5f92ef8b62f0a884f4ef9ffbea149ad87c4bca6d",class:"sidebar-separator"})}};t.style=s();export{t as ifx_sidebar_separator};
