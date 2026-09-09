8f50d98806d28982ebe95d5969f7fafe532fdb9d
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-09-09 14:30:59 +0000

    Deploy preview for PR 2504 🛫

---
+++ pr-preview-html-vite-example/pr-2504/assets/ifx-dropdown-separator.entry-C784pVIu.js
@@ -0,0 +1,2 @@
+import{r as o,h as e}from"./index-HLXCSaVG.js";const a=()=>".dropdown-separator{height:1px;background-color:#eeeded;margin:8px 16px}",s=class{constructor(r){o(this,r)}render(){return e("div",{key:"e6c4bce7e7c98197593b9b86ea3251293628a403",class:"dropdown-separator"})}};s.style=a();export{s as ifx_dropdown_separator};
+//# sourceMappingURL=ifx-dropdown-separator.entry-C784pVIu.js.map
---
+++ pr-preview-html-vite-example/pr-2504/assets/ifx-dropdown-trigger.entry-G9yY8__j.js
@@ -0,0 +1,2 @@
+import{r as e,h as s}from"./index-HLXCSaVG.js";const o=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){e(this,r)}isOpen=!1;render(){return s("slot",{key:"c788b20c17ec63ba2228380fc006f0e8bf4e0b63"})}};t.style=o();export{t as ifx_dropdown_trigger};
+//# sourceMappingURL=ifx-dropdown-trigger.entry-G9yY8__j.js.map
