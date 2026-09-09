d84004e00db73907484aae69990014c0e2f02178
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-09-09 14:35:30 +0000

    Deploy preview for PR 2504 🛫

---
+++ pr-preview-angular-module-example/pr-2504/chunk-5HGALAPG.js
@@ -0,0 +1,1 @@
+import{h as r,j as o}from"./chunk-D5WP5YGW.js";var a=()=>".dropdown-separator{height:1px;background-color:#eeeded;margin:8px 16px}",s=class{constructor(e){r(this,e)}render(){return o("div",{key:"e6c4bce7e7c98197593b9b86ea3251293628a403",class:"dropdown-separator"})}};s.style=a();export{s as ifx_dropdown_separator};
