da9e53fdbd17efe24c6f0578bf209a7ec4707513
bestcatarina <bestcatarina@users.noreply.github.com>
2026-09-10 09:03:19 +0000

    Deploy preview for PR 2503 🛫

---
+++ pr-preview-html-vite-example/pr-2503/assets/ifx-tab.entry-LAKpRT6-.js
@@ -0,0 +1,2 @@
+import{r as t,c as a,g as s,h as i}from"./index-DojsAMWl.js";const n=()=>":host{display:block}",r=class{constructor(e){t(this,e),this.tabHeaderChange=a(this,"tabHeaderChange",7)}get el(){return s(this)}header;disabled;icon="";iconPosition="left";subline;sublinePosition="left";label;number=0;positionSticky=!1;tabHeaderChange;componentWillUpdate(){this.tabHeaderChange.emit(this.header)}render(){return i("slot",{key:"bbb2339a535da46a12c9482458105fc777a22a64"})}};r.style=n();export{r as ifx_tab};
+//# sourceMappingURL=ifx-tab.entry-LAKpRT6-.js.map
