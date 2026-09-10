f1ced2bedc8a9837be0cd3a91baa09be9c19e1c7
bestcatarina <bestcatarina@users.noreply.github.com>
2026-09-10 10:56:28 +0000

    Deploy preview for PR 2503 🛫

---
+++ pr-preview-storybook/pr-2503/assets/formatter-L2YTDE3Q-DI8wZP9e.js
@@ -0,0 +1,1 @@
+import{d as m}from"./blocks-BzbNzBjZ.js";import{d as e,c as o}from"./iframe-Cm_x9wti.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(m(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:e(r));export{s as formatter};
