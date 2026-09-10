5d6cb2b852d3158e8f73296591aca247212cf61b
joaoaraujo-pt <joaoaraujo-pt@users.noreply.github.com>
2026-09-10 14:41:49 +0000

    Deploy preview for PR 2505 🛫

---
+++ pr-preview-html-vite-example/pr-2505/assets/dom-utils-CtINY9El-CZY0_Num.js
@@ -0,0 +1,2 @@
+const n=t=>{let e=t.parentNode;for(;e;){if(e instanceof ShadowRoot){e=e.host;continue}if(e instanceof HTMLElement&&e.tagName.toLowerCase().startsWith("ifx"))return!0;e=e.parentNode}return!1};export{n as i};
+//# sourceMappingURL=dom-utils-CtINY9El-CZY0_Num.js.map
