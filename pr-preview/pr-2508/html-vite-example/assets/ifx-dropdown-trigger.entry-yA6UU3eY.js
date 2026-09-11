fab6e82e31f262b8f3b8d7e14ed36e9a63898069
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-09-11 09:55:53 +0000

    Deploy preview for PR 2508 🛫

---
+++ pr-preview/pr-2508/html-vite-example/assets/ifx-dropdown-trigger.entry-yA6UU3eY.js
@@ -0,0 +1,2 @@
+import{r,h as s}from"./index-DE5eTc1m.js";const o=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(e){r(this,e)}isOpen=!1;render(){return s("slot",{key:"9cc34e6eeea48f64d8e091ed8cf2ee1e543d7b67"})}};t.style=o();export{t as ifx_dropdown_trigger};
+//# sourceMappingURL=ifx-dropdown-trigger.entry-yA6UU3eY.js.map
