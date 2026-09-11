09eaf9d48455bd0e77c21aa5a0e1a2e2d84f0a36
joaoaraujo-pt <joaoaraujo-pt@users.noreply.github.com>
2026-09-11 15:37:09 +0000

    Deploy preview for PR 2510 🛫

--- pr-preview/pr-2510/vue-example/assets/ifx-dropdown-trigger.entry-DZWw3Kud.js
+++
@@ -1,1 +0,0 @@
-import{r as e,h as s}from"./index-CDy0RQGX.js";const o=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){e(this,r)}isOpen=!1;render(){return s("slot",{key:"c788b20c17ec63ba2228380fc006f0e8bf4e0b63"})}};t.style=o();export{t as ifx_dropdown_trigger};
