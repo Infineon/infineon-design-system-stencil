4b76b7ce9b50eafa98b4023e21cd478d4c69561b
João Araújo <joao.araujo2@infineon.com>
2026-09-14 16:38:11 +0100

    feat(advanced-table): adds controls to Storybook

--- packages/components/src/components/table-advanced-version/table.stories.ts
+++ packages/components/src/components/table-advanced-version/table.stories.ts
@@ -395,6 +395,20 @@ export default {
       },
     },
 
+    showSidebarFiltersButton: {
+      description: 'Shows or hides the sidebar filters toggle button. Only applies when filterOrientation is "sidebar".',
+      control: { type: 'boolean' },
+      table: {
+        category: 'ifx-table props',
+        defaultValue: {
+          summary: true,
+        },
+        type: {
+          summary: 'boolean',
+        },
+      },
+    },
+
     ifxSelectionChange: {
       action: 'ifxSelectionChange',
       description: 'A custom event triggered on checkbox selection',
@@ -515,7 +529,8 @@ const DefaultTemplate = (args: any) => {
     pagination="${args.pagination}"
     pagination-items-per-page='${args.paginationItemsPerPage}'
     filter-orientation="${args.filterOrientation}"
-    column-value-position="${args.columnValuePosition}">
+    column-value-position="${args.columnValuePosition}"
+    show-sidebar-filters-button="${args.showSidebarFiltersButton}">
     ${filterTypeGroupComponent}
 </ifx-table>`;
 
@@ -692,6 +707,7 @@ SidebarFilter.args = {
   filterOrientation: 'sidebar',
   pagination: true,
   paginationItemsPerPage: '[{"value":"10","selected":true}, {"value":"20","selected":false}, {"value":"30","selected":false}, {"value":"all","selected":false}]',
+  showSidebarFiltersButton: true,
 };
 
 export const TopbarFilter: any = DefaultTemplate.bind({});
