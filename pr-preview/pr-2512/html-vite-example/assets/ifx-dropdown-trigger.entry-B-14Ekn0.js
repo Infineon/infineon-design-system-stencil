bf865bb673eb6db0be674730b8ca43f6af73d040
joaoaraujo-pt <joaoaraujo-pt@users.noreply.github.com>
2026-09-18 09:29:17 +0000

    Deploy preview for PR 2512 🛫

---
+++ pr-preview/pr-2512/html-vite-example/assets/ifx-dropdown-trigger.entry-B-14Ekn0.js
@@ -0,0 +1,2 @@
+import{r as s,h as o}from"./index-CWQyt7jz.js";const e=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){s(this,r)}isOpen=!1;render(){return o("slot",{key:"d81c4b88d4acac4f2a6ba054d830650d4a3e9268"})}};t.style=e();export{t as ifx_dropdown_trigger};
+//# sourceMappingURL=ifx-dropdown-trigger.entry-B-14Ekn0.js.map
