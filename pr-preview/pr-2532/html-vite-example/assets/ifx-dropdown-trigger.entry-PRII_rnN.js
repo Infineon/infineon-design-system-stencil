d2b3009fae0c3f7ea2f0124b27cc606ac1f875de
tishoyanchev <tishoyanchev@users.noreply.github.com>
2026-09-22 06:49:33 +0000

    Deploy preview for PR 2532 🛫

---
+++ pr-preview/pr-2532/html-vite-example/assets/ifx-dropdown-trigger.entry-PRII_rnN.js
@@ -0,0 +1,2 @@
+import{r as s,h as o}from"./index-DVN6dhOf.js";const e=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){s(this,r)}isOpen=!1;render(){return o("slot",{key:"d81c4b88d4acac4f2a6ba054d830650d4a3e9268"})}};t.style=e();export{t as ifx_dropdown_trigger};
+//# sourceMappingURL=ifx-dropdown-trigger.entry-PRII_rnN.js.map
--- pr-preview/pr-2532/vue-example/assets/ifx-dropdown-trigger.entry-4-IcYQSB.js
+++
@@ -1,1 +0,0 @@
-import{r as s,h as o}from"./index-CEfgLgF3.js";const e=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){s(this,r)}isOpen=!1;render(){return o("slot",{key:"d81c4b88d4acac4f2a6ba054d830650d4a3e9268"})}};t.style=e();export{t as ifx_dropdown_trigger};
