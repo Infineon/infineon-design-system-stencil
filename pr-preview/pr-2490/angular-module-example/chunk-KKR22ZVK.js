dae762006c41938fe29242091fb3accfe0c13699
sofiacarvalho-ifx <sofiacarvalho-ifx@users.noreply.github.com>
2026-09-22 11:36:26 +0000

    Deploy preview for PR 2490 🛫

---
+++ pr-preview/pr-2490/angular-module-example/chunk-KKR22ZVK.js
@@ -0,0 +1,1 @@
+import{h as r,j as a}from"./chunk-YT46RWRM.js";var s=()=>".sidebar-separator{height:1px;background-color:#eeeded;margin:8px 0px}",t=class{constructor(e){r(this,e)}render(){return a("div",{key:"8d065ddba0a3c426ec2f47af830444b9ba0566e4",class:"sidebar-separator"})}};t.style=s();export{t as ifx_sidebar_separator};
---
+++ pr-preview/pr-2490/storybook/assets/formatter-L2YTDE3Q-DtnsHsa9.js
@@ -0,0 +1,1 @@
+import{d as m}from"./blocks-DY5-f-6T.js";import{d as e,c as o}from"./iframe-DuKGEZ4t.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(m(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:e(r));export{s as formatter};
