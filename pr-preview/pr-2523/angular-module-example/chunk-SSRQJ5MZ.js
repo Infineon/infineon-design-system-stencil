0d3088992eeff7191eb7a8ffe27cc88cbfd747f9
sofiacarvalho-ifx <sofiacarvalho-ifx@users.noreply.github.com>
2026-09-22 10:47:32 +0000

    Deploy preview for PR 2523 🛫

--- pr-preview/pr-2523/angular-module-example/chunk-SSRQJ5MZ.js
+++
@@ -1,1 +0,0 @@
-import{h as r,j as s}from"./chunk-AGKDYUSW.js";var e=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(o){r(this,o)}isOpen=!1;render(){return s("slot",{key:"d81c4b88d4acac4f2a6ba054d830650d4a3e9268"})}};t.style=e();export{t as ifx_dropdown_trigger};
---
+++ pr-preview/pr-2523/html-vite-example/assets/ifx-dropdown-trigger.entry-CYv-3v53.js
@@ -0,0 +1,2 @@
+import{r as s,h as o}from"./index-DLezkubs.js";const e=()=>":host{display:inline-block;cursor:pointer}",t=class{constructor(r){s(this,r)}isOpen=!1;render(){return o("slot",{key:"d81c4b88d4acac4f2a6ba054d830650d4a3e9268"})}};t.style=e();export{t as ifx_dropdown_trigger};
+//# sourceMappingURL=ifx-dropdown-trigger.entry-CYv-3v53.js.map
