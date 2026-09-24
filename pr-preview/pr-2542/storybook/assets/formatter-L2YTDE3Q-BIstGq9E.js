c743dd3517c4179f3a3c2c7c538145f4cd95bd5f
pfafffabian-ifx <pfafffabian-ifx@users.noreply.github.com>
2026-09-24 08:52:51 +0000

    Deploy preview for PR 2542 🛫

---
+++ pr-preview/pr-2542/storybook/assets/formatter-L2YTDE3Q-BIstGq9E.js
@@ -0,0 +1,1 @@
+import{d as m}from"./blocks-DaKQ9df6.js";import{d as e,c as o}from"./iframe-BmjdLECi.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(m(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:e(r));export{s as formatter};
